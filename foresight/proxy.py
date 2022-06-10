f8fd6e9ec8b7e4cfbb7e072d74fa4d80592ec2f6
Mahdi  B <b.mahdi@gmail.com>
2022-06-09 16:08:37 -1000

    Updates to foresight Proxy

--- foresight/proxy.py
+++ foresight/proxy.py
@@ -1,3 +1,5 @@
+
+
 #-----------------------------------------------------------------------------
 #  Copyright (c) SAGE3 Development Team
 #
@@ -12,130 +14,66 @@
 #  and no unknwon fields
 
 # TODO: call this class something else?
-
-import time
-import socketio
+import asyncio
 import json
-import os
-from wall import Wall
-import requests
+import websockets
 import argparse
-import urllib3
+# import urllib3
+import uuid
 
-urllib3.disable_warnings()
+# urllib3.disable_warnings()
 
 
 class BoardProxy():
-    def __init__(self, config_file):
+    def __init__(self, config_file, room_id):
         NB_TRIALS = 5
         self.wall = None
-        try:
-            self.__config = json.load(open(config_file, 'r'))
-            # Loop to wait for node server to be up
-            while NB_TRIALS:
-                try:
-                    response = requests.get(self.__config['server'], timeout=5)
-                    if response.status_code != 500:
-                        print(f'Server ready {response.status_code}')
-                        break
-                except requests.exceptions.RequestException as e:
-                    print('Server not ready, trying again', e)
-                    time.sleep(1)
-                    NB_TRIALS -= 1
+        self.__config = json.load(open(config_file))
+        self.__room_id = room_id
+        self.__headers = {'Authorization': f"Bearer {self.__config['token']}"}
 
-            # UUID of the board to track
-            self.board_uuid = self.__config["board_uuid"]
+    async def subscribe(self, sock, room_id):
+        subscription_id = str(uuid.uuid4())
+        message_id = str(uuid.uuid4())
+        print('Subscribing to board:', room_id, 'with subscriptionId:', subscription_id)
+        msg_sub = {
+            'route': '/api/apps/subscribe/:roomId', 'id': message_id,
+                   'body': {'subId': subscription_id, 'roomId': room_id}
+                   }
+        await sock.send(json.dumps(msg_sub))
+        msg_sub = {
+            'route': '/api/boards/subscribe/:roomId', 'id': message_id,
+                   'body': {'subId': subscription_id, 'roomId': room_id}
+                   }
+        await sock.send(json.dumps(msg_sub))
 
-            #  Build header with auth token
-            self.__head = {'Authorization': 'Bearer {}'.format(self.__config["token"])}
+        # # send the message
+        # msg_sub = {'route': '/api/apps/subscribe/:roomId', 'id': message_id,
+        #            'body': {'subId': subscription_id, 'roomId': room_id}}
+        # await sock.send(json.dumps(msg_sub))
 
-        except Exception as e:
-            print(f"Error in the proxy: {e}")
-            raise e
-
-    def setup(self):
-        self.sio = socketio.Client(ssl_verify=False)
-        self.call_backs()
-        print(self.__config["server"])
-        print(self.__config["path"])
-        print({'token': self.__config["token"]})
-        self.sio.connect(self.__config["server"],
-                         socketio_path=self.__config["path"],
-                         auth={'token': self.__config["token"]})
-
-    def loop(self):
-        if self.__config['production']:
-            self.sio.wait()
-        else:
-            pass
-
-    def call_backs(self):
-        @self.sio.event
-        def connect():
-            self.sio.emit('board-connect', {'boardId': self.board_uuid})
-            print('connection established')
-
-        @self.sio.on("data")
-        def raw_data(data):
-            print(f"Received msg: {data}")
-            if len(data['updates']['apps']) == 0 and len(data['updates']['data']) == 0:
-                # just connected and got a wall state message
-                # to make sure nothing weired is happending here
-                # data["state"]["apps"]
-                self.wall = Wall(data, self.__config)
-            elif len(data['updates']['apps']) > 0:
-                # an app was created or deleted or changes its features (ex. pos)
-                app_uuid = list(data["updates"]["apps"].keys())[0]
-                if app_uuid not in data["state"]["apps"]:
-                    # the app was deleted. Handle app delete
-                    print(f"app {app_uuid} was deleted")
-                    self.wall.remove_smartbit(app_uuid)
-                else:
-                    if app_uuid in self.wall.smartbits:
-                        # TODO: handle changes to the app features(position)
-                        #  or addition of a new element for collection type smartbits (ex.ImageViewer)
-                        smartbit = self.wall.smartbits[app_uuid]
-                        # check if smartbit exist for this app type
-                        if smartbit:
-                            smartbit.update_from_msg(
-                                data["state"]["apps"][app_uuid])
-                    else:
-                        print(f"app {app_uuid} was added")
-                        # the app was added. Handle app addition
-                        app_data = data["state"]["apps"][app_uuid]
-                        self.wall.create_smartbit(app_uuid, app_data)
-
-            elif len(data['updates']['data']) > 0:
-                # an app changed its data.
-                state_uuid = list(data['updates']['data'].keys())[0]
-                result = [app_uuid for app_uuid,
-                                       app in self.wall.smartbits if app and (app.state_uuid == state_uuid)]
-                if len(result) == 1:
-                    app_uuid = result[0]
-                    print(f"App {app_uuid} changed data")
-                    new_data = self.wall.smartbits[app_uuid].get_state()
-                    self.wall.smartbits[app_uuid].apply_new_state(
-                        new_data["data"])
-            else:
-                raise Exception(f"Unhandled situation in the proxy. {data}")
-
-        @self.sio.event
-        def disconnect():
-            print('Disconnecting from the server')
-
-    def run(self):
-        self.setup()
-        self.loop()
+        
+    async def run(self):
+        async with websockets.connect(self.__config["socket_server"], extra_headers={"Authorization": f"Bearer {self.__config['token']}"}) as ws:
+            await self.subscribe(ws, self.__room_id)
+            async for msg in ws:
+                print(f"I am here and msg is:\n {msg}")
 
 
+
+
+
+
+        
 def get_cmdline_parser():
     parser = argparse.ArgumentParser(description='Sage3 Python Proxy Server')
     parser.add_argument('-c', '--config_file', type=str, required=True, help="Configuration file path")
+    parser.add_argument('-r', '--room_id', type=str, required=False, help="Room id")
     return parser
 
 
 if __name__ == "__main__":
     parser = get_cmdline_parser()
     args = parser.parse_args()
-    board_proxy = BoardProxy(config_file=args.config_file)
-    board_proxy.run()
+    board_proxy = BoardProxy(args.config_file, args.room_id)
+    asyncio.run(board_proxy.run())
