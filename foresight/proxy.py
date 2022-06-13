5e75dcdecbe417f35628f1cdbb01c5647195893c
Mahdi  B <b.mahdi@gmail.com>
2022-06-13 12:41:59 -1000

    refactoring proxy for new backend

--- foresight/proxy.py
+++ foresight/proxy.py
@@ -16,21 +16,33 @@
 # TODO: call this class something else?
 import asyncio
 import json
+import time
+import multiprocessing
 import websockets
 import argparse
 # import urllib3
+from board import Board
 import uuid
+import threading
+from multiprocessing import Queue
+
+from threading import Thread
 
 # urllib3.disable_warnings()
 
 
 class BoardProxy():
+
+    OBJECT_CREATION_METHODS = {"BOARDS": Board}
+
     def __init__(self, config_file, room_id):
         NB_TRIALS = 5
-        self.wall = None
+        self.boards = {}
         self.__config = json.load(open(config_file))
         self.__room_id = room_id
         self.__headers = {'Authorization': f"Bearer {self.__config['token']}"}
+        self.__message_queue = Queue()
+
 
     async def subscribe(self, sock, room_id):
         subscription_id = str(uuid.uuid4())
@@ -52,19 +64,38 @@ class BoardProxy():
         #            'body': {'subId': subscription_id, 'roomId': room_id}}
         # await sock.send(json.dumps(msg_sub))
 
-        
-    async def run(self):
-        async with websockets.connect(self.__config["socket_server"], extra_headers={"Authorization": f"Bearer {self.__config['token']}"}) as ws:
-            await self.subscribe(ws, self.__room_id)
-            async for msg in ws:
-                print(f"I am here and msg is:\n {msg}")
+    def receive_messages(self):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        async def _run(self):
+            async with websockets.connect(self.__config["socket_server"], extra_headers={"Authorization": f"Bearer {self.__config['token']}"}) as ws:
+                await self.subscribe(ws, self.__room_id)
+                async for msg in ws:
+                    msg = json.loads(msg)
+                    print(f"I receive the follwing messages and I'm adding it to the queue\n {msg}")
+                    self.__message_queue.put("1")
+        asyncio.get_event_loop().run_until_complete(_run(self))
 
 
+    def process_messages(self):
+        while True:
+            item = self.__message_queue.get()
+            print(f'Working on {item}')
+            print(f'Finished {item}')
+
+    def clean_up(self):
+        print("cleaning up the queue")
+        if self.__message_queue.qsize() > 0:
+            print("Queue was not empty")
+        self.__message_queue.close()
 
 
+# if msg["event"]["type"] == "CREATE":
+#     if  msg["event"]["key"].split(":")[2] == "BOARDS":
+#         new_board = Board(msg["event"]["doc"]["data"])
+#         self.boards[new_board.id] = new_board
+#     else:
+#         pass
 
-
-        
 def get_cmdline_parser():
     parser = argparse.ArgumentParser(description='Sage3 Python Proxy Server')
     parser.add_argument('-c', '--config_file', type=str, required=True, help="Configuration file path")
@@ -76,4 +107,17 @@ if __name__ == "__main__":
     parser = get_cmdline_parser()
     args = parser.parse_args()
     board_proxy = BoardProxy(args.config_file, args.room_id)
-    asyncio.run(board_proxy.run())
+    listening_process = multiprocessing.Process(target=board_proxy.receive_messages)
+    worker_process = multiprocessing.Process(target=board_proxy.process_messages)
+    try:
+        # start the process responsible for listening to messages and adding them to the queue
+        listening_process.start()
+        # start the process responsible for handling message added to the queue.
+        worker_process.start()
+        while True: time.sleep(100)
+    except (KeyboardInterrupt, SystemExit):
+        print('\n! Received keyboard interrupt, quitting threads.\n')
+        board_proxy.clean_up()
+        listening_process.st
+        # worker_process.join()
+        print("I am here")
