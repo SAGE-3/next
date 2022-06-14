b58604f995bf5cee0162ee2d7d19eaa4fbaf3d2d
Ryan Theriot <rtheriot@hawaii.edu>
2022-06-14 09:06:31 -1000

    Cleaned up board page. Added dynamic app creation menu. If the app is setup properly it will be listed in the drop down menu.

--- webstack/apps/webapp/src/app/pages/Board.tsx
+++ webstack/apps/webapp/src/app/pages/Board.tsx
@@ -5,13 +5,14 @@
  * the file LICENSE, distributed as part of this software.
  *
  */
-import { Box, Button } from '@chakra-ui/react';
-import { Applications, initialValues } from '@sage3/applications/apps';
-import { useAppStore } from '@sage3/frontend';
 
 import { useEffect } from 'react';
-import Draggable from 'react-draggable';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Avatar, Box, Button, Select, Text } from '@chakra-ui/react';
+
+import { Applications, initialValues } from '@sage3/applications/apps';
+import { AppName } from '@sage3/applications/schema';
+import { useAppStore, useBoardStore, useUserStore } from '@sage3/frontend';
 
 type LocationParams = {
   boardId: string;
@@ -26,13 +27,17 @@ export function BoardPage() {
 
   const apps = useAppStore((state) => state.apps);
   const createApp = useAppStore((state) => state.create);
-  const subToBoard = useAppStore((state) => state.subscribeByBoardId);
-  const unsubToApp = useAppStore((state) => state.unsub);
+  const subBoard = useAppStore((state) => state.subToBoard);
+  const unsubBoard = useAppStore((state) => state.unsubToBoard);
+
+  const user = useUserStore((state) => state.user);
+  const boards = useBoardStore((state) => state.boards);
+  const board = boards.find(el => el.id === locationState.boardId);
 
   useEffect(() => {
-    subToBoard(locationState.boardId);
+    subBoard(locationState.boardId);
     return () => {
-      unsubToApp();
+      unsubBoard();
     }
   }, []);
 
@@ -40,87 +45,53 @@ export function BoardPage() {
     navigate('/home');
   }
 
-  function handleNoteClick() {
+  const handleNewApp = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const appName = event.target.value as AppName;
+    if (!appName) return;
     createApp(
-      'Note',
-      'Note Description',
+      appName,
+      `${appName} - Description`,
       locationState.roomId,
       locationState.boardId,
       { x: 0, y: 0, z: 0 },
       { width: 300, height: 300, depth: 0 },
       { x: 0, y: 0, z: 0 },
-      'Note',
-      initialValues['Note']);
-  }
-
-  function handleCounterClick() {
-    createApp(
-      'Counter',
-      'Counter Description',
-      locationState.roomId,
-      locationState.boardId,
-      { x: 0, y: 0, z: 0 },
-      { width: 300, height: 300, depth: 0 },
-      { x: 0, y: 0, z: 0 },
-      'Counter',
-      initialValues['Counter']);
-  }
-
-  function handleImageClick() {
-    createApp(
-      'Image',
-      'Image Description',
-      locationState.roomId,
-      locationState.boardId,
-      { x: 0, y: 0, z: 0 },
-      { width: 300, height: 300, depth: 0 },
-      { x: 0, y: 0, z: 0 },
-      'Image',
-      initialValues['Image']);
-  }
-
-  function handleSliderClick() {
-    createApp(
-      'Slider',
-      'Slider Description',
-      locationState.roomId,
-      locationState.boardId,
-      { x: 0, y: 0, z: 0 },
-      { width: 300, height: 300, depth: 0 },
-      { x: 0, y: 0, z: 0 },
-      'Slider',
-      initialValues['Slider']);
-  }
-
-  function handleLinkerClick() {
-    createApp(
-      'Linker',
-      'Linker Description',
-      locationState.roomId,
-      locationState.boardId,
-      { x: 0, y: 0, z: 0 },
-      { width: 300, height: 300, depth: 0 },
-      { x: 0, y: 0, z: 0 },
-      'Linker',
-      initialValues['Linker']);
+      appName,
+      initialValues[appName]);
   }
 
   return (
     <>
-      <Button colorScheme="green" onClick={handleHomeClick}>Home</Button>
-      <Button onClick={handleNoteClick}>Note App</Button>
-      <Button onClick={handleCounterClick}>Counter App</Button>
-      <Button onClick={handleImageClick}>Image App</Button>
-      <Button onClick={handleSliderClick}>Slider App</Button>
-      <Button onClick={handleLinkerClick}>Linker App</Button>
+      {/* Top bar */}
+      <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
+        <Button colorScheme="green" onClick={handleHomeClick}>Home</Button>
+        <Text fontSize="3xl">{board?.name}</Text>
+        <Avatar size='md' name={user?.name} backgroundColor={user?.color} color="black" />
+      </Box>
 
-
+      {/* Apps */}
       {apps.map((app) => {
         const Component = Applications[app.type];
         return (
           <Component key={app.id} {...app}></Component>
         );
       })}
+
+
+      {/* Bottom Bar */}
+      <Box display="flex" justifyContent="space-between" alignItems="center" p={2} position="absolute" bottom="0">
+
+        {/* App Create Menu */}
+        <Select
+          colorScheme="green"
+          placeholder='Create New App'
+          onChange={handleNewApp}
+          width="200px"
+        >
+          {Object.keys(Applications).map((appName) => <option key={appName} value={appName}>{appName}</option>)}
+        </Select>
+
+      </Box>
 
     </>
   );
