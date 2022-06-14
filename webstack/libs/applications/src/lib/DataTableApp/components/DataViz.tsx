7862e159f75a37c011479ac48849a344c874976d
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2022-06-13 21:31:13 -1000

    Console logs cell index and highlights cell on hover

--- webstack/libs/applications/src/lib/DataTableApp/components/DataViz.tsx
+++ webstack/libs/applications/src/lib/DataTableApp/components/DataViz.tsx
@@ -14,8 +14,7 @@ import {
 import './styles.css'
 import * as React from "react";
 import {useState} from "react";
-// import {Util} from "leaflet";
-// import isArray = Util.isArray;
+
 
 interface Props{
     data:any;
@@ -27,6 +26,16 @@ export const DataViz = ({data}:Props) => {
     const [loaded, setLoaded] = useState(false);
     const [headers, setHeaders] = useState<any[]>([]);
     let arr = [[]];
+
+
+
+    function clickMe() {
+        const cells = document.querySelectorAll('td');
+        cells.forEach(cell => {
+            cell.addEventListener('click', () =>
+                console.log("Row index: " + cell?.closest('tr')?.rowIndex + " | Column index: " + cell.cellIndex));
+        });
+    }
 
     function handleSubmit() {
         console.log(inputVal)
@@ -45,12 +54,18 @@ export const DataViz = ({data}:Props) => {
 
     function handleNesting(child: []) {
         if (typeof Object.keys(child) === 'object') {
+            console.log("This is a nested element")
             Array.from(child).forEach(element => {
                 // arr.push(element)
                 console.log(Object.keys(element))
             })
         }
         return arr
+    }
+
+    function handleCellClick() {
+        console.log('clicked!')
+        return 1
     }
 
     return (
@@ -78,11 +93,11 @@ export const DataViz = ({data}:Props) => {
                 <Tbody>
                         {
                             items.map((item) => (
-                                <Tr>
-                                    {Object.values(item).map((itemChild: any, index) => (
-                                        <>{(typeof itemChild === 'object') ?<Td> {handleNesting(itemChild)} </Td> : <Td> {itemChild} </Td>}</>
-                                    ))}
-                                </Tr>
+                                    <Tr key={item.id}>
+                                        {Object.values(item).map((itemChild: any, index) => (
+                                            <>{(typeof itemChild === 'object') ?<Td> {handleNesting(itemChild)} </Td> : <Td onClick={clickMe}> {itemChild} </Td>}</>
+                                        ))}
+                                    </Tr>
                             ))
                         }
                 </Tbody>
