756e61bc7530447b564092ca1043ad40c88814f5
Mahdi  B <b.mahdi@gmail.com>
2022-06-22 20:44:00 -1000

    updated allocation

--- foresight/proxy.py
+++ foresight/proxy.py
@@ -1,3 +1,4 @@
+
 # -----------------------------------------------------------------------------
 #  Copyright (c) SAGE3 Development Team
 #
@@ -24,6 +25,7 @@ from board import Board
 import uuid
 from multiprocessing import Queue
 import requests
+from smartbitfactory import SmartBitFactory
 
 from threading import Thread
 
@@ -60,11 +62,12 @@ class BoardProxy():
         self.__OBJECT_CREATION_METHODS = {
             "CREATE": self.__handle_create,
             "UPDATE": self.__handle_update,
+            "DELETE": self.__handle_delete,
         }
 
     def authenticat_new_user(self):
-        head = {'Authorization': f"Bearer {self.__config['token']}"}
-        r = requests.post( self.__config['server']+ '/auth/jwt', headers=head)
+        headers = {'Authorization': f"Bearer {self.__config['token']}"}
+        r = requests.post( self.__config['server'] + '/auth/jwt', headers=headers)
         response = r.json()
 
     def receive_messages(self):
@@ -74,6 +77,7 @@ class BoardProxy():
             async with websockets.connect(self.__config["socket_server"],
                                           extra_headers={"Authorization": f"Bearer {self.__config['token']}"}) as ws:
                 await subscribe(ws, self.room.room_id)
+                print("completed subscription, checking if boards and apps already there")
                 async for msg in ws:
                     msg = json.loads(msg)
                     print(f"I receive the follwing messages and I'm adding it to the queue\n {msg}")
@@ -89,10 +93,8 @@ class BoardProxy():
         """
         while True:
             msg = self.__message_queue.get()
-            print(f"Handling message: {msg}")
-            print(f'Working on {msg}')
-            # self.__OBJECT_CREATION_METHODS[message_type](msg)
-            # print(f'Finished processing {msg["id"]}')
+            msg_type = msg["event"]["type"]
+            self.__OBJECT_CREATION_METHODS[msg_type](msg)
 
     def clean_up(self):
         print("cleaning up the queue")
@@ -100,20 +102,29 @@ class BoardProxy():
             print("Queue was not empty")
         self.__message_queue.close()
 
+    def __handle_exec(self, msg):
+        pass
+
     def __handle_create(self, msg):
-        collection = msg["event"]["key"].split(":")[2]
+        data = msg["event"]["doc"]["data"]
+        collection = msg["event"]['col']
         if collection == "BOARDS":
-            print("Yes, I am adding a new board to the boards collection")
-            new_board = Board(msg["event"]["doc"]["data"])
-            print(new_board)
-            print(f"Room is of type {type(self.room)}")
+            print("BOARD CREATED")
+            new_board = Board(data)
             self.room.boards[new_board.id] = new_board
-            self.room.boards[1] = 2
-            print(f"self.boards is now: {self.room.boards}")
-            print("Done adding the new board")
+        elif collection == "APPS":
+            print("APP CREATED")
+            smartbit = SmartBitFactory.create_smartbit(data)
+            self.room.boards[data["boardId"]].smartbits[smartbit.id] = smartbit
 
     def __handle_update(self, msg):
+        print("HANDLE UPDATE")
         pass
+
+    def __handle_delete(self, msg):
+        print("HANDLE DELETE")
+        pass
+
 
 
 def get_cmdline_parser():
@@ -123,32 +134,40 @@ def get_cmdline_parser():
     return parser
 
 
-if __name__ == "__main__":
-    # The below is needed in running in iPython -- need to dig into this more
-    # multiprocessing.set_start_method("fork")
-    # parser = get_cmdline_parser()
-    # args = parser.parse_args()
-    board_proxy = BoardProxy("config.json", "18b94448-7698-4124-9d70-f3a78d0b41f3")
 
-    # room = Room("08d37fb0-b0a7-475e-a007-6d9dd35538ad")
-    # board_proxy = BoardProxy(args.config_file, args.room_id)
-    # listening_process = multiprocessing.Process(target=board_proxy.receive_messages)
-    # worker_process = multiprocessing.Process(target=board_proxy.process_messages)
+board_proxy = BoardProxy("config.json", "8723076c-d28f-4b11-8931-e15be5613c34")
+listening_process = threading.Thread(target=board_proxy.receive_messages)
+worker_process = threading.Thread(target=board_proxy.process_messages)
+listening_process.start()
+worker_process.start()
 
-    listening_process = threading.Thread(target=board_proxy.receive_messages)
-    worker_process = threading.Thread(target=board_proxy.process_messages)
-
-    try:
-        # start the process responsible for listening to messages and adding them to the queue
-        listening_process.start()
-        # start the process responsible for handling message added to the queue.
-        worker_process.start()
-
-        # while True:
-        #     time.sleep(100)
-    except (KeyboardInterrupt, SystemExit):
-        print('\n! Received keyboard interrupt, quitting threads.\n')
-        board_proxy.clean_up()
-        listening_process.st
-        # worker_process.join()
-        print("I am here")
+# TODO start threads cleanly in a way that can be easily stopped.
+# if __name__ == "__main__":
+#     # The below is needed in running in iPython -- need to dig into this more
+#     # multiprocessing.set_start_method("fork")
+#     # parser = get_cmdline_parser()
+#     # args = parser.parse_args()
+#     board_proxy = BoardProxy("config.json", "05828804-d87f-4498-857e-02f288effd3d")
+#
+#     # room = Room("08d37fb0-b0a7-475e-a007-6d9dd35538ad")
+#     # board_proxy = BoardProxy(args.config_file, args.room_id)
+#     # listening_process = multiprocessing.Process(target=board_proxy.receive_messages)
+#     # worker_process = multiprocessing.Process(target=board_proxy.process_messages)
+#
+#     listening_process = threading.Thread(target=board_proxy.receive_messages)
+#     worker_process = threading.Thread(target=board_proxy.process_messages)
+#
+#     try:
+#         # start the process responsible for listening to messages and adding them to the queue
+#         listening_process.start()
+#         # start the process responsible for handling message added to the queue.
+#         worker_process.start()
+#
+#         # while True:
+#         #     time.sleep(100)
+#     except (KeyboardInterrupt, SystemExit):
+#         print('\n! Received keyboard interrupt, quitting threads.\n')
+#         board_proxy.clean_up()
+#         listening_process.st
+#         # worker_process.join()
+#         print("I am here")
--- foresight/smartbits/smartbit.py
+++ foresight/smartbits/smartbit.py
@@ -4,6 +4,7 @@
 #  Distributed under the terms of the SAGE3 License.  The full license is in
 #  the file LICENSE, distributed as part of this software.
 #-----------------------------------------------------------------------------
+from enum import Enum
 
 from utils.wall_utils import Sage3Communication
 from collections import defaultdict
@@ -15,207 +16,56 @@ from collections import defaultdict
 # _default.default = JSONEncoder.default
 # JSONEncoder.default = _default
 
+from pydantic import BaseModel, Field,  validator
 
-class SmartBit(object):
-    # def __init__(self, app_uuid, state_uuid, x, y, width, height, last_update):
-    def __init__(self, state_name, data):
-        self.state_name = state_name
-        self.app_uuid = data["id"]
-        self.state_uuid = data['state'][self.state_name]["reference"]
-        self.__x = data["position"]["x"]
-        self.__y = data["position"]["y"]
-        self.__width = data["position"]["height"]
-        self.__height = data["position"]["width"]
-        self.state = {}
+class Position(BaseModel):
+    x: int
+    y: int
+    z: int
 
-        self.is_collection = False
+class Size(BaseModel):
+    width: int
+    height:int
+    depth: int
 
-        self.communication = Sage3Communication.instance()
+class Rotation(BaseModel):
+    x: int
+    y: int
+    z: int
 
-        # only listens to state variable updates
-        self.attrs_callbacks = defaultdict(list)
+class AppTypes(Enum):
+    counter = "Counter"
+    note = "Note"
 
-    def update_from_msg(self, data):
-        # update can to features of to data
-        if not bool(data["data"]):
-            self.app_uuid = data["id"]
-            self.state_uuid = data['state'][self.state_name]["reference"]
-            self.__x = data["position"]["x"]
-            self.__y = data["position"]["y"]
-            self.__width = data["position"]["width"]
-            self.__height = data["position"]["height"]
-        else:
-            # update the data (ex. add new image)
-            for datum_type in data["data"].keys():
-                if datum_type != 'file':
-                    for datum in data["data"][datum_type]:
-                        if datum["reference"] not in self.state:
-                            state = self.get_state(datum["reference"])["data"]
-                            self.state[datum["reference"]] = state
+class SmartBit(BaseModel):
+    id: str
+    name: str
+    description: str
+    position: Position
+    size: Size
+    rotation: Rotation
+    type: AppTypes
+    owner_id: str = Field(alias='ownerId')
 
-    @property
-    def x(self):
-        return self.__x
+    class Config:
+        validate_assignment = True
+        # need to field when obj was initialized to keep track of touched fields
+        initialized = False
+        touched_fields = set()
 
-    @x.setter
-    def x(self, value):
-        if value != self.__x:
-            self.__x = value
-            self.move_app(self.app_uuid, self.__x, self.__y)
+    def __init__(self, **kwargs):
+            self.__config__.initialized = False
+        self.__config__.touched_fields = set()
+        super().__init__(**kwargs)
+        self.__config__.initialized = True
 
-    @property
-    def y(self):
-        return self.__y
+    @validator("*")
+    def validate(cls, value, values, config, field):
+        if config.initialized:
+            print(f"field name is {field}")
+            config.touched_fields.add(field.name)
+        return value
 
-    @y.setter
-    def y(self, value):
-        if value != self.__y:
-            self.__y = value
-            self.move_app(self.app_uuid, self.__x, self.__y)
+    # def update(self):
+    #     print(f"updating fields {self.__config__.touched_fields}")
 
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if value != self.__width:
-            self.__width = value
-            self.resize_app(self.app_uuid, self.__x, self.__y,
-                            self.__width, self.__height)
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if value != self.__height:
-            self.__height = value
-            self.resize_app(self.app_uuid, self.__x, self.__y,
-                            self.__width, self.__height)
-
-    def get_state(self, obj_uuid=None):
-        # TODO: This need to be completely rewritten to use a communication object
-        #  the object should be responsible to do its own thing
-        if obj_uuid is None:
-            obj_uuid = self.state_uuid
-        return self.communication.get_app_data(obj_uuid)
-
-    def apply_new_state(self, state):
-        # gets new data from server and updates local attributes
-        try:
-            # self.state.update(state)
-            # invoke the callback now
-            for key, new_val in state.items():
-                self.state[key] = new_val
-                if key in self.attrs_callbacks:
-                    for func in self.attrs_callbacks[key]:
-                        func(new_val)
-
-        except Exception as e:
-            raise Exception(f"{e} In SmartBit, cannot update the data")
-
-    def update_state(self, new_attr_vals):
-        # updates the states on the Node server
-        print(f"\n\n updating the state with {new_attr_vals}")
-        self.communication.set_app_state(
-            self.state_uuid, new_attr_vals, self.state_type)
-
-    def update_state_attr(self, action_type, new_attr_vals, ):
-        # new_attr_vals is only the subset of state we want to pass
-        self.communication.set_app_attrs_state(
-            self.state_uuid, action_type, new_attr_vals)
-
-    def add_change_listener(self, attr_name, func):
-        self.attrs_callbacks[attr_name].append(func)
-
-    def move_app(self, x, y):
-        payload = {'id': self.app_uuid, 'type': 'move',
-                   'position': {'x': x, 'y': y}}
-        response = self.communication.send_payload(payload)
-        return response
-
-    def resize_app(self, x, y, width, height):
-        payload = {'id': self.app_uuid, 'type': 'resize', 'position': {
-            'x': x, 'y': y, 'width': width, 'height': height}}
-        response = self.communication.send_payload(payload)
-        return response
-
-
-class ContainerMixin:
-    is_collection = True
-
-    # def log(self):
-    #     pass
-    #
-    # def save_state(self):
-    #     pass
-    #
-    # #@_action(enqueue=False)
-    # def update_wall_coordinates(self, wall_coordinates):
-    #     # assumes wall coordinates are valid (proxy's job )
-    #
-    #     self.wall_coordinates = wall_coordinates
-    #     params = {"wall_coordinates": wall_coordinates}
-    #     return {"channel": "execute:down", "action": "update", "action_results": params}
-    #
-    # #@_action(enqueue=False)
-    # def update_attribute(self, attr_name, new_attr_value):
-    #     try:
-    #         setattr(self, attr_name, new_attr_value)
-    #     except:
-    #         raise Exception(f"Could not set attribute {attr_name} on class postit {self.__class__.__name__}")
-    #     params = {str(attr_name): new_attr_value}
-    #     return {"channel": "execute:down", "action": "update", "action_results": params}
-    #
-    # def jsonify(self, ignore=None, as_string = True):
-    #     """
-    #     :return:
-    #     """
-    #     if ignore is None:
-    #         ignore = []
-    #     json_repr = {}
-    #
-    #     temp_dict_representation = self.__dict__.copy()
-    #     temp_dict_representation['_created_date'] = self.__dict__['_created_date'].__str__()
-    #     temp_dict_representation["smartbit_type"] = type(self).__name__
-    #     temp_dict_representation["available_actions"] = self.get_actions()
-    #     # ignore certain field
-    #     for field in ignore:
-    #         del(temp_dict_representation[field])
-    #
-    #     # Use field's jsonify() if it has one
-    #     to_remove = []
-    #     for k, v in temp_dict_representation.items():
-    #         # print(k)
-    #         if hasattr(v, "jsonify"):
-    #             # we convert the objects to json because we don't want nest json strings
-    #             json_repr[k] = v.jsonify()
-    #             to_remove.append(k)
-    #     for k in to_remove:
-    #         del (temp_dict_representation[k])
-    #
-    #     if as_string:
-    #         json_repr.update(temp_dict_representation)
-    #
-    #     return json_repr
-    #
-    # def get_possible_actions(self):
-    #     return [func[0] for func in inspect.getmembers(self, predicate=inspect.ismethod) if
-    #                 hasattr(func[1], "action")]
-    #
-    # def get_attributes(self):
-    #     attributes = inspect.getmembers(self, lambda a: not (inspect.isroutine(a)))
-    #     non_ignore_attributes = []
-    #     return dict([a for a in attributes if not (a[0].startswith('_'))])
-    #
-    # def get_actions(self):
-    #     return [func[0] for func in inspect.getmembers(self, predicate=inspect.ismethod) if
-    #                 hasattr(func[1], "action")]
-    #
-    # def execute_up(self, msg):
-    #     self.redis_client.publish("execute:up", json.dumps(msg))
-    #
-    # def execute_down(self, msg):
-    #     self.redis_client.publish("execute:up", json.dumps(msg))
