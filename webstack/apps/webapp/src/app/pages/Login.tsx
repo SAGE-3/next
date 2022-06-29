0bd4455737b8b5d1ae2501323cff3310e3d70d77
renambot <renambot@gmail.com>
2022-06-29 18:54:32 -0500

    new: put back the login page and add CILogin (for production)

--- webstack/apps/webapp/src/app/pages/Login.tsx
+++ webstack/apps/webapp/src/app/pages/Login.tsx
@@ -1,10 +1,27 @@
-import { Button, ButtonGroup, IconButton } from '@chakra-ui/react';
+/**
+ * Copyright (c) SAGE3 Development Team
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ *
+ */
+
+
+import {
+  Button, ButtonGroup, IconButton,
+  Box, useColorMode, Image, Center, Text, VStack
+} from '@chakra-ui/react';
+
 import { AuthHTTPService, BgColor, useAuth } from '@sage3/frontend';
 import { useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGhost } from 'react-icons/fa';
+
+import sage3DarkMode from '../../assets/SAGE3DarkMode.png';
+import sage3LightMode from '../../assets/SAGE3LightMode.png';
+import cilogonLogo from '../../assets/cilogon-logo-32x32.png';
 
 export function LoginPage() {
   const auth = useAuth();
@@ -21,51 +38,103 @@ export function LoginPage() {
     authNavCheck();
   }, [authNavCheck]);
 
+  //  Dark/light mode
+  const { colorMode } = useColorMode()
+
+  // Detect if in production or development mode
+  let production = true;
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    production = false;
+  }
 
   return (
-    <div>
-      <h1>LOGIN PAGE</h1>
-      {/* Google Auth Service */}
-      <ButtonGroup isAttached width="17rem" size="lg">
-        <IconButton
-          width="6rem"
-          px="1rem"
-          aria-label="Login with Google"
-          icon={<FcGoogle size="25" />}
-          pointerEvents="none"
-          borderRight={`3px ${BgColor()} solid`}
+    <Box position="absolute" top="30%" left="50%" transform="translateX(-50%)">
+      <Box pb={'2rem'} alignItems="center">
+        <Image
+          width="20vw"
+          minWidth="400px"
+          maxWidth="35rem"
+          src={colorMode === 'light' ? sage3LightMode : sage3DarkMode}
+          alt="SAGE3 Logo"
+          fit="contain"
         />
-        <Button
-          width="17rem"
-          pl="1rem"
-          _hover={{ bg: 'teal.200', color: 'rgb(26, 32, 44)' }}
-          justifyContent="flex-start"
-          onClick={() => AuthHTTPService.loginWithGoogle()}
-        >
-          Login with Google
-        </Button>
-      </ButtonGroup>
+      </Box>
 
-      {/* Guest Auth Service */}
-      <ButtonGroup isAttached width="17rem" size="lg">
-        <IconButton
-          width="6rem"
-          px="1rem"
-          aria-label="Login as Guest"
-          icon={<FaGhost size="25" color="gray" />}
-          pointerEvents="none"
-          borderRight={`3px ${BgColor()} solid`}
-        />
-        <Button
-          width="17rem"
-          pl="1rem"
-          _hover={{ bg: 'teal.200', color: 'rgb(26, 32, 44)' }}
-          justifyContent="flex-start"
-          onClick={() => AuthHTTPService.loginWithGuest()}
-        >
-          Login as Guest
-        </Button>
-      </ButtonGroup>
-    </div>
+      {/* <Center>
+        <Text mr={'.5rem'}>Host: serverName </Text>
+      </Center> */}
+
+      <Box w="25rem">
+        <VStack spacing={4}>
+          {/* Google Auth Service */}
+          <ButtonGroup isAttached width="18rem" size="lg">
+            <IconButton
+              width="6rem"
+              px="1rem"
+              aria-label="Login with Google"
+              icon={<FcGoogle size="25" />}
+              pointerEvents="none"
+              borderRight={`3px ${BgColor()} solid`}
+            />
+            <Button
+              width="18rem"
+              pl="1rem"
+              _hover={{ bg: 'teal.200', color: 'rgb(26, 32, 44)' }}
+              justifyContent="flex-start"
+              disabled={!production}
+              onClick={() => AuthHTTPService.loginWithGoogle()}
+            >
+              Login with Google
+            </Button>
+          </ButtonGroup>
+
+          {/* CILogon Auth Service */}
+          <ButtonGroup isAttached width="18rem" size="lg">
+            <IconButton
+              width="6rem"
+              px="1rem"
+              aria-label="Login wit CILogon"
+              icon={<Image src={cilogonLogo} width="25px" />}
+              pointerEvents="none"
+              borderRight={`3px ${BgColor()} solid`}
+            />
+            <Button
+              width="18rem"
+              pl="1rem"
+              _hover={{ bg: 'teal.200', color: 'rgb(26, 32, 44)' }}
+              justifyContent="flex-start"
+              disabled={!production}
+              onClick={() => AuthHTTPService.loginWithGuest()}
+            >
+              Login with CILogon
+            </Button>
+          </ButtonGroup>
+
+          {/* Guest Auth Service */}
+          <ButtonGroup isAttached width="18rem" size="lg">
+            <IconButton
+              width="6rem"
+              px="1rem"
+              aria-label="Login as Guest"
+              icon={<FaGhost size="25" color="gray" />}
+              pointerEvents="none"
+              borderRight={`3px ${BgColor()} solid`}
+            />
+            <Button
+              width="18rem"
+              pl="1rem"
+              _hover={{ bg: 'teal.200', color: 'rgb(26, 32, 44)' }}
+              justifyContent="flex-start"
+              onClick={() => AuthHTTPService.loginWithGuest()}
+            >
+              Login as Guest
+            </Button>
+          </ButtonGroup>
+
+        </VStack>
+      </Box>
+
+
+    </Box >
   );
 }
