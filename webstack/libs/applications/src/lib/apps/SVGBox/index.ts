728552d8e18b61d6ba6ab2e83513e3217f61d5f2
renambot <renambot@gmail.com>
2022-07-21 17:01:28 -0500

    new: svg test app

---
+++ webstack/libs/applications/src/lib/apps/SVGBox/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) SAGE3 Development Team
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ *
+ */
+
+import { z } from 'zod';
+
+/**
+ * SAGE3 application: SVGBox
+ * created by: Luc Renambot
+ */
+
+export const schema = z.object({
+  file: z.string(),
+  city: z.string(),
+});
+export type state = z.infer<typeof schema>;
+
+export const init: Partial<state> = {
+  file: 'file.svg',
+  city: 'Chicago',
+};
+
+export const name = 'SVGBox';
