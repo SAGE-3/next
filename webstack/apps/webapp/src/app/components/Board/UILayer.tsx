974bf3e443e11f2a3839f35255fb5e2e2dfeee1e
renambot <renambot@gmail.com>
2022-08-18 18:12:32 -0500

    new: put confirmation modal before clearing the board

--- webstack/apps/webapp/src/app/components/Board/UILayer.tsx
+++ webstack/apps/webapp/src/app/components/Board/UILayer.tsx
@@ -8,7 +8,7 @@
 
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
-import { Box, useDisclosure } from '@chakra-ui/react';
+import { Box, useDisclosure, Modal } from '@chakra-ui/react';
 
 import {
   AssetModal, ContextMenu, UploadModal, useAppStore, useBoardStore, useUIStore,
@@ -24,7 +24,7 @@ import { InfoPanel } from './UI/InfoPanel';
 import { MiniMap } from './UI/Minimap';
 import { ButtonPanel, Panel } from './UI/Panel';
 import { Twilio } from './UI/Twilio';
-
+import { ClearBoardModal } from './UI/ClearBoardModal';
 import { AlfredComponent } from '@sage3/frontend';
 
 type UILayerProps = {
@@ -61,11 +61,12 @@ export function UILayer(props: UILayerProps) {
   const createApp = useAppStore((state) => state.create);
   const deleteApp = useAppStore((state) => state.delete);
 
-  // Asset manager button
+  // Asset manager modal
   const { isOpen: assetIsOpen, onOpen: assetOnOpen, onClose: assetOnClose } = useDisclosure();
-
   // Upload modal
   const { isOpen: uploadIsOpen, onOpen: uploadOnOpen, onClose: uploadOnClose } = useDisclosure();
+  // Clear boar modal
+  const { isOpen: clearIsOpen, onOpen: clearOnOpen, onClose: clearOnClose } = useDisclosure();
 
   const navigate = useNavigate();
 
@@ -187,6 +188,16 @@ export function UILayer(props: UILayerProps) {
     }
   }, [user, apps, props.boardId, presences]);
 
+  /**
+   * Clear the board confirmed
+   */
+  const onClearConfirm = () => {
+    // delete all apps
+    apps.forEach((a) => deleteApp(a._id));
+    // close the modal
+    clearOnClose();
+  };
+
   return (
     <Box display="flex" flexDirection="column" height="100vh" id="uilayer">
       <Panel title={'Applications'} opened={true} setPosition={setAppPanelPosition} position={appPanelPosition} height={351}>
@@ -199,7 +210,7 @@ export function UILayer(props: UILayerProps) {
         <ButtonPanel title="Home" onClick={handleHomeClick} colorScheme="blackAlpha" />
         <ButtonPanel title="Asset Browser" onClick={assetOnOpen} />
         <ButtonPanel title="Upload" onClick={uploadOnOpen} />
-        <ButtonPanel title="Clear Board" onClick={() => apps.forEach((a) => deleteApp(a._id))} />
+        <ButtonPanel title="Clear Board" onClick={clearOnOpen} />
       </Panel>
 
       <AppToolbar position={appToolbarPanelPosition} setPosition={setAppToolbarPosition}></AppToolbar>
@@ -207,7 +218,7 @@ export function UILayer(props: UILayerProps) {
       <MiniMap position={minimapPanelPosition} setPosition={setminimapPanelPosition} stuck={true} />
 
       <ContextMenu divId="board">
-        <BoardContextMenu boardId={props.boardId} roomId={props.roomId} clearBoard={() => apps.forEach((a) => deleteApp(a._id))} />
+        <BoardContextMenu boardId={props.boardId} roomId={props.roomId} clearBoard={clearOnOpen} />
       </ContextMenu>
 
       <InfoPanel
@@ -223,6 +234,11 @@ export function UILayer(props: UILayerProps) {
 
       {/* Upload dialog */}
       <UploadModal isOpen={uploadIsOpen} onOpen={uploadOnOpen} onClose={uploadOnClose}></UploadModal>
+
+      {/* Clear board dialog */}
+      <Modal isCentered isOpen={clearIsOpen} onClose={clearOnClose}>
+        <ClearBoardModal onClick={onClearConfirm} onClose={clearOnClose} ></ClearBoardModal>
+      </Modal>
 
       <Twilio roomName={props.boardId} connect={twilioConnect} />
 
