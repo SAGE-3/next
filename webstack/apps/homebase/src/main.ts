1131c37958ffbcdbc599e487cf01d8698a7c31b1
renambot <renambot@gmail.com>
2022-08-23 20:44:29 -0500

    fix: auth for JWT users

--- webstack/apps/homebase/src/main.ts
+++ webstack/apps/homebase/src/main.ts
@@ -251,8 +251,15 @@ async function startServer() {
             console.log('Authorization> ws ok', decoded?.sub);
             const payload = decoded as JWTPayload;
             if (decoded?.sub) {
+              const displayName = payload.name;
+              const email = payload.sub;
+              const extras = {
+                displayName: displayName ?? '',
+                email: email ?? '',
+                picture: '',
+              };
               // Find the actual user based on the token information
-              const authRecord = await SBAuthDB.findOrAddAuth('jwt', payload.sub);
+              const authRecord = await SBAuthDB.findOrAddAuth('jwt', payload.sub, extras);
               // Add the info to the request
               req.user = authRecord;
               if (wsPath === 'api') {
