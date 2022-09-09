a69badcb9b77cc7abb81b3c9b528cfd9129488be
Ryan Theriot <rtheriot@hawaii.edu>
2022-09-09 10:43:26 -1000

    Fix for cursor positions. Added event to the window and update cursor position in reference to the board's position

--- webstack/apps/webapp/src/app/components/Board/BackgroundLayer.tsx
+++ webstack/apps/webapp/src/app/components/Board/BackgroundLayer.tsx
@@ -6,10 +6,10 @@
  *
  */
 
-import { Tag } from '@chakra-ui/react';
+import { Box, Tag } from '@chakra-ui/react';
 import { Applications, AppError } from '@sage3/applications/apps';
 import { useAppStore, usePresence, usePresenceStore, useUIStore, useUser, useUsersStore } from '@sage3/frontend';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { DraggableEvent } from 'react-draggable';
 import { ErrorBoundary } from 'react-error-boundary';
 import { GiArrowCursor } from 'react-icons/gi';
@@ -45,50 +45,68 @@ export function BackgroundLayer(props: BackgroundLayerProps) {
   const presences = usePresenceStore((state) => state.presences);
   const users = useUsersStore((state) => state.users);
 
-  // On a drag stop of the board. Set the board position locally.
-  function handleDragBoardStop(event: DraggableEvent, data: DraggableData) {
-    // const x = Math.abs(data.x);
-    // const y = Math.abs(data.y);
-    const x = data.x;
-    const y = data.y;
-    setBoardPosition({ x, y });
+  const [dragging, setDragging] = useState(false);
+
+  function handleDragBoardStart() {
+    setDragging(true);
   }
 
-  // Track board position
-  useEffect(() => {
-    if (nodeRef.current) {
-      nodeRef.current.updatePosition({ x: boardPosition.x, y: boardPosition.y });
-    }
-  }, [boardPosition.x, boardPosition.y])
-
-  // Update the cursor every half second
-  // TODO: we skip events when the cursor is over the applications
-  const throttleCursor = throttle(500, (e: React.MouseEvent<HTMLDivElement>) => {
-    updatePresence({ cursor: { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY, z: 0 } });
-  });
-
-  // Keep a copy of the function
-  const throttleCursorFunc = useRef(throttleCursor);
-  const cursorFunc = (e: React.MouseEvent<HTMLDivElement>) => {
-    const boardElt = document.getElementById('board');
-    // Check if event is on the board
-    if (updatePresence && e.target === boardElt) {
-      // Send the throttled version to the server
-      throttleCursorFunc.current(e);
-    }
-  };
+  // On a drag stop of the board. Set the board position locally.
+  function handleDragBoardStop(event: DraggableEvent, data: DraggableData) {
+    const x = data.x;
+    const y = data.y;
+    setDragging(false);
+    setBoardPosition({ x, y });
+  }
 
   // Reset the global zIndex when no apps
   useEffect(() => {
     if (apps.length === 0) resetZIndex();
   }, [apps]);
 
+  // Track board position
+  useEffect(() => {
+    if (nodeRef.current) {
+      nodeRef.current.updatePosition({ x: boardPosition.x, y: boardPosition.y });
+    }
+  }, [boardPosition.x, boardPosition.y]);
+
+  // Update the cursor every half second
+  // TODO: we skip events when the cursor is over the applications
+  const throttleCursor = throttle(500, (e: MouseEvent) => {
+    if (dragging) return;
+    const winX = e.clientX;
+    const winY = e.clientY;
+    const bx = boardPosition.x;
+    const by = boardPosition.y;
+    const s = scale;
+    const x = winX / s - bx;
+    const y = winY / s - by;
+    const z = 0;
+    updatePresence({ cursor: { x, y, z } });
+  });
+
+  // Keep a copy of the function
+  const throttleCursorFunc = useCallback(throttleCursor, [boardPosition.x, boardPosition.y, scale, dragging]);
+  const cursorFunc = (e: MouseEvent) => {
+    // Check if event is on the board
+    if (updatePresence) {
+      // Send the throttled version to the server
+      throttleCursorFunc(e);
+    }
+  };
+
+  // Attach the mouse move event to the window
+  useEffect(() => {
+    const mouseMove = (e: MouseEvent) => cursorFunc(e);
+    window.addEventListener('mousemove', mouseMove);
+    return () => window.removeEventListener('mousemove', mouseMove);
+  }, [boardPosition.x, boardPosition.y, scale, dragging]);
+
   return (
-    <div style={{ transform: `scale(${scale})` }} onDoubleClick={() => setSelectedApp('')}>
+    <div style={{ transform: `scale(${scale})`, transformOrigin: 'top left' }} onDoubleClick={() => setSelectedApp('')}>
       {/* Board. Uses lib react-rnd for drag events.
        * Draggable Background below is the actual target for drag events.*/}
-      {/*Cursors */}
-
       <Rnd
         // Remember board position and size
         default={{
@@ -97,14 +115,14 @@ export function BackgroundLayer(props: BackgroundLayerProps) {
           width: boardWidth,
           height: boardHeight,
         }}
+        onDragStart={handleDragBoardStart}
         onDragStop={handleDragBoardStop}
         enableResizing={false}
         dragHandleClassName={'board-handle'}
-        scale={scale}
-        onMouseMove={cursorFunc}
-        // position={{ x: boardPosition.x, y: boardPosition.y }}
         // Get a ref the board object
-        ref={(c: Rnd) => { nodeRef.current = c; }}
+        ref={(c: Rnd) => {
+          nodeRef.current = c;
+        }}
       >
         {/* Apps - SORT is to zIndex order them */}
         {apps
@@ -127,18 +145,19 @@ export function BackgroundLayer(props: BackgroundLayerProps) {
         {/* Draw the cursors: filter by board and not myself */}
         {presences
           .filter((el) => el.data.boardId === props.boardId)
-          .filter((el) => el.data.userId !== user?._id)
+          // .filter((el) => el.data.userId !== user?._id)
           .map((presence) => {
             return (
               <div
                 key={presence.data.userId}
                 style={{
                   position: 'absolute',
-                  left: presence.data.cursor.x + 'px',
-                  top: presence.data.cursor.y + 'px',
+                  left: presence.data.cursor.x - 4 + 'px',
+                  top: presence.data.cursor.y - 3 + 'px',
                   transition: 'all 0.5s ease-in-out',
                   pointerEvents: 'none',
                   display: 'flex',
+                  zIndex: 100000,
                 }}
               >
                 <GiArrowCursor color="red"></GiArrowCursor>
@@ -153,6 +172,6 @@ export function BackgroundLayer(props: BackgroundLayerProps) {
         {/* Draggable Background */}
         <Background boardId={props.boardId} roomId={props.roomId}></Background>
       </Rnd>
-    </div >
+    </div>
   );
 }
