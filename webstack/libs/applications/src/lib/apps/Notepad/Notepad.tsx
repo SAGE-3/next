f0bef5cf549ebc4672e3db2f4488d26e5279ef23
Ryan Theriot <rtheriot@hawaii.edu>
2022-09-14 18:44:54 -1000

    Removed unused stuff from app component. commented some stuff

--- webstack/libs/applications/src/lib/apps/Notepad/Notepad.tsx
+++ webstack/libs/applications/src/lib/apps/Notepad/Notepad.tsx
@@ -6,40 +6,40 @@
  *
  */
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
-// Editor and collaborative editing
+// Yjs Imports
 import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 import { QuillBinding } from 'y-quill';
 import Quill from 'quill';
 import QuillCursors from 'quill-cursors';
 
+// Styles
+import 'quill/dist/quill.snow.css';
+import './styles.css';
+
+// SAGE Imports
 import { useUser } from '@sage3/frontend';
 import { state as AppState } from './index';
 import { AppWindow } from '../../components';
 import { App } from '@sage3/applications/schema';
 import { sageColorByName } from '@sage3/shared';
 
-// Styles
-import 'quill/dist/quill.snow.css';
-import './styles.css';
-
+// Have to register the module before using it
 Quill.register('modules/cursors', QuillCursors);
 
 function AppComponent(props: App): JSX.Element {
-  const divref = useRef(null);
-
-  const [quill, setQuill] = useState<Quill>();
-  const [ydoc, setYdoc] = useState<Y.Doc>();
-  const [provider, setProvider] = useState<WebsocketProvider>();
-  const [quillBinding, setQuillBinding] = useState<QuillBinding>();
-
+  const quillRef = useRef(null);
   const { user } = useUser();
 
   useEffect(() => {
-    if (divref.current) {
-      const quill = new Quill(divref.current, {
+    // Setup Yjs stuff
+    let provider: WebsocketProvider | null = null;
+    let ydoc: Y.Doc | null = null;
+    let binding: QuillBinding | null = null;
+    if (quillRef.current) {
+      const quill = new Quill(quillRef.current, {
         modules: {
           cursors: true,
           toolbar: [
@@ -47,10 +47,8 @@ function AppComponent(props: App): JSX.Element {
             ['bold', 'italic', 'underline', 'strike'],
             [{ color: [] }, { background: [] }],
             ['clean'],
-            // [{ script: 'super' }, { script: 'sub' }],
             ['code-block'],
             [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-            // [{ align: [] }],
             ['link', 'image'],
           ],
           history: {
@@ -63,35 +61,36 @@ function AppComponent(props: App): JSX.Element {
       });
 
       // A Yjs document holds the shared data
-      const ydoc = new Y.Doc();
+      ydoc = new Y.Doc();
 
       // WS Provider
       const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-      const provider = new WebsocketProvider(`${protocol}://${window.location.host}/yjs`, props._id, ydoc);
+      provider = new WebsocketProvider(`${protocol}://${window.location.host}/yjs`, props._id, ydoc);
 
       // Define a shared text type on the document
       const ytext = ydoc.getText('quill');
 
       // "Bind" the quill editor to a Yjs text type.
-      const binding = new QuillBinding(ytext, quill, provider.awareness);
+      // Uses SAGE3 information to set the color of the cursor
+      binding = new QuillBinding(ytext, quill, provider.awareness);
       if (user) {
         provider.awareness.setLocalStateField('user', {
-          // Define a print name that should be displayed
           name: user.data.name,
-          // Define a color that should be associated to the user:
           color: sageColorByName(user.data.color), // should be a hex color
         });
       }
     }
     return () => {
-      if (quillBinding) quillBinding.destroy();
+      // Remove the bindings and disconnect the provider
+      if (ydoc) ydoc.destroy();
+      if (binding) binding.destroy();
       if (provider) provider.disconnect();
     };
-  }, [divref]);
+  }, [quillRef]);
 
   return (
     <AppWindow app={props}>
-      <div ref={divref} style={{ width: '100%', height: '100%', backgroundColor: '#e5e5e5' }}></div>
+      <div ref={quillRef} style={{ width: '100%', height: '100%', backgroundColor: '#e5e5e5' }}></div>
     </AppWindow>
   );
 }
