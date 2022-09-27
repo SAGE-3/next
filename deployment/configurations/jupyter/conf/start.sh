058b6018713ffc8d8b2f7f0d19cc4c6dfb9fcd6d
renambot <renambot@gmail.com>
2022-09-27 14:51:55 -0500

    new: new deployment of containers and python example to connect

--- deployment/configurations/jupyter/conf/start.sh
+++ deployment/configurations/jupyter/conf/start.sh
@@ -10,6 +10,7 @@ echo "Token: $token"
 # Save it into a file
 echo "{ \"token\": \"$token\" }" >/conf/info.json
 
+# Inside docker compose, the redis server is named `redis-server`
 export REDIS_URL="redis-server"
 export REDIS_PORT=6379
 pip3 install redis
@@ -17,16 +18,15 @@ python3 /conf/redis-store.py $token
 
 # dependencies (requirements.txt)
 pip3 install websockets matplotlib rejson pytest requests namesgenerator httpx pydantic
-# extras
-# pip3 install python-magic rq dill opencv-python Pillow
-
 pip3 install ipython jupyter_client jupyter-console jupyterlab-link-share
+# extras
+#pip3 install python-magic rq dill opencv-python Pillow
 
 # 2D extension
 jupyter nbextension install /conf/2D-Jupyter --user
 jupyter nbextension enable 2D-Jupyter/2D-Jupyter --user
 
-# Start jupyter
+# Start jupyter: production mode equals SSL (docker will export the port)
 if [ "$ENVIRONMENT" = production ]; then
   jupyter lab --collaborative --no-browser --ServerApp.token="$token" --ServerApp.port=8888 \
     --ServerApp.ip=0.0.0.0 --ServerApp.allow_origin='*' \
