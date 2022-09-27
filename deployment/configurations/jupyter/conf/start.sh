3939d8cbe182250ffb1d90b4dd0bc227c23478f0
renambot <renambot@gmail.com>
2022-09-26 19:43:15 -0500

    new: start jupyter in prod or dev mode

--- deployment/configurations/jupyter/conf/start.sh
+++ deployment/configurations/jupyter/conf/start.sh
@@ -20,19 +20,20 @@ pip3 install websockets matplotlib rejson pytest requests namesgenerator httpx p
 # extras
 # pip3 install python-magic rq dill opencv-python Pillow
 
-# for jypyter console and client
-# pip3 install ipython jupyter_client==6.1.12 jupyter-console jupyterlab-link-share
 pip3 install ipython jupyter_client jupyter-console jupyterlab-link-share
 
 # 2D extension
 jupyter nbextension install /conf/2D-Jupyter --user
 jupyter nbextension enable 2D-Jupyter/2D-Jupyter --user
-# jupyter labextension install --user /conf/2D-Jupyter
-# jupyter labextension enable 2D-Jupyter/2D-Jupyter
 
 # Start jupyter
-#jupyter notebook --no-browser --NotebookApp.token="$token" --ServerApp.port=8888 --ServerApp.ip=0.0.0.0 --notebook-dir="notebooks"
-#jupyter lab --collaborative --no-browser --NotebookApp.token="$token" --ServerApp.port=8888 --ServerApp.ip=0.0.0.0 --notebook-dir="notebooks"
-
-jupyter lab --collaborative --no-browser --ServerApp.token="$token" --ServerApp.port=8888 --ServerApp.ip=0.0.0.0 --ServerApp.allow_origin='*' --notebook-dir="notebooks"
-
+if [ "$ENVIRONMENT" = production ]; then
+  jupyter lab --collaborative --no-browser --ServerApp.token="$token" --ServerApp.port=8888 \
+    --ServerApp.ip=0.0.0.0 --ServerApp.allow_origin='*' \
+    --ServerApp.keyfile=/conf/keys/private-key.key --ServerApp.certfile=/conf/keys/certificate.crt \
+    --notebook-dir="notebooks"
+else
+  jupyter lab --collaborative --no-browser --ServerApp.token="$token" --ServerApp.port=8888 \
+    --ServerApp.ip=0.0.0.0 --ServerApp.allow_origin='*' \
+    --notebook-dir="notebooks"
+fi
