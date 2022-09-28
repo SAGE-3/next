9fbc097bd93168da59008d8e432cfb68df1782e2
renambot <renambot@gmail.com>
2022-09-28 16:47:10 -0500

    new: add skeleton for Kernels app, showing kernels status.

--- webstack/libs/applications/src/lib/apps.ts
+++ webstack/libs/applications/src/lib/apps.ts
@@ -1,74 +1,71 @@
-/**
- * Copyright (c) SAGE3 Development Team
- *
- * Distributed under the terms of the SAGE3 License.  The full license is in
- * the file LICENSE, distributed as part of this software.
- *
- */
+// SAGE3 Generated from apps.json file
 
-import { name as CounterName } from './apps/Counter';
-import { name as LinkerName } from './apps/Linker';
-import { name as StickieName } from './apps/Stickie';
-import { name as PDFViewerName } from './apps/PDFViewer';
-import { name as CodeCellName } from './apps/CodeCell';
-import { name as ImageViewerName } from './apps/ImageViewer';
-import { name as LeafLetName } from './apps/LeafLet';
-import { name as VideoViewerName } from './apps/VideoViewer';
-import { name as WebviewName } from './apps/Webview';
-import { name as DataTableAppName } from './apps/DataTableApp';
 import { name as CSVViewerName } from './apps/CSVViewer';
-import { name as RTCChatName } from './apps/RTCChat';
-import { name as ScreenshareName } from './apps/TwilioScreenshare';
-import { name as VegaLiteViewerName } from './apps/VegaLiteViewer';
-import { name as VegaLiteName } from './apps/VegaLite';
 import { name as ClockName } from './apps/Clock';
 import { name as CobrowseName } from './apps/Cobrowse';
-import { name as JupyterLabName } from './apps/JupyterLab';
+import { name as CodeCellName } from './apps/CodeCell';
+import { name as CounterName } from './apps/Counter';
+import { name as DataTableAppName } from './apps/DataTableApp';
 import { name as DeepZoomImageName } from './apps/DeepZoomImage';
+import { name as ImageViewerName } from './apps/ImageViewer';
+import { name as JupyterLabName } from './apps/JupyterLab';
+import { name as KernelsName } from './apps/Kernels';
+import { name as LeafLetName } from './apps/LeafLet';
+import { name as LinkerName } from './apps/Linker';
 import { name as NotepadName } from './apps/Notepad';
+import { name as PDFViewerName } from './apps/PDFViewer';
+import { name as RTCChatName } from './apps/RTCChat';
+import { name as ScreenshareName } from './apps/TwilioScreenshare';
+import { name as StickieName } from './apps/Stickie';
+import { name as VegaLiteName } from './apps/VegaLite';
+import { name as VegaLiteViewerName } from './apps/VegaLiteViewer';
+import { name as VideoViewerName } from './apps/VideoViewer';
+import { name as WebviewName } from './apps/Webview';
 
-import Counter from './apps/Counter/Counter';
-import Linker from './apps/Linker/Linker';
-import Stickie from './apps/Stickie/Stickie';
-import PDFViewer from './apps/PDFViewer/PDFViewer';
-import CodeCell from './apps/CodeCell/CodeCell';
-import ImageViewer from './apps/ImageViewer/ImageViewer';
-import LeafLet from './apps/LeafLet/LeafLet';
-import VideoViewer from './apps/VideoViewer/VideoViewer';
-import Webview from './apps/Webview/Webview';
-import DataTableApp from './apps/DataTableApp/DataTableApp';
 import CSVViewer from './apps/CSVViewer/CSVViewer';
-import RTCChat from './apps/RTCChat/RTCChat';
-import Screenshare from './apps/TwilioScreenshare/TwilioScreenshare';
 import Clock from './apps/Clock/Clock';
 import Cobrowse from './apps/Cobrowse/Cobrowse';
-import JupyterLab from './apps/JupyterLab/JupyterLab';
-import VegaLiteViewer from './apps/VegaLiteViewer/VegaLiteViewer';
-import VegaLite from './apps/VegaLite/VegaLite';
+import CodeCell from './apps/CodeCell/CodeCell';
+import Counter from './apps/Counter/Counter';
+import DataTableApp from './apps/DataTableApp/DataTableApp';
 import DeepZoomImage from './apps/DeepZoomImage/DeepZoomImage';
+import ImageViewer from './apps/ImageViewer/ImageViewer';
+import JupyterLab from './apps/JupyterLab/JupyterLab';
+import Kernels from './apps/Kernels/Kernels';
+import LeafLet from './apps/LeafLet/LeafLet';
+import Linker from './apps/Linker/Linker';
 import Notepad from './apps/Notepad/Notepad';
+import PDFViewer from './apps/PDFViewer/PDFViewer';
+import RTCChat from './apps/RTCChat/RTCChat';
+import Screenshare from './apps/TwilioScreenshare/TwilioScreenshare';
+import Stickie from './apps/Stickie/Stickie';
+import VegaLite from './apps/VegaLite/VegaLite';
+import VegaLiteViewer from './apps/VegaLiteViewer/VegaLiteViewer';
+import VideoViewer from './apps/VideoViewer/VideoViewer';
+import Webview from './apps/Webview/Webview';
 
 export const Applications = {
-  [CounterName]: Counter,
-  [LinkerName]: Linker,
-  [StickieName]: Stickie,
-  [PDFViewerName]: PDFViewer,
-  [CodeCellName]: CodeCell,
-  [ImageViewerName]: ImageViewer,
-  [LeafLetName]: LeafLet,
-  [VideoViewerName]: VideoViewer,
-  [WebviewName]: Webview,
-  [DataTableAppName]: DataTableApp,
   [CSVViewerName]: CSVViewer,
-  [RTCChatName]: RTCChat,
-  [ScreenshareName]: Screenshare,
   [ClockName]: Clock,
   [CobrowseName]: Cobrowse,
-  [JupyterLabName]: JupyterLab,
-  [VegaLiteViewerName]: VegaLiteViewer,
-  [VegaLiteName]: VegaLite,
+  [CodeCellName]: CodeCell,
+  [CounterName]: Counter,
+  [DataTableAppName]: DataTableApp,
   [DeepZoomImageName]: DeepZoomImage,
+  [ImageViewerName]: ImageViewer,
+  [JupyterLabName]: JupyterLab,
+  [KernelsName]: Kernels,
+  [LeafLetName]: LeafLet,
+  [LinkerName]: Linker,
   [NotepadName]: Notepad,
+  [PDFViewerName]: PDFViewer,
+  [RTCChatName]: RTCChat,
+  [ScreenshareName]: Screenshare,
+  [StickieName]: Stickie,
+  [VegaLiteName]: VegaLite,
+  [VegaLiteViewerName]: VegaLiteViewer,
+  [VideoViewerName]: VideoViewer,
+  [WebviewName]: Webview,
 } as unknown as Record<string, { AppComponent: () => JSX.Element; ToolbarComponent: () => JSX.Element }>;
 
 export * from './components';
--- webstack/libs/applications/src/lib/types.ts
+++ webstack/libs/applications/src/lib/types.ts
@@ -1,66 +1,28 @@
 // SAGE3 Generated from apps.json file
 
-import { state as CounterState, name as CounterName } from './apps/Counter';
-import { state as LinkerState, name as LinkerName } from './apps/Linker';
-import { state as StickieState, name as StickieName } from './apps/Stickie';
-import { state as PDFViewerState, name as PDFViewerName } from './apps/PDFViewer';
+import { state as CSVViewerState, name as CSVViewerName } from './apps/CSVViewer';
+import { state as ClockState, name as ClockName } from './apps/Clock';
+import { state as CobrowseState, name as CobrowseName } from './apps/Cobrowse';
 import { state as CodeCellState, name as CodeCellName } from './apps/CodeCell';
+import { state as CounterState, name as CounterName } from './apps/Counter';
+import { state as DataTableAppState, name as DataTableAppName } from './apps/DataTableApp';
+import { state as DeepZoomImageState, name as DeepZoomImageName } from './apps/DeepZoomImage';
 import { state as ImageViewerState, name as ImageViewerName } from './apps/ImageViewer';
+import { state as JupyterLabState, name as JupyterLabName } from './apps/JupyterLab';
+import { state as KernelsState, name as KernelsName } from './apps/Kernels';
 import { state as LeafLetState, name as LeafLetName } from './apps/LeafLet';
+import { state as LinkerState, name as LinkerName } from './apps/Linker';
+import { state as NotepadState, name as NotepadName } from './apps/Notepad';
+import { state as PDFViewerState, name as PDFViewerName } from './apps/PDFViewer';
+import { state as RTCChatState, name as RTCChatName } from './apps/RTCChat';
+import { state as ScreenshareState, name as ScreenshareName } from './apps/Screenshare';
+import { state as StickieState, name as StickieName } from './apps/Stickie';
+import { state as VegaLiteState, name as VegaLiteName } from './apps/VegaLite';
+import { state as VegaLiteViewerState, name as VegaLiteViewerName } from './apps/VegaLiteViewer';
 import { state as VideoViewerState, name as VideoViewerName } from './apps/VideoViewer';
 import { state as WebviewState, name as WebviewName } from './apps/Webview';
-import { state as DataTableAppState, name as DataTableAppName } from './apps/DataTableApp';
-import { state as CSVViewerState, name as CSVViewerName } from './apps/CSVViewer';
-import { state as RTCChatState, name as RTCChatName } from './apps/RTCChat';
-import { state as ScreenshareState, name as ScreenshareName } from './apps/TwilioScreenshare';
-import { state as ClockState, name as ClockName } from './apps/Clock';
-import { state as JupyterLabState, name as JupyterLabName } from './apps/JupyterLab';
-import { state as CobrowseState, name as CobrowseName } from './apps/Cobrowse';
-import { state as VegaLiteViewerState, name as VegaLiteViewerName } from './apps/VegaLiteViewer';
-import { state as VegaLiteState, name as VegaLiteName } from './apps/VegaLite';
-import { state as DeepZoomImageState, name as DeepZoomImageName } from './apps/DeepZoomImage';
-import { state as NotepadState, name as NotepadName } from './apps/Notepad';
 
-export type AppState =
-  | CounterState
-  | LinkerState
-  | StickieState
-  | PDFViewerState
-  | CodeCellState
-  | ImageViewerState
-  | LeafLetState
-  | VideoViewerState
-  | WebviewState
-  | DataTableAppState
-  | CSVViewerState
-  | RTCChatState
-  | ScreenshareState
-  | ClockState
-  | CobrowseState
-  | JupyterLabState
-  | VegaLiteViewerState
-  | VegaLiteState
-  | DeepZoomImageState
-  | NotepadState;
 
-export type AppName =
-  | typeof CounterName
-  | typeof LinkerName
-  | typeof StickieName
-  | typeof PDFViewerName
-  | typeof CodeCellName
-  | typeof ImageViewerName
-  | typeof LeafLetName
-  | typeof VideoViewerName
-  | typeof WebviewName
-  | typeof DataTableAppName
-  | typeof CSVViewerName
-  | typeof RTCChatName
-  | typeof ScreenshareName
-  | typeof ClockName
-  | typeof CobrowseName
-  | typeof JupyterLabName
-  | typeof VegaLiteViewerName
-  | typeof VegaLiteName
-  | typeof DeepZoomImageName
-  | typeof NotepadName;
+export type AppState = CSVViewerState | ClockState | CobrowseState | CodeCellState | CounterState | DataTableAppState | DeepZoomImageState | ImageViewerState | JupyterLabState | KernelsState | LeafLetState | LinkerState | NotepadState | PDFViewerState | RTCChatState | ScreenshareState | StickieState | VegaLiteState | VegaLiteViewerState | VideoViewerState | WebviewState;
+
+export type AppName = typeof CSVViewerName | typeof ClockName | typeof CobrowseName | typeof CodeCellName | typeof CounterName | typeof DataTableAppName | typeof DeepZoomImageName | typeof ImageViewerName | typeof JupyterLabName | typeof KernelsName | typeof LeafLetName | typeof LinkerName | typeof NotepadName | typeof PDFViewerName | typeof RTCChatName | typeof ScreenshareName | typeof StickieName | typeof VegaLiteName | typeof VegaLiteViewerName | typeof VideoViewerName | typeof WebviewName;
