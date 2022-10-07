2a1daa793e1552f1b8b9242bb83aa5a85b3ae14c
Michael Rogers <mlr2010@hawaii.edu>
2022-10-07 13:04:09 -1000

    Code cleanup and output resize fixed

--- webstack/libs/applications/src/lib/apps/SageCell/SageCell.tsx
+++ webstack/libs/applications/src/lib/apps/SageCell/SageCell.tsx
@@ -6,29 +6,19 @@
  *
  */
 
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Box, Button, HStack, useColorModeValue, Tooltip,
-  IconButton, VStack, Flex, ButtonGroup, Select, Badge
+  ButtonGroup, Select, Badge,
 } from '@chakra-ui/react';
 
 import './components/styles.css';
 // Date manipulation (for filename)
 import dateFormat from 'date-fns/format';
-// UUID generation
-import { v4 as getUUID } from 'uuid';
-
-import { MdDelete, MdPlayArrow, MdFileDownload, MdAdd, MdRemove, MdArrowDropDown } from 'react-icons/md';
-
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-python';
-import 'ace-builds/src-noconflict/mode-json';
-import 'ace-builds/src-noconflict/theme-tomorrow_night_bright';
-import 'ace-builds/src-noconflict/theme-xcode';
-import 'ace-builds/src-noconflict/keybinding-vscode';
+import { MdFileDownload, MdAdd, MdRemove, MdArrowDropDown } from 'react-icons/md';
 
 // SAGE3 imports
-import { useAppStore, useUser } from '@sage3/frontend';
+import { useAppStore } from '@sage3/frontend';
 import { state as AppState } from './index';
 import { AppWindow } from '../../components';
 import { App } from '../../schema';
@@ -37,7 +27,9 @@ import { App } from '../../schema';
 import { downloadFile } from '@sage3/frontend';
 
 // Rendering functions
-import { ProcessedOutput } from './render';
+// import { ProcessedOutput } from './render';
+import { InputBox } from './components/InputBox';
+import { OutputBox } from './components/OutputBox';
 
 /**
  * SageCell - SAGE3 application
@@ -47,147 +39,54 @@ import { ProcessedOutput } from './render';
  */
 const AppComponent = (props: App): JSX.Element => {
 
+  const [output, setOutput] = useState({} as any);
+
   const s = props.data.state as AppState;
+  
+  useEffect(() => {
+    if (s.output) {
+      try {
+        const parsed = JSON.parse(s.output);
+        if (parsed) {
+          // console.log(parsed);
+          setOutput(parsed);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [s.output]);
 
   return (
     <AppWindow app={props}>
-      <>
-        <Box w={'100%'} h={'100%'} bg={useColorModeValue('#E8E8E8', '#1A1A1A')} overflowY={'scroll'}>
-          <Box>{InputBox(props)}</Box>
-          <Box w={'100%'} h={'100%'} bg={useColorModeValue('#E8E8E8', '#1A1A1A')}>
-            <Box p={4} fontSize={s.fontSize + 'rem'} color={'GrayText'} overflowX={'hidden'}>
-              {ProcessedOutput(s.output)}
-            </Box>
+      <Box id="sc-container" w={'100%'} h={'100%'} bg={useColorModeValue('#E8E8E8', '#1A1A1A')} overflowY={'scroll'}>
+        {InputBox(props)}
+        {!output ? null : 
+        <>
+          <Box
+            p={4}
+            m={4}
+            className="sc-output"
+            style={{
+                height: '100%',
+                overflowX: 'auto',
+                backgroundColor: useColorModeValue('#F0F2F6', '#111111'),
+                boxShadow: '0 0 0 2px ' + useColorModeValue('rgba(0,0,0,0.4)', 'rgba(0, 128, 128, 0.5)'),
+                borderRadius: '4px',
+                fontFamily: 'monospace',
+                fontSize: `${s.fontSize}rem`,
+                color: useColorModeValue('#000000', '#FFFFFF'),
+                whiteSpace: 'pre-wrap',
+                wordWrap: 'break-word',
+                overflowWrap: 'break-word',
+            }}
+            >
+            {OutputBox(output)}
           </Box>
-        </Box>
-      </>
+        </>
+        }
+      </Box>
     </AppWindow>
-  );
-}
-
-/**
- *
- * @param props
- * @returns
- */
-const InputBox = (props: App): JSX.Element => {
-  const s = props.data.state as AppState;
-  const updateState = useAppStore((state) => state.updateState);
-  const ace = useRef<AceEditor>(null);
-  const [code, setCode] = useState<string>(s.code);
-  const { user } = useUser();
-  const [fontSize, setFontSize] = useState(s.fontSize);
-
-  const handleExecute = () => {
-    const code = ace.current?.editor?.getValue();
-    if (code) {
-      updateState(props._id, {
-        code: code,
-        output: '',
-        executeInfo: { executeFunc: 'execute', params: { uuid: getUUID() } },
-      });
-    }
-  };
-
-  const handleClear = () => {
-    updateState(props._id, {
-      code: '',
-      output: '',
-      executeInfo: { executeFunc: '', params: {} },
-    });
-    ace.current?.editor?.setValue('');
-  };
-
-  useEffect(() => {
-    setCode(s.code);
-  }, [s.code]);
-
-  // Update from Ace Editor
-  const updateCode = (c: string) => {
-    setCode(c);
-  };
-
-  useEffect(() => {
-    // update local state from global state
-    setFontSize(s.fontSize);
-  }, [s.fontSize]);
-
-  return (
-    <>
-      <HStack>
-        <AceEditor
-          ref={ace}
-          name="ace"
-          value={code}
-          onChange={updateCode}
-          readOnly={user?._id !== props._createdBy}
-          fontSize={fontSize + 'rem'}
-          minLines={4}
-          maxLines={20}
-          placeholder="Enter code here"
-          mode={s.language}
-          theme={useColorModeValue('xcode', 'tomorrow_night_bright')}
-          editorProps={{ $blockScrolling: true }}
-          setOptions={{
-            hasCssTransforms: true,
-            showGutter: true,
-            showPrintMargin: false,
-            highlightActiveLine: true,
-            showLineNumbers: true,
-          }}
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            marginTop: 15,
-            marginLeft: 15,
-            marginRight: 0,
-            marginBottom: 10,
-            padding: 0,
-            overflow: 'hidden',
-            backgroundColor: useColorModeValue('#F0F2F6', '#111111'),
-            boxShadow: '0 0 0 2px ' + useColorModeValue('rgba(0,0,0,0.4)', 'rgba(0, 128, 128, 0.5)'),
-            borderRadius: '4px',
-          }}
-          commands={[
-            { name: 'Execute', bindKey: { win: 'Shift-Enter', mac: 'Shift-Enter' }, exec: handleExecute },
-            { name: 'Clear', bindKey: { win: 'Ctrl-Alt-Backspace', mac: 'Ctrl-Alt-Backspace' }, exec: handleClear },
-          ]}
-        />
-        <VStack pr={2}>
-          <Tooltip hasArrow label="Execute" placement="right-start">
-            <IconButton
-              _hover={{ bg: 'invisible', transform: 'scale(1.2)', transition: 'transform 0.2s' }}
-              boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.4)'}
-              size={'xs'}
-              rounded={'full'}
-              onClick={handleExecute}
-              aria-label={''}
-              disabled={user?._id !== props._createdBy}
-              bg={useColorModeValue('#FFFFFF', '#000000')}
-              icon={<MdPlayArrow size={'1.5em'} color={useColorModeValue('#008080', '#008080')} />}
-            />
-          </Tooltip>
-          <Tooltip hasArrow label="Clear All" placement="right-start">
-            <IconButton
-              _hover={{ bg: 'invisible', transform: 'scale(1.2)', transition: 'transform 0.2s' }}
-              boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.4)'}
-              size={'xs'}
-              rounded={'full'}
-              onClick={handleClear}
-              aria-label={''}
-              disabled={user?._id !== props._createdBy}
-              bg={useColorModeValue('#FFFFFF', '#000000')}
-              icon={<MdDelete size={'1.5em'} color={useColorModeValue('#008080', '#008080')} />}
-            />
-          </Tooltip>
-        </VStack>
-      </HStack>
-      <Flex pr={10} h={'24px'} fontSize={'16px'} color={'GrayText'} justifyContent={'right'}>
-        Ln: {ace.current?.editor.getCursorPosition() ? ace.current?.editor.getCursorPosition().row + 1 : 1}, Col:{' '}
-        {ace.current?.editor.getCursorPosition() ? ace.current?.editor.getCursorPosition().column + 1 : 1}
-      </Flex>
-    </>
   );
 }
 
@@ -198,30 +97,26 @@ const InputBox = (props: App): JSX.Element => {
  * @returns {JSX.Element}
  */
 function ToolbarComponent(props: App): JSX.Element {
+  // Access the global app state
   const s = props.data.state as AppState;
   // Update functions from the store
-  const updateState = useAppStore((state) => state.updateState);
   const update = useAppStore((state) => state.update);
-  // List of kernel names
-  // const [kernels, setKernels] = useState<{ id: string; name: string }[]>([]);
+  const updateState = useAppStore((state) => state.updateState);
   const [selected, setSelected] = useState<string>();
 
   // Update from the props
-  // useEffect(() => {
-  //   if (s.kernel) {
-  //     setSelected(s.kernel);
-  //     const name = kernels.find((k) => k.id === s.kernel)?.name || '-';
-  //     update(props._id, { description: 'SageCell> kernel ' + name });
-  //   }
-  // }, [s.kernel, kernels]);
+  useEffect(() => {
+    s.kernel ? setSelected(s.kernel) : setSelected('Select kernel');
+  }, [s.kernel]);
 
   function selectKernel(e: React.ChangeEvent<HTMLSelectElement>) {
     if (e.target.value) {
-      console.log('Selected kernel>', e.target.value);
       // save local state
       setSelected(e.target.value);
       // updae the app
       updateState(props._id, { kernel: e.target.value });
+      // update the app description
+      update(props._id, { description: `SageCell> ${e.target.value.slice(0,8)}` });
     }
   }
 
@@ -238,42 +133,6 @@ function ToolbarComponent(props: App): JSX.Element {
     downloadFile(txturl, filename);
   };
 
-  // // renders the selection options for the kernel dropdown menu
-  const RenderSelect = (kernels: string) => {
-    if (kernels) {
-      try {
-        const parsed = JSON.parse(kernels);
-        if (Object.keys(parsed).length > 0) {
-          return Object.keys(parsed).map((kernel) => (
-            <option key={kernel} value={kernel}>
-              {kernel}
-            </option>
-          ));
-        } else {
-          return null;
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    return [];
-  };
-  // if (s.kernels && kernels.length === 0) {
-  //   try {
-  //     const parsed = JSON.parse(s.kernels);
-  //     if (Object.keys(parsed).length > 0) {
-  //       setKernels(
-  //         Object.keys(parsed).map((kernel) => ({
-  //           id: kernel,
-  //           name: parsed[kernel],
-  //         }))
-  //       );
-  //     }
-  //   }
-  //   catch (e) {
-  //     console.log(e);
-  //   }
-  // }
 
   return (
     <>
@@ -300,22 +159,20 @@ function ToolbarComponent(props: App): JSX.Element {
           icon={<MdArrowDropDown />}
           onChange={selectKernel}
           variant={'outline'}
-          // value={selected ?? undefined}
+          value={selected ?? undefined}
         >
-          {RenderSelect(s.kernels)}
-          {/* {kernels.map((k) => (
-            <option key={k.id} value={k.id}>
-              {' '}
-              {k.name}{' '}
+          {s.kernels ? Object.keys(JSON.parse(s.kernels)).map((kernel) => (
+            <option key={kernel} value={kernel}>
+              {kernel}
             </option>
-          ))} */}
+          )) : null}
         </Select>
 
         <ButtonGroup isAttached size="xs" colorScheme="teal">
           <Tooltip placement="top-start" hasArrow={true} label={'Decrease Font Size'} openDelay={400}>
             <Button
-              isDisabled={s.fontSize < 0.5}
-              onClick={() => updateState(props._id, { fontSize: Math.max(0.5, s.fontSize / 1.2) })}
+              isDisabled={s.fontSize < 1}
+              onClick={() => updateState(props._id, { fontSize: Math.max(1, s.fontSize / 1.2) })}
               _hover={{ opacity: 0.7, transform: 'scaleY(1.3)' }}
             >
               <MdRemove />
@@ -323,8 +180,8 @@ function ToolbarComponent(props: App): JSX.Element {
           </Tooltip>
           <Tooltip placement="top-start" hasArrow={true} label={'Increase Font Size'} openDelay={400}>
             <Button
-              isDisabled={s.fontSize >= 2}
-              onClick={() => updateState(props._id, { fontSize: Math.min(s.fontSize * 1.2, 2) })}
+              isDisabled={s.fontSize >= 3}
+              onClick={() => updateState(props._id, { fontSize: Math.min(s.fontSize * 1.2, 3) })}
               _hover={{ opacity: 0.7, transform: 'scaleY(1.3)' }}
             >
               <MdAdd />
---
+++ webstack/libs/applications/src/lib/apps/SageCell/components/inputBox.tsx
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) SAGE3 Development Team
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ *
+ */
+import { useRef, useEffect, useState } from 'react';
+
+import {
+  HStack,
+  useColorModeValue,
+  Tooltip,
+  IconButton,
+  VStack,
+  Flex,
+} from '@chakra-ui/react';
+
+// import './components/styles.css';
+// // Date manipulation (for filename)
+// import dateFormat from 'date-fns/format';
+// UUID generation
+import { v4 as getUUID } from 'uuid';
+
+import { MdDelete, MdPlayArrow } from 'react-icons/md';
+
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-tomorrow_night_bright';
+import 'ace-builds/src-noconflict/theme-xcode';
+import 'ace-builds/src-noconflict/keybinding-vscode';
+
+// SAGE3 imports
+import { useAppStore, useUser } from '@sage3/frontend';
+import { state as AppState } from '../index';
+// import { AppWindow } from '../../components';
+import { App } from '../../../schema';
+import React from 'react';
+
+// Utility functions from SAGE3
+// import { downloadFile } from '@sage3/frontend';
+/**
+ *
+ * @param props
+ * @returns
+ */
+export const InputBox = (props: App): JSX.Element => {
+  const s = props.data.state as AppState;
+  const updateState = useAppStore((state) => state.updateState);
+  const ace = useRef<AceEditor>(null);
+  const [code, setCode] = useState<string>(s.code);
+  const { user } = useUser();
+  const [fontSize, setFontSize] = useState(s.fontSize);
+
+  const handleExecute = () => {
+    const code = ace.current?.editor?.getValue();
+    if (code) {
+      updateState(props._id, {
+        code: code,
+        output: '',
+        executeInfo: { executeFunc: 'execute', params: { uuid: getUUID() } },
+      });
+    }
+  };
+
+  const handleClear = () => {
+    updateState(props._id, {
+      code: '',
+      output: '',
+      executeInfo: { executeFunc: '', params: {} },
+    });
+    ace.current?.editor?.setValue('');
+  };
+
+  useEffect(() => {
+    if (s.code !== code) {
+      setCode(s.code);
+    }
+  }, [s.code]);
+
+  // Update from Ace Editor
+  const updateCode = (c: string) => {
+    setCode(c);
+  };
+
+  useEffect(() => {
+    // update local state from global state
+    setFontSize(s.fontSize);
+  }, [s.fontSize]);
+
+  return (
+    <>
+      <HStack>
+        <AceEditor
+          ref={ace}
+          name="ace"
+          value={code}
+          onChange={updateCode}
+          readOnly={user?._id !== props._createdBy}
+          fontSize={`${fontSize}rem`}
+          minLines={4}
+          maxLines={20}
+          placeholder="Enter code here"
+          mode={s.language}
+          theme={useColorModeValue('xcode', 'tomorrow_night_bright')}
+          editorProps={{ $blockScrolling: true }}
+          setOptions={{
+            hasCssTransforms: true,
+            showGutter: true,
+            showPrintMargin: false,
+            highlightActiveLine: true,
+            showLineNumbers: true,
+          }}
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',
+            marginTop: 15,
+            marginLeft: 15,
+            marginRight: 0,
+            marginBottom: 10,
+            padding: 0,
+            overflow: 'hidden',
+            backgroundColor: useColorModeValue('#F0F2F6', '#111111'),
+            boxShadow: '0 0 0 2px ' + useColorModeValue('rgba(0,0,0,0.4)', 'rgba(0, 128, 128, 0.5)'),
+            borderRadius: '4px',
+          }}
+          commands={[
+            { name: 'Execute', bindKey: { win: 'Shift-Enter', mac: 'Shift-Enter' }, exec: handleExecute },
+            { name: 'Clear', bindKey: { win: 'Ctrl-Alt-Backspace', mac: 'Ctrl-Alt-Backspace' }, exec: handleClear },
+          ]}
+        />
+        <VStack pr={2}>
+          <Tooltip hasArrow label="Execute" placement="right-start">
+            <IconButton
+              _hover={{ bg: 'invisible', transform: 'scale(1.2)', transition: 'transform 0.2s' }}
+              boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.6)'}
+              size={'xs'}
+              rounded={'full'}
+              onClick={handleExecute}
+              aria-label={''}
+              disabled={user?._id !== props._createdBy}
+              bg={useColorModeValue('#FFFFFF', '#000000')}
+              variant="outline"
+              icon={<MdPlayArrow size={'1.5em'} color={useColorModeValue('#008080', '#008080')} />}
+            />
+          </Tooltip>
+          <Tooltip hasArrow label="Clear All" placement="right-start">
+            <IconButton
+              _hover={{ bg: 'invisible', transform: 'scale(1.2)', transition: 'transform 0.2s' }}
+              boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.4)'}
+              size={'xs'}
+              rounded={'full'}
+              onClick={handleClear}
+              aria-label={''}
+              disabled={user?._id !== props._createdBy}
+              bg={useColorModeValue('#FFFFFF', '#000000')}
+              variant="outline"
+              icon={<MdDelete size={'1.5em'} color={useColorModeValue('#008080', '#008080')} />}
+            />
+          </Tooltip>
+        </VStack>
+      </HStack>
+      <Flex pr={10} h={'24px'} fontSize={'16px'} color={'GrayText'} justifyContent={'right'}>
+        Ln: {ace.current?.editor.getCursorPosition() ? ace.current?.editor.getCursorPosition().row + 1 : 1}, Col:{' '}
+        {ace.current?.editor.getCursorPosition() ? ace.current?.editor.getCursorPosition().column + 1 : 1}
+      </Flex>
+    </>
+  );
+};
