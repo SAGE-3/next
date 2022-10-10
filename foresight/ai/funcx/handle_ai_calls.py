104b13782127b9e384119d162a4bc1085509e6b4
Mahdi  B <b.mahdi@gmail.com>
2022-10-10 11:34:29 -1000

    Updates to config and doc

---
+++ foresight/ai/funcx/handle_ai_calls.py
@@ -0,0 +1,9 @@
+def run_ai_model_test(model_id, data):
+    return {"output": "test compeleted"}
+
+
+def handle_ai_calls(model_id, data):
+
+    pass
+
+# func_uuid = fxc.register_function(handle_ai_calls, description="Hanlde a SAGE3 app call")
