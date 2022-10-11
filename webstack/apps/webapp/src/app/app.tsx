d224c08006d161c9300152d433e65b023fec0232
Ryan Theriot <rtheriot@hawaii.edu>
2022-10-11 11:48:07 -1000

    Status check that shows if you lose connection to the server

--- webstack/apps/webapp/src/app/app.tsx
+++ webstack/apps/webapp/src/app/app.tsx
@@ -8,16 +8,14 @@
 
 import { Routes, Route, Navigate, RouteProps } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
-import {
-  PresenceProvider, theme, UserProvider,
-  useUser, AuthProvider, useAuth
-} from '@sage3/frontend';
+import { Box, Button, ChakraProvider, Text, Image, useColorModeValue } from '@chakra-ui/react';
+import { PresenceProvider, theme, UserProvider, useUser, AuthProvider, useAuth, SocketAPI, useHexColor } from '@sage3/frontend';
 
 import { LoginPage } from './pages/Login';
 import { HomePage } from './pages/Home';
 import { BoardPage } from './pages/Board';
 import { AccountPage } from './pages/Account';
+import { useEffect, useState } from 'react';
 
 /**
  * Main application component
@@ -26,51 +24,69 @@ import { AccountPage } from './pages/Account';
  * @returns
  */
 export function App() {
+  const status = useConnectStatus();
+  const color = useHexColor('red');
+
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
         <UserProvider>
           <PresenceProvider>
+            {!status ? (
+              <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
 
-            <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/login" element={<LoginPage />} />
+                <Route
+                  path="/createuser"
+                  element={
+                    <ProtectedAuthRoute>
+                      <AccountPage />
+                    </ProtectedAuthRoute>
+                  }
+                />
 
-              <Route
-                path="/createuser"
-                element={
-                  <ProtectedAuthRoute>
-                    <AccountPage />
-                  </ProtectedAuthRoute>
-                }
-              />
+                <Route
+                  path="/home"
+                  element={
+                    <ProtectedAuthRoute>
+                      <ProtectedUserRoute>
+                        <HomePage />
+                      </ProtectedUserRoute>
+                    </ProtectedAuthRoute>
+                  }
+                />
 
-              <Route
-                path="/home"
-                element={
-                  <ProtectedAuthRoute>
-                    <ProtectedUserRoute>
-                      <HomePage />
-                    </ProtectedUserRoute>
-                  </ProtectedAuthRoute>
-                }
-              />
+                <Route
+                  path="/board"
+                  element={
+                    <ProtectedAuthRoute>
+                      <ProtectedUserRoute>
+                        <BoardPage />
+                      </ProtectedUserRoute>
+                    </ProtectedAuthRoute>
+                  }
+                />
+              </Routes>
+            ) : (
+              <Box display="flex" flexDir="column" alignItems="center" textAlign={'center'} justifyContent="center" height="100%">
+                <Box width="100%" maxWidth="1200px">
+                  <Text fontSize="7xl" pb="0">
+                    SAGE3
+                  </Text>
 
-              <Route
-                path="/board"
-                element={
-                  <ProtectedAuthRoute>
-                    <ProtectedUserRoute>
-                      <BoardPage />
-                    </ProtectedUserRoute>
-                  </ProtectedAuthRoute>
-                }
-              />
-            </Routes>
+                  <Text fontSize="3xl" color={color} mb="5">
+                    Lost connection to server.
+                  </Text>
+                  <Button onClick={() => window.location.reload()} colorScheme="green" size="lg">
+                    Try to reconnect
+                  </Button>
+                </Box>
+              </Box>
+            )}
           </PresenceProvider>
         </UserProvider>
       </AuthProvider>
-
     </ChakraProvider>
   );
 }
@@ -95,4 +111,33 @@ export const ProtectedAuthRoute = (props: RouteProps): JSX.Element => {
 export const ProtectedUserRoute = (props: RouteProps): JSX.Element => {
   const { user } = useUser();
   return user ? <> {props.children}</> : <Navigate to="/createuser" replace />;
+};
+
+// Check the connection status using the API socket.
+const useConnectStatus = () => {
+  const [connected, setConnection] = useState(true);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    async function setup() {
+      const socket = await SocketAPI.getSocket();
+      setSocket(socket);
+    }
+    setup();
+  }, []);
+
+  useEffect(() => {
+    function disconnected() {
+      console.log('connection issues');
+      setConnection(false);
+    }
+    if (socket) {
+      socket.addEventListener('close', disconnected);
+    }
+    return () => {
+      if (socket) {
+        socket.removeEventListener('close', disconnected);
+      }
+    };
+  }, [socket]);
+  return connected;
 };
