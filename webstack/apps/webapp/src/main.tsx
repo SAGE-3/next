830b781653ee2c53d099ecc631c222d425b770fa
Ryan Theriot <rtheriot@hawaii.edu>
2022-10-11 14:37:17 -1000

    Fix for url route

--- webstack/apps/webapp/src/main.tsx
+++ webstack/apps/webapp/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './app/app';
 
@@ -10,9 +10,9 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
