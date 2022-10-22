7b2f9ecc7e5ab9b99a29d90c63eb33073c1cfb68
Mahdi  B <b.mahdi@gmail.com>
2022-10-21 21:53:06 -1000

    Docker running proxy.py

--- foresight/docker_info/start.sh
+++ foresight/docker_info/start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 
+pip install --upgrade pip
+
 pip install -r /foresight/requirements.txt
 
-echo "room is $1"
+# echo "room is $1" > /foresight/room.id
+
+python /foresight/docker_info/test.py
+
+# start proxy.py with $1 as room id
---
+++ foresight/docker_info/test.py
@@ -0,0 +1,5 @@
+import time
+while True:
+    time.sleep(20)
+    print("sleeping")
+    
