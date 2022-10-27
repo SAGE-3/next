c0ef53b5c8e7a023427d3af230796bc2e5e851a2
Ryan Theriot <rtheriot@hawaii.edu>
2022-10-27 12:38:26 -1000

    work done with any and mahdi

--- webstack/libs/applications/src/lib/apps/ImageViewer/data_types.ts
+++ webstack/libs/applications/src/lib/apps/ImageViewer/data_types.ts
@@ -1,19 +1,19 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
-export interface dimensions{
-      xmin: number,
-      ymin: number,
-      xmax: number,
-      ymax: number,
-  }
+export interface dimensions {
+  xmin: number;
+  ymin: number;
+  xmax: number;
+  ymax: number;
+}
 
 export const bboxType = z.record(
   z.object({
-  xmin: z.number().optional(),
-  ymin: z.number().optional(),
-  xmax: z.number().optional(),
-  ymax: z.number().optional(),
-}))
-
+    xmin: z.number(),
+    ymin: z.number(),
+    xmax: z.number(),
+    ymax: z.number(),
+  })
+);
 
 // export type bboxType2 = z.infer<typeof bboxType>
