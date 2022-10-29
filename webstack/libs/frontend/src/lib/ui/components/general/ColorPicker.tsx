92d2adc16b9ba0217f04705adeb45972aef2021f
Ryan Theriot <rtheriot@hawaii.edu>
2022-10-29 09:59:35 -1000

    Whiteboard mode, esc now only exits whiteboard mode. Hotkey is now 'shift+w', shift is probably safer than ctrl. Decreased size of buttons in the Whiteboard Panel. Colorpicker now accepts a 'size' prop.

--- webstack/libs/frontend/src/lib/ui/components/general/ColorPicker.tsx
+++ webstack/libs/frontend/src/lib/ui/components/general/ColorPicker.tsx
@@ -11,17 +11,21 @@ import { Button, ButtonGroup } from '@chakra-ui/react';
 
 import { SAGEColors, colors } from '@sage3/shared';
 import { useHexColor } from '../../../hooks';
+import { useUIStore } from '@sage3/frontend';
 
 type ColorPickerProps = {
   selectedColor: SAGEColors;
   onChange: (newColor: SAGEColors) => void;
+  size?: 'xs' | 'sm' | 'md' | 'lg';
 };
 
 export function ColorPicker(props: ColorPickerProps) {
   const [selectedColor, setSelectedColor] = useState<SAGEColors>(props.selectedColor);
+  const setWhiteboardMode = useUIStore((state) => state.setWhiteboardMode);
 
   const handleChange = (color: SAGEColors) => {
     setSelectedColor(color);
+    setWhiteboardMode(true);
     props.onChange(color);
   };
 
@@ -36,10 +40,9 @@ export function ColorPicker(props: ColorPickerProps) {
             bgColor={c}
             _hover={{ background: c, opacity: 0.7, transform: 'scaleY(1.2)' }}
             _active={{ background: c, opacity: 0.9 }}
-            size="md"
+            size={props.size ? props.size : 'md'}
             onClick={() => handleChange(color)}
             transform={selectedColor === color ? 'scaleY(1.2)' : 'scaleY(1)'}
-            width="43px"
           />
         );
       })}
