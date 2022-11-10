db16507b59f0158c28618607725bc342929c957d
renambot <renambot@gmail.com>
2022-11-10 16:28:31 -0600

    new: create a new route to access the file from an asset using a uuidv5 token as auth

---
+++ webstack/apps/homebase/src/api/routers/custom/files.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) SAGE3 Development Team
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ *
+ */
+
+/**
+ * Upload API Router.
+ * @file Upload Router
+ * @author <a href="mailto:renambot@uic.edu">Luc Renambot</a>
+ * @version 1.0.0
+ */
+
+// Express web server framework
+import * as express from 'express';
+// Node modules
+import * as path from 'path';
+import { v5 as uuidv5 } from 'uuid';
+
+// Get server configuration
+import { config } from '../../../config';
+// Asset model
+import { AssetsCollection } from '../../collections';
+
+/**
+ * App route/api express middleware.
+ * @returns {express.Router} returns the express router object
+ */
+export function FilesRouter(): express.Router {
+  const router = express.Router();
+
+  // Get one asset: GET /api/files/:id/:token
+  router.get('/:id/:token', async ({ params }, res) => {
+    // Get the asset
+    const data = await AssetsCollection.getAsset(params.id);
+    // Calculate the uuid v5 from asset id and namespace (from config)
+    const key = uuidv5(params.id, config.namespace);
+    // if it matches the passed token, send the file
+    if (data && key === params.token) {
+      res.status(200).sendFile(path.resolve(data.data.path), data.data.originalfilename);
+    } else {
+      res.status(500).send({ success: false });
+    }
+  });
+
+  return router;
+}
