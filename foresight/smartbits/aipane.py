ee455f076f21e333005f66995a7ed8757bf9ab52
Mahdi  B <b.mahdi@gmail.com>
2022-11-10 21:20:30 -1000

    generate public URL in ai_pane

--- foresight/smartbits/aipane.py
+++ foresight/smartbits/aipane.py
@@ -129,7 +129,9 @@ class AIPane(SmartBit):
             # img_name = self._s3_comm.get_asset(asset_id)["data"]["path"].split("/")[-1]
             # img_url = self._s3_comm.conf[self._s3_comm.prod_type]['web_server'] + "/api/assets/static/" + img_name
             # public_url = format_public_url(self, asset_id)
-            urls.append(asset_id)
+            # TODO  check that the asset belongs to the room/wall (ISSUE #400)
+            public_url = self._s3_comm.format_public_url(asset_id)
+            urls.append(public_url)
             print(f"Public URLs for data are: {urls}")
 
         # need to upload images to public server so they are visible to Compaas.
