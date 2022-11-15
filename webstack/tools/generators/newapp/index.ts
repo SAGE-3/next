ce7c6152fb3035dd487f5c7b49c09a59b15ff6ea
renambot <renambot@gmail.com>
2022-11-15 17:16:47 -0600

    fix: udpate app generator

--- webstack/tools/generators/newapp/index.ts
+++ webstack/tools/generators/newapp/index.ts
@@ -80,11 +80,13 @@ async function updateApps(root: string) {
   }
 
   output += `\n`;
-  output += `\n`;
+  output += `import React from 'react';\n\n`;
   output += `export const Applications = {\n`;
   for (let i in apps) {
     const it = apps[i];
-    output += `  [${it}Name]: ${it},\n`;
+    output += `  [${it}Name]: { AppComponent: React.memo(${it}.AppComponent), ToolbarComponent: ${it}.ToolbarComponent},\n`;
+    // output += `  [${it}Name]: ${it},\n`;
+    // [StickieName]: { AppComponent: React.memo(Stickie.AppComponent), ToolbarComponent: Stickie.ToolbarComponent },
   }
   output += `} as unknown as Record<string, { AppComponent: () => JSX.Element, ToolbarComponent: () => JSX.Element }>;\n`;
 
@@ -150,13 +152,14 @@ async function updateTypes(root: string) {
   output += `\n`;
   output += `\n`;
   output += `export type AppState = `;
+  output += ` {} \n`;
   for (let i in apps) {
     const it = apps[i];
     const idx = Number(i);
     if (idx == apps.length - 1) {
-      output += `${it}State;`;
+      output += ` | ${it}State;\n`;
     } else {
-      output += `${it}State | `;
+      output += ` | ${it}State\n`;
     }
   }
 
