0f57f18816e542d192c3ba8b2326d7284ac83e02
renambot <renambot@gmail.com>
2022-12-14 19:59:41 -0600

    new: notarize for arm

---
+++ webstack/clients/electron/notarize-arm.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) SAGE3 Development Team
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ *
+ */
+
+const fs = require('fs');
+var electron_notarize = require('electron-notarize');
+
+async function GO() {
+  console.log('Starting');
+
+  // Same appId in electron-builder.
+  let appId = 'app.sage3';
+  let appPath = 'SAGE3-darwin-arm64/SAGE3.app';
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    await electron_notarize.notarize({
+      tool: 'notarytool',
+      appBundleId: appId,
+      appPath: appPath,
+      // Apple ID  toto@mac.com
+      appleId: process.env.APPLE_ID || 'XXXX',
+      // an application specific password
+      appleIdPassword: process.env.APPLE_ID_PWD || 'YYYYY',
+      // number at end of `security find-identity -p basic -v`
+      teamId: process.env.APPLE_TEAM_ID || 'ZZZZZ',
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+}
+
+GO();
