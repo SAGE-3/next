0ccf76dea060271beb887e9d59b2335df8a231b5
Mahdi  B <b.mahdi@gmail.com>
2023-01-13 12:28:49 -1000

    updates to seer

--- foresight/smartbits/seer.py
+++ foresight/smartbits/seer.py
@@ -9,8 +9,10 @@
 from smartbits.smartbit import SmartBit, ExecuteInfo
 from smartbits.smartbit import TrackedBaseModel
 from pydantic import PrivateAttr
-
+from jupyterkernelproxy import JupyterKernelProxy
+import httpx
 import json
+import os
 
 class SeerState(TrackedBaseModel):
     code: str = ""
@@ -21,24 +23,89 @@ class SeerState(TrackedBaseModel):
 class Seer(SmartBit):
     # the key that is assigned to this in state is
     state: SeerState
-    # _some_private_info: dict = PrivateAttr()
+    _kernel = PrivateAttr()
+    _inferred_code = PrivateAttr()
+    _jupyter_client = PrivateAttr()
+    _token = PrivateAttr()
+
 
     def __init__(self, **kwargs):
         # THIS ALWAYS NEEDS TO HAPPEN FIRST!!
         super(Seer, self).__init__(**kwargs)
-        # self._some_private_info = {1: 2}
+        self._jupyter_client = JupyterKernelProxy()
+        self._kernel = None
+        self._inferred_code  = None
+        valid_kernel_list = [k['id'] for k in self._jupyter_client.get_kernels()]
+        if valid_kernel_list:
+            self._kernel =  valid_kernel_list[0]
+        self._token = {'Authorization': 'Bearer ' + os.getenv("TOKEN")}
 
-    def execute(self, uuid):
-        print("Responding to an execute func")
-        print("I am in seer's execute. ")
-
-        print(f"uuid is: {uuid}")
-
-        output = json.dumps({'request_id': '0eacbf77298041b0bbe7ae21d68d68b3', 'stream': {'name': 'stdout', 'text': f'{self.state.code}'}})
-        self.state.output = output
+    def handle_exec_result(self, msg):
+        print(f"\n\n\nreceived exec result: {msg}\n\n\n\n")
+        self.state.output = json.dumps(msg)
         self.state.executeInfo.executeFunc = ""
         self.state.executeInfo.params = {}
         self.send_updates()
+
+
+    def exec_python(self, uuid, code):
+        """
+        Non blocking function to execute code. The proxy has the responsibility to execute the code
+        and to call a call_back function which know how to handle the results message
+        :param uuid:
+        :param code:
+        :return:
+        """
+        command_info = {
+            "uuid": uuid,
+            "call_fn": self.handle_exec_result,
+            "code": code,
+            "kernel": self._kernel,
+            "token": ""
+        }
+
+        if self._kernel:
+            self._jupyter_client.execute(command_info)
+
+    def execute(self, uuid):
+        print("I am in seer's execute.")
+        # TODO: handle the posts as async instead
+        intent = None
+        if self.state.code.startswith("!# "):
+            payload = {"query": self.state.code[3:]}
+            headers = {'Content-Type': 'application/json'}
+            resp = httpx.post('http://127.0.0.1:5000/predict_intent', headers=headers, json=payload)
+            if resp.status_code == 200:
+                intent = resp.json()["intent"]
+
+            if intent is None or intent == "?":
+                # TODO return an error saying that we cannot find the intent.
+                msg = {"request_id": uuid,
+                       "error": {
+                           'ename': "IntentNotFound",  # Exception name, as a string
+                           'evalue': "Errot detection instruction intent",  # Exception value, as a string
+                           'traceback': [self.state.code[3:]],
+                       }
+                }
+                self.handle_exec_result(msg)
+
+            else:
+                if intent == "LOAD":
+                    resp = httpx.post('http://127.0.0.1:5000/load_dataset', headers=headers, json=payload)
+                    if resp.status_code == 200 and resp.json()["status"] == "success":
+                        code = resp.json()["code"]
+                    headers = f"storage_params = {self._token}"
+                    code = headers + "\n\n" + code
+                    print(f"\n\n\ncode is: {code}\n\n\n")
+                    self.exec_python(uuid, code)
+                elif intent.upper() == "QUERY" or intent.upper() == "VISUAL":
+                    resp = httpx.post('http://127.0.0.1:5000/handle_query', headers=headers, json=payload)
+                    if resp.status_code == 200 and resp.json()["status"] == "success":
+                        code = resp.json()["code"]
+                        print(f"\n\n\ncode is: {code}\n\n\n")
+                        self.exec_python(uuid, code)
+        else:
+            self.exec_python(uuid, self.state.code)
 
     def clean_up(self):
         pass
--- webstack/libs/applications/src/lib/apps/Seer/Seer.tsx
+++ webstack/libs/applications/src/lib/apps/Seer/Seer.tsx
@@ -156,8 +156,6 @@ const InputBox = (props: InputBoxProps): JSX.Element => {
       console.log("flipping the property");
       props.setFieldType("code");
     }
-
-
     console.log(code)
   };
 
@@ -224,173 +222,173 @@ type OutputBoxProps = {
 };
 
 const OutputBox = (props: OutputBoxProps): JSX.Element => {
-  const s = props.app.data.state as AppState;
+  // const s = props.app.data.state as AppState;
+  // const parsedJSON = JSON.parse(props.output);
+  // const fieldType = props.fieldType;
+  // // parsedJSON["stream"]["text"]
+  // return(
+  //   <div>
+  //     <div>FieldType is: {fieldType}</div>
+  //     <div>Received output is: {s.output}</div>
+  //
+  //   </div>
+  // );
+
   const parsedJSON = JSON.parse(props.output);
-  const fieldType = props.fieldType;
-  // parsedJSON["stream"]["text"]
-  return(
-    <div>
-      <div>FieldType is: {fieldType}</div>
-      <div>Received output is: {s.output}</div>
+  const s = props.app.data.state as AppState;
+  const updateState = useAppStore((state) => state.updateState);
 
-    </div>
+  if (!props.output) return <></>;
+  if (typeof props.output === 'object' && Object.keys(props.output).length === 0) return <></>;
+  return (
+    <Box
+      p={MARGIN}
+      m={MARGIN}
+      hidden={!parsedJSON ? true : false}
+      className="sc-output"
+      style={{
+        overflow: 'auto',
+        backgroundColor: useColorModeValue('#F0F2F6', '#111111'),
+        boxShadow: '0 0 0 2px ' + useColorModeValue('rgba(0,0,0,0.4)', 'rgba(0, 128, 128, 0.5)'),
+        borderRadius: '4px',
+        fontFamily: 'monospace',
+        fontSize: s.fontSize + 'px',
+        color: useColorModeValue('#000000', '#FFFFFF'),
+        whiteSpace: 'pre-wrap',
+        wordWrap: 'break-word',
+        overflowWrap: 'break-word',
+      }}
+    >
+      {!parsedJSON.execute_result || !parsedJSON.execute_result.execution_count ? null : (
+        <Text
+          fontSize="xs"
+          color="gray.500"
+          style={{
+            fontFamily: 'monospace',
+          }}
+        >
+          {`Out [${parsedJSON.execute_result.execution_count}]`}
+        </Text>
+      )}
+      {parsedJSON.request_id ? null : null}
+      {!parsedJSON.error ? null : !Array.isArray(parsedJSON.error) ? (
+        <Alert status="error">{`${parsedJSON.error.ename}: ${parsedJSON.error.evalue}`}</Alert>
+      ) : (
+        <Alert status="error" variant="left-accent">
+          <AlertIcon/>
+          <Ansi>{parsedJSON.error[parsedJSON.error.length - 1]}</Ansi>
+        </Alert>
+      )}
+
+      {!parsedJSON.stream ? null : parsedJSON.stream.name === 'stdout' ? (
+        <Text id="sc-stdout">{parsedJSON.stream.text}</Text>
+      ) : (
+        <Text id="sc-stderr" color="red">
+          {parsedJSON.stream.text}
+        </Text>
+      )}
+
+      {!parsedJSON.display_data
+        ? null
+        : Object.keys(parsedJSON.display_data).map((key) => {
+          if (key === 'data') {
+            return Object.keys(parsedJSON.display_data.data).map((key, i) => {
+              switch (key) {
+                case 'text/plain':
+                  return (
+                    <Text key={i} id="sc-stdout">
+                      {parsedJSON.display_data.data[key]}
+                    </Text>
+                  );
+                case 'text/html':
+                  return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.display_data.data[key]}}/>;
+                case 'image/png':
+                  return <Image key={i} src={`data:image/png;base64,${parsedJSON.display_data.data[key]}`}/>;
+                case 'image/jpeg':
+                  return <Image key={i} src={`data:image/jpeg;base64,${parsedJSON.display_data.data[key]}`}/>;
+                case 'image/svg+xml':
+                  return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.display_data.data[key]}}/>;
+                default:
+                  return MapJSONObject(parsedJSON.display_data[key]);
+              }
+            });
+          }
+          return null;
+        })}
+
+      {!parsedJSON.execute_result
+        ? null
+        : Object.keys(parsedJSON.execute_result).map((key) => {
+          if (key === 'data') {
+            return Object.keys(parsedJSON.execute_result.data).map((key, i) => {
+              switch (key) {
+                case 'text/plain':
+                  if (parsedJSON.execute_result.data['text/html']) return null; // don't show plain text if there is html
+                  return (
+                    <Text key={i} id="sc-stdout">
+                      {parsedJSON.execute_result.data[key]}
+                    </Text>
+                  );
+                case 'text/html':
+                  return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.execute_result.data[key]}}/>;
+                case 'image/png':
+                  return <Image key={i} src={`data:image/png;base64,${parsedJSON.execute_result.data[key]}`}/>;
+                case 'image/jpeg':
+                  return <Image key={i} src={`data:image/jpeg;base64,${parsedJSON.execute_result.data[key]}`}/>;
+                case 'image/svg+xml':
+                  return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.execute_result.data[key]}}/>;
+                default:
+                  return null;
+              }
+            });
+          }
+          return null;
+        })}
+    </Box>
   );
-
-//   const parsedJSON = JSON.parse(props.output);
-//   const s = props.app.data.state as AppState;
-//   const updateState = useAppStore((state) => state.updateState);
-//
-//   if (!props.output) return <></>;
-//   if (typeof props.output === 'object' && Object.keys(props.output).length === 0) return <></>;
-//   return (
-//     <Box
-//       p={MARGIN}
-//       m={MARGIN}
-//       hidden={!parsedJSON ? true : false}
-//       className="sc-output"
-//       style={{
-//         overflow: 'auto',
-//         backgroundColor: useColorModeValue('#F0F2F6', '#111111'),
-//         boxShadow: '0 0 0 2px ' + useColorModeValue('rgba(0,0,0,0.4)', 'rgba(0, 128, 128, 0.5)'),
-//         borderRadius: '4px',
-//         fontFamily: 'monospace',
-//         fontSize: s.fontSize + 'px',
-//         color: useColorModeValue('#000000', '#FFFFFF'),
-//         whiteSpace: 'pre-wrap',
-//         wordWrap: 'break-word',
-//         overflowWrap: 'break-word',
-//       }}
-//     >
-//       {!parsedJSON.execute_result || !parsedJSON.execute_result.execution_count ? null : (
-//         <Text
-//           fontSize="xs"
-//           color="gray.500"
-//           style={{
-//             fontFamily: 'monospace',
-//           }}
-//         >
-//           {`Out [${parsedJSON.execute_result.execution_count}]`}
-//         </Text>
-//       )}
-//       {parsedJSON.request_id ? null : null}
-//       {!parsedJSON.error ? null : !Array.isArray(parsedJSON.error) ? (
-//         <Alert status="error">{`${parsedJSON.error.ename}: ${parsedJSON.error.evalue}`}</Alert>
-//       ) : (
-//         <Alert status="error" variant="left-accent">
-//           <AlertIcon/>
-//           <Ansi>{parsedJSON.error[parsedJSON.error.length - 1]}</Ansi>
-//         </Alert>
-//       )}
-//
-//       {!parsedJSON.stream ? null : parsedJSON.stream.name === 'stdout' ? (
-//         <Text id="sc-stdout">{parsedJSON.stream.text}</Text>
-//       ) : (
-//         <Text id="sc-stderr" color="red">
-//           {parsedJSON.stream.text}
-//         </Text>
-//       )}
-//
-//       {!parsedJSON.display_data
-//         ? null
-//         : Object.keys(parsedJSON.display_data).map((key) => {
-//           if (key === 'data') {
-//             return Object.keys(parsedJSON.display_data.data).map((key, i) => {
-//               switch (key) {
-//                 case 'text/plain':
-//                   return (
-//                     <Text key={i} id="sc-stdout">
-//                       {parsedJSON.display_data.data[key]}
-//                     </Text>
-//                   );
-//                 case 'text/html':
-//                   return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.display_data.data[key]}}/>;
-//                 case 'image/png':
-//                   return <Image key={i} src={`data:image/png;base64,${parsedJSON.display_data.data[key]}`}/>;
-//                 case 'image/jpeg':
-//                   return <Image key={i} src={`data:image/jpeg;base64,${parsedJSON.display_data.data[key]}`}/>;
-//                 case 'image/svg+xml':
-//                   return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.display_data.data[key]}}/>;
-//                 default:
-//                   return MapJSONObject(parsedJSON.display_data[key]);
-//               }
-//             });
-//           }
-//           return null;
-//         })}
-//
-//       {!parsedJSON.execute_result
-//         ? null
-//         : Object.keys(parsedJSON.execute_result).map((key) => {
-//           if (key === 'data') {
-//             return Object.keys(parsedJSON.execute_result.data).map((key, i) => {
-//               switch (key) {
-//                 case 'text/plain':
-//                   if (parsedJSON.execute_result.data['text/html']) return null; // don't show plain text if there is html
-//                   return (
-//                     <Text key={i} id="sc-stdout">
-//                       {parsedJSON.execute_result.data[key]}
-//                     </Text>
-//                   );
-//                 case 'text/html':
-//                   return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.execute_result.data[key]}}/>;
-//                 case 'image/png':
-//                   return <Image key={i} src={`data:image/png;base64,${parsedJSON.execute_result.data[key]}`}/>;
-//                 case 'image/jpeg':
-//                   return <Image key={i} src={`data:image/jpeg;base64,${parsedJSON.execute_result.data[key]}`}/>;
-//                 case 'image/svg+xml':
-//                   return <div key={i} dangerouslySetInnerHTML={{__html: parsedJSON.execute_result.data[key]}}/>;
-//                 default:
-//                   return null;
-//               }
-//             });
-//           }
-//           return null;
-//         })}
-//     </Box>
-//   );
 };
 
-// const MapJSONObject = (obj: any): JSX.Element => {
-//   if (!obj) return <></>;
-//   if (typeof obj === 'object' && Object.keys(obj).length === 0) return <></>;
-//   return (
-//     <Box
-//       pl={2}
-//       ml={2}
-//       bg={useColorModeValue('#FFFFFF', '#000000')}
-//       boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.6)'}
-//       rounded={'md'}
-//       fontSize={'16px'}
-//       color={useColorModeValue('#000000', '#FFFFFF')}
-//     >
-//       {typeof obj === 'object'
-//         ? Object.keys(obj).map((key) => {
-//           if (typeof obj[key] === 'object') {
-//             return (
-//               <Box key={key}>
-//                 <Box as="span" fontWeight="bold">
-//                   {key}:
-//                 </Box>
-//                 <Box as="span" ml={2}>
-//                   {MapJSONObject(obj[key])}
-//                 </Box>
-//               </Box>
-//             );
-//           } else {
-//             return (
-//               <Box key={key}>
-//                 <Box as="span" fontWeight="bold">
-//                   {key}:
-//                 </Box>
-//                 <Box as="span" ml={2}>
-//                   {obj[key]}
-//                 </Box>
-//               </Box>
-//             );
-//           }
-//         })
-//         : null}
-//     </Box>
-//   );
-// };
+const MapJSONObject = (obj: any): JSX.Element => {
+  if (!obj) return <></>;
+  if (typeof obj === 'object' && Object.keys(obj).length === 0) return <></>;
+  return (
+    <Box
+      pl={2}
+      ml={2}
+      bg={useColorModeValue('#FFFFFF', '#000000')}
+      boxShadow={'2px 2px 4px rgba(0, 0, 0, 0.6)'}
+      rounded={'md'}
+      fontSize={'16px'}
+      color={useColorModeValue('#000000', '#FFFFFF')}
+    >
+      {typeof obj === 'object'
+        ? Object.keys(obj).map((key) => {
+          if (typeof obj[key] === 'object') {
+            return (
+              <Box key={key}>
+                <Box as="span" fontWeight="bold">
+                  {key}:
+                </Box>
+                <Box as="span" ml={2}>
+                  {MapJSONObject(obj[key])}
+                </Box>
+              </Box>
+            );
+          } else {
+            return (
+              <Box key={key}>
+                <Box as="span" fontWeight="bold">
+                  {key}:
+                </Box>
+                <Box as="span" ml={2}>
+                  {obj[key]}
+                </Box>
+              </Box>
+            );
+          }
+        })
+        : null}
+    </Box>
+  );
+};
 
