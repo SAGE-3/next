da6eb0bc760039f7766ee225b93559f28da42ec4
Ryan Theriot <rtheriot@hawaii.edu>
2023-01-13 15:03:12 -1000

    First pass at follow feature

--- webstack/apps/webapp/src/app/components/Board/UI/Panels/Users/UserAvatarGroup.tsx
+++ webstack/apps/webapp/src/app/components/Board/UI/Panels/Users/UserAvatarGroup.tsx
@@ -6,15 +6,19 @@
  * the file LICENSE, distributed as part of this software.
  */
 
-import { Avatar, Box, Tooltip, AvatarGroup, GridItem, Grid } from '@chakra-ui/react';
+import { Avatar, Tooltip, GridItem, Grid, Menu, MenuButton, MenuItem, MenuList, MenuGroup } from '@chakra-ui/react';
 
 import { usePresenceStore, useUser, useUsersStore, initials, useHexColor, useUIStore } from '@sage3/frontend';
+import { Presence, User } from '@sage3/shared/types';
 import { useEffect, useState } from 'react';
+
+import { MdRemoveRedEye } from 'react-icons/md';
 
 type AvatarGroupProps = {
   boardId: string;
 };
 
+type UserAndPresence = { presence: Presence; user: User };
 export function UserAvatarGroup(props: AvatarGroupProps) {
   // Get current user
   const { user } = useUser();
@@ -22,6 +26,11 @@ export function UserAvatarGroup(props: AvatarGroupProps) {
   const setBoardPosition = useUIStore((state) => state.setBoardPosition);
   const setScale = useUIStore((state) => state.setScale);
   const scale = useUIStore((state) => state.scale);
+  const followColor = useHexColor('red.300');
+  const borderColor = useHexColor('');
+
+  // User you are currently following
+  const [following, setFollowing] = useState<UserAndPresence | null>(null);
 
   // Get all users
   const users = useUsersStore((state) => state.users);
@@ -31,17 +40,33 @@ export function UserAvatarGroup(props: AvatarGroupProps) {
   presences = presences.filter((el) => el.data.boardId === props.boardId && el._id !== user?._id);
 
   // Combine users and presences
-  const userPresence = presences.map((el) => {
-    const user = users.find((user) => user._id === el._id);
-    if (user) {
+  const userPresence: UserAndPresence[] = presences
+    .filter((p) => users.find((u) => u._id === p._id))
+    .map((p) => {
       return {
-        presence: el,
-        user: user,
+        presence: p,
+        user: users.find((u) => u._id === p._id) as User,
       };
+    });
+
+  useEffect(() => {
+    // If not following anyone, exit
+    if (!following) return;
+    const user = userPresence.find((el) => el.presence._id === following.presence._id);
+    // Check if the you are following is still on this board
+    if (!user) {
+      setFollowing(null);
     } else {
-      return null;
+      // Set you position to their current position
+      goToViewport(user);
     }
-  });
+  }, [userPresence, following]);
+
+  useEffect(() => {
+    return () => {
+      setFollowing(null);
+    };
+  }, []);
 
   // Sort walls to top
   userPresence.sort((a, b) => {
@@ -50,70 +75,106 @@ export function UserAvatarGroup(props: AvatarGroupProps) {
     return aType - bType;
   });
 
-  function handleAvatarClick(user: typeof userPresence[0]) {
+  function goToCursor(user: UserAndPresence) {
     if (user) {
       const type = user.user.data.userType;
-      if (type === 'client') {
-        const cx = -user.presence.data.cursor.x;
-        const cy = -user.presence.data.cursor.y;
-        const wx = window.innerWidth / scale / 2;
-        const wy = window.innerHeight / scale / 2;
 
-        setBoardPosition({ x: cx + wx, y: cy + wy });
-      } else if (type == 'wall') {
-        const vx = -user.presence.data.viewport.position.x;
-        const vy = -user.presence.data.viewport.position.y;
-        const vw = -user.presence.data.viewport.size.width;
-        const vh = -user.presence.data.viewport.size.height;
-        const vcx = vx + vw / 2;
-        const vcy = vy + vh / 2;
+      const cx = -user.presence.data.cursor.x;
+      const cy = -user.presence.data.cursor.y;
+      const wx = window.innerWidth / scale / 2;
+      const wy = window.innerHeight / scale / 2;
 
-        const ww = window.innerWidth;
-        const wh = window.innerHeight;
+      setBoardPosition({ x: cx + wx, y: cy + wy });
+    }
+  }
 
-        const s = Math.min(ww / -vw, wh / -vh);
-        const cx = vcx + ww / s / 2;
-        const cy = vcy + wh / s / 2;
+  function goToViewport(user: UserAndPresence) {
+    if (user) {
+      const type = user.user.data.userType;
+      const vx = -user.presence.data.viewport.position.x;
+      const vy = -user.presence.data.viewport.position.y;
+      const vw = -user.presence.data.viewport.size.width;
+      const vh = -user.presence.data.viewport.size.height;
+      const vcx = vx + vw / 2;
+      const vcy = vy + vh / 2;
 
-        setScale(s);
-        setBoardPosition({ x: cx, y: cy });
-      }
+      const ww = window.innerWidth;
+      const wh = window.innerHeight;
+
+      const s = Math.min(ww / -vw, wh / -vh);
+      const cx = vcx + ww / s / 2;
+      const cy = vcy + wh / s / 2;
+
+      setScale(s);
+      setBoardPosition({ x: cx, y: cy });
+    }
+  }
+
+  function followUser(user: UserAndPresence) {
+    if (user.user._id === following?.user._id) {
+      setFollowing(null);
+    } else {
+      setFollowing(user);
     }
   }
 
   return (
-    <Grid templateColumns="repeat(10, 0fr)" gap={2}>
-      {userPresence.map((el) => {
-        if (el == null) return;
-        const color = useHexColor(el.user.data.color);
-        const isWall = el.user.data.userType === 'wall';
-        return (
-          <GridItem w="100%" h="10" key={'userpanel-' + el.user._id}>
-            <Tooltip
-              key={el.presence.data.userId}
-              aria-label="username"
-              hasArrow={true}
-              placement="top"
-              label={el.user.data.name}
-              shouldWrapChildren={true}
-            >
-              <Avatar
-                name={el.user.data.name}
-                getInitials={initials}
-                backgroundColor={color || 'orange'}
-                size="sm"
-                color="white"
-                showBorder={true}
-                borderRadius={isWall ? '0%' : '100%'}
-                borderWidth={'1px'}
-                borderColor="whiteAlpha.600"
-                onClick={() => handleAvatarClick(el)}
-                cursor="pointer"
-              />
-            </Tooltip>
-          </GridItem>
-        );
-      })}
-    </Grid>
+    <>
+      <Grid templateColumns="repeat(10, 0fr)" gap={2}>
+        {userPresence.map((el) => {
+          if (el == null) return;
+          const color = useHexColor(el.user.data.color);
+          const isWall = el.user.data.userType === 'wall';
+          const followingThisUser = following?.user._id === el.user._id;
+          return (
+            <GridItem w="100%" h="10" key={'userpanel-' + el.user._id}>
+              <Tooltip
+                key={el.presence.data.userId}
+                aria-label="username"
+                hasArrow={true}
+                placement="top"
+                label={el.user.data.name}
+                shouldWrapChildren={true}
+              >
+                <Menu>
+                  <MenuButton
+                    as={Avatar}
+                    name={' '}
+                    backgroundColor={color || 'orange'}
+                    size="sm"
+                    color="white"
+                    showBorder={true}
+                    borderRadius={isWall ? '0%' : '100%'}
+                    borderColor="transparent"
+                    cursor="pointer"
+                    textAlign="center"
+                    fontWeight="bold"
+                    fontSize="14px"
+                  >
+                    {followingThisUser ? (
+                      <MdRemoveRedEye
+                        style={{
+                          fontSize: '18px',
+                          transform: 'translateX(4px)',
+                        }}
+                      />
+                    ) : (
+                      initials(el.user.data.name)
+                    )}
+                  </MenuButton>
+                  <MenuList>
+                    <MenuGroup title={el.user.data.name}>
+                      <MenuItem onClick={() => goToCursor(el)}>Go to Cursor</MenuItem>
+                      <MenuItem onClick={() => goToViewport(el)}>Match Viewport</MenuItem>
+                      <MenuItem onClick={() => followUser(el)}>{followingThisUser ? 'Unfollow' : 'Follow'}</MenuItem>
+                    </MenuGroup>
+                  </MenuList>
+                </Menu>
+              </Tooltip>
+            </GridItem>
+          );
+        })}
+      </Grid>
+    </>
   );
 }
