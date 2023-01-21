902f73d8ad3c1da6290b81fac9d69510b4874ce9
Ryan Theriot <rtheriot@hawaii.edu>
2023-01-20 15:28:03 -1000

    Saving changes

--- foresight/_docker_scripts/start-proxy.sh
+++ foresight/_docker_scripts/start-proxy.sh
@@ -4,16 +4,15 @@
 # check the room id is provided
 # ROOM_ID is exported by the compose up script as part of the .env file
 
-if [ -z "$ROOM_ID" ]; then
-    echo "Must include the room id to run a proxy cotainer" 1>&2
-    exit 1
-fi
+# if [ -z "$ROOM_ID" ]; then
+#     echo "Must include the room id to run a proxy cotainer" 1>&2
+#     exit 1
+# fi
 
 
 export PIP_ROOT_USER_ACTION=ignore
 pip install --upgrade pip
 pip install -r /foresight/requirements.txt
-echo ${ROOM_ID} > /foresight/room.id
 python /foresight/_docker_scripts/test.py
 
 # start proxy.py $1 
