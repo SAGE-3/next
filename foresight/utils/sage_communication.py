f6e63b5ec2aebcd2fa5b659d78c64ce613479620
Mahdi  B <b.mahdi@gmail.com>
2023-01-25 12:58:30 -1000

    Updating logging.

--- foresight/utils/sage_communication.py
+++ foresight/utils/sage_communication.py
@@ -7,11 +7,12 @@
 # -----------------------------------------------------------------------------
 
 import uuid
-
 import httpx
 import os
-
 from utils.sage_websocket import SageWebsocket
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class Borg:
@@ -56,9 +57,7 @@ class SageCommunication(Borg):
         :param data: data
         :return:
         """
-        print("app update data is")
-        print(data)
-        print("------------")
+        logger.debug(f"sendign following update: {data}")
         r = self.httpx_client.put(self.conf[self.prod_type]['web_server'] + self.routes["send_update"].format(app_id),
                                   headers=self.__headers,
                                   json=data)
@@ -128,11 +127,11 @@ class SageCommunication(Borg):
         :return: dict representing the
         """
         url = self.conf[self.prod_type]['web_server'] + self.routes["get_apps"]
-        if app_id:
+        if app_id is not None:
             url += app_id
         r = self.httpx_client.get(url, headers=self.__headers)
         json_data = r.json()
-        print(f"json_data result is {json_data}")
+        logger.debug(f"received apps info: {json_data}")
         data = json_data['data']
         if r.is_success:
             if room_id is not None:
