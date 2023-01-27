4dd0a1ceadfbeca48428b6e6b4d255b0989797bb
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2023-01-26 15:45:57 -1000

    moved leaflet into its own component wrapper

--- webstack/libs/applications/src/lib/apps/HCDP/HCDP.tsx
+++ webstack/libs/applications/src/lib/apps/HCDP/HCDP.tsx
@@ -6,12 +6,11 @@
  * the file LICENSE, distributed as part of this software.
  */
 
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState } from 'react';
 import { HStack, InputGroup, Input, ButtonGroup, Tooltip, Button, useColorModeValue, propNames } from '@chakra-ui/react';
 
 import { useAppStore, useAssetStore, useHexColor, useHotkeys, useUIStore } from '@sage3/frontend';
-import { Asset } from '@sage3/shared/types';
-import { App, AppSchema } from '../../schema';
+import { App } from '../../schema';
 
 import { state as AppState } from './index';
 import { AppWindow } from '../../components';
@@ -20,6 +19,7 @@ import { AppWindow } from '../../components';
 import * as Leaflet from 'leaflet';
 import * as esriLeafletGeocoder from 'esri-leaflet-geocoder';
 import { MapContainer, TileLayer, LayersControl, Marker, Popup } from 'react-leaflet';
+import LeafletWrapper from './LeafletWrapper';
 
 // Import the CSS style sheet from the node_modules folder
 import 'leaflet/dist/leaflet.css';
@@ -63,244 +63,12 @@ const stationData = [
 // Leaflet App
 function AppComponent(props: App): JSX.Element {
   const s = props.data.state as AppState;
-  const updateState = useAppStore((state) => state.updateState);
-  const update = useAppStore((state) => state.update);
-
-  const selectedId = useUIStore((state) => state.selectedAppId);
-  const selected = props._id === selectedId;
-
-  // The map: any, I kown, should be Leaflet.Map but don't work
-  const [map, setMap] = useState<any>();
-  // Keep an handle of the overlay, to show/hide
-  const overlayLayer = useRef<Leaflet.GeoJSON>();
-  // Assets store
-  const assets = useAssetStore((state) => state.assets);
-  const [file, setFile] = useState<Asset>();
-  const saveMap = useStore((state: any) => state.saveMap);
 
   const createApp = useAppStore((state) => state.create);
-
-  // Convert ID to asset
-  useEffect(() => {
-    const myasset = assets.find((a) => a._id === s.assetid);
-    if (myasset) {
-      setFile(myasset);
-      // Update the app title
-      update(props._id, { title: myasset?.data.originalfilename });
-    }
-  }, [s.assetid, assets]);
-
-  // Convert asset to URL
-  useEffect(() => {
-    if (file) {
-      const newURL = getStaticAssetUrl(file.data.file);
-
-      // Fetch the data from local server
-      if (newURL) {
-        fetch(newURL, {
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-        })
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (gson) {
-            // Create special marker
-            const markerOptions = {
-              radius: 5,
-              fillColor: '#ff7800',
-              color: '#000',
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.4,
-            };
-            // Add the data into a layer
-            overlayLayer.current = Leaflet.geoJSON(gson, {
-              // draw circles on the map instead  of markers
-              pointToLayer: function (feature, latlng) {
-                return Leaflet.circleMarker(latlng, markerOptions);
-              },
-            }).addTo(map);
-            // Fit view to new data
-            const bounds = overlayLayer.current.getBounds();
-            const center = bounds.getCenter();
-            map.setView(center);
-            const value: [number, number] = [center.lat, center.lng];
-            updateState(props._id, { location: value });
-            map.fitBounds(bounds);
-            // Add a new control (don't know how to add it to main control, need the list of layers)
-            Leaflet.control.layers({}, { 'Data layer': overlayLayer.current }).addTo(map);
-          });
-      }
-    }
-  }, [file]);
-
-  useEffect(() => {
-    if (!map) return;
-    // put in the  zustand store
-    saveMap(props._id, map);
-    // Update the default markers
-    Leaflet.Icon.Default.mergeOptions({
-      iconRetinaUrl: 'assets/marker-icon-2x.png',
-      iconUrl: 'assets/marker-icon.png',
-      shadowUrl: 'assets/marker-shadow.png',
-      shadowSize: [0, 0],
-      iconSize: [24, 40],
-    });
-  }, [map, s.overlay]);
-
-  // Add or remove the overlay layer with the geojson data
-  useEffect(() => {
-    if (s.overlay && map) {
-      overlayLayer.current?.addTo(map);
-    } else {
-      if (map) overlayLayer.current?.removeFrom(map);
-    }
-  }, [map, s.overlay]);
-
-  // Window resize
-  useEffect(() => {
-    if (map) {
-      // Using timeout here due to invalidateSize seems to be ahead of the parent div's size being set
-      // 250ms seems to fix the issue when resizing
-      // what?
-      setTimeout(() => {
-        if (map) {
-          map.invalidateSize();
-          if (map.getCenter().lat !== s.location[0] || map.getCenter().lng !== s.location[1]) {
-            const loc = new Leaflet.LatLng(map.getCenter().lat, map.getCenter().lng);
-            map.setView(loc);
-          }
-        }
-      }, 250);
-    }
-  }, [props.data.size.width, props.data.size.height, map]);
-
-  // Location sync
-  const onMove = useCallback(() => {
-    if (map) {
-      const value: [number, number] = [map.getCenter().lat, map.getCenter().lng];
-      updateState(props._id, { location: value });
-    }
-  }, [map, s.location]);
-
-  // Drag events
-  useEffect(() => {
-    if (map) {
-      map.on('dragend', onMove);
-    }
-    return () => {
-      if (map) {
-        map.off('dragend', onMove);
-      }
-    };
-  }, [map, onMove]);
-
-  // Synchronize the view
-  useEffect(() => {
-    if (map) {
-      const loc = new Leaflet.LatLng(s.location[0], s.location[1]);
-      map.setView(loc);
-    }
-  }, [s.location, map]);
-
-  // BaseLayer sync
-  const onBaseLayerChange = useCallback(
-    (e: any) => {
-      const value = e.name;
-      updateState(props._id, { baseLayer: value });
-    },
-    [s.baseLayer]
-  );
-
-  useEffect(() => {
-    if (map) {
-      map.on('baselayerchange', onBaseLayerChange);
-    }
-    return () => {
-      if (map) {
-        map.off('baselayerchange', onBaseLayerChange);
-      }
-    };
-  }, [map, onBaseLayerChange]);
-
-  // Overlay layer control
-  const onOverlayAdd = useCallback(() => {
-    updateState(props._id, { overlay: true });
-  }, [s.overlay]);
-
-  const onOverlayRemove = useCallback(() => {
-    updateState(props._id, { overlay: false });
-  }, [s.overlay]);
-
-  useEffect(() => {
-    if (map) {
-      map.on('overlayadd', onOverlayAdd);
-      map.on('overlayremove', onOverlayRemove);
-    }
-    return () => {
-      if (map) {
-        map.off('overlayadd', onOverlayAdd);
-        map.off('overlayremove', onOverlayRemove);
-      }
-    };
-  }, [map]);
-
-  useEffect(() => {
-    if (map) {
-      map.invalidateSize();
-    }
-  }, [map, s.baseLayer]);
-
-  useEffect(() => {
-    if (map) {
-      map.setZoom(s.zoom);
-    }
-  }, [map, s.zoom]);
-
-  // Zoom in on the map
-  const incZoom = () => {
-    if (selected) {
-      const zoom = s.zoom + 1;
-      const limitZoom = Math.min(zoom, maxZoom);
-      updateState(props._id, { zoom: limitZoom });
-    }
-  };
-
-  // Zoom out on the map
-  const decZoom = () => {
-    if (selected) {
-      const zoom = s.zoom - 1;
-      const limitZoom = Math.max(zoom, minZoom);
-      updateState(props._id, { zoom: limitZoom });
-    }
-  };
-
-  // + and - keyboard keys for zooming
-  useHotkeys('=', incZoom, { dependencies: [selected, s.zoom] });
-  useHotkeys('-', decZoom, { dependencies: [selected, s.zoom] });
 
   const createCharts = (data: { lat: number; lon: number; name: string }) => {
     const createAppAtPos = (whereToCreateApp: string, traces: any[]): void => {
       let appPos = { x: 0, y: 0, z: 0 };
-      // switch (whereToCreateApp) {
-      //   case 'top':
-      //     appPos = { x: props.data.position.x, y: props.data.position.y - props.data.size.height, z: 0 };
-      //     break;
-      //   case 'right':
-      //     appPos = { x: props.data.position.x + props.data.size.height, y: props.data.position.y, z: 0 };
-      //     break;
-      //   case 'left':
-      //     appPos = { x: props.data.position.x - props.data.size.height, y: props.data.position.y, z: 0 };
-      //     break;
-      //   case 'bottom':
-      //     appPos = { x: props.data.position.x, y: props.data.position.y + props.data.size.height, z: 0 };
-      //     break;
-      //   default:
-      //     appPos = { x: 0, y: 0, z: 0 };
-      // }
       let newLineMultiplier = 0;
 
       for (let i = 0; i < traces.length; i++) {
@@ -329,8 +97,6 @@ function AppComponent(props: App): JSX.Element {
       }
     };
 
-    // createAppAtPos('top');
-    // createAppAtPos('left');
     let climateData: any[] = [];
 
     fetch(
@@ -359,60 +125,36 @@ function AppComponent(props: App): JSX.Element {
         }
         console.log(traces);
         createAppAtPos('right', traces);
-
-        // createAppAtPos('right', traces);
       });
     });
-    // createAppAtPos('bottom');
   };
 
   return (
-    <AppWindow app={props}>
-      <MapContainer
-        center={[s.location[0], s.location[1]]}
-        zoom={s.zoom}
-        scrollWheelZoom={false}
-        doubleClickZoom={false}
-        keyboard={false}
-        preferCanvas={true}
-        style={{ height: `100%`, width: `100%` }}
-        ref={setMap}
-        attributionControl={false}
-        zoomControl={false}
-      >
-        <LayersControl>
-          <LayersControl.BaseLayer checked={s.baseLayer === 'OpenStreetMap'} name="OpenStreetMap">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {stationData.map((data, index) => {
-              return (
-                <Marker
-                  // eventHandlers={{
-                  //   click: (e) => {
-                  //     createCharts(data);
-                  //   },
-                  // }}
-                  key={index}
-                  position={[data.lat, data.lon]}
-                >
-                  <Popup>
-                    <Button onClick={() => createCharts(data)}>All Data</Button>
-                  </Popup>
-                </Marker>
-              );
-            })}
-          </LayersControl.BaseLayer>
-          {/* <LayersControl.BaseLayer checked={s.baseLayer === 'World Imagery'} name="World Imagery">
-            <TileLayer
-              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            />
-          </LayersControl.BaseLayer> */}
-        </LayersControl>
-      </MapContainer>
-    </AppWindow>
+    <LeafletWrapper {...props}>
+      <LayersControl.BaseLayer checked={s.baseLayer === 'OpenStreetMap'} name="OpenStreetMap">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {stationData.map((data, index) => {
+          return (
+            <Marker
+              // eventHandlers={{
+              //   click: (e) => {
+              //     createCharts(data);
+              //   },
+              // }}
+              key={index}
+              position={[data.lat, data.lon]}
+            >
+              <Popup>
+                <Button onClick={() => createCharts(data)}>All Data</Button>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </LayersControl.BaseLayer>
+    </LeafletWrapper>
   );
 }
 
@@ -526,4 +268,20 @@ function ToolbarComponent(props: App): JSX.Element {
   );
 }
 
+// switch (whereToCreateApp) {
+//   case 'top':
+//     appPos = { x: props.data.position.x, y: props.data.position.y - props.data.size.height, z: 0 };
+//     break;
+//   case 'right':
+//     appPos = { x: props.data.position.x + props.data.size.height, y: props.data.position.y, z: 0 };
+//     break;
+//   case 'left':
+//     appPos = { x: props.data.position.x - props.data.size.height, y: props.data.position.y, z: 0 };
+//     break;
+//   case 'bottom':
+//     appPos = { x: props.data.position.x, y: props.data.position.y + props.data.size.height, z: 0 };
+//     break;
+//   default:
+//     appPos = { x: 0, y: 0, z: 0 };
+// }
 export default { AppComponent, ToolbarComponent };
