ad0383d558acbeb8225348fda920286e329d7ed5
renambot <renambot@gmail.com>
2023-02-06 15:36:17 -0600

    new: headers

--- webstack/clients/electron/src/bookmarkstore.js
+++ webstack/clients/electron/src/bookmarkstore.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) SAGE3 Development Team 2023. All Rights Reserved
+ * University of Hawaii, University of Illinois Chicago, Virginia Tech
+ *
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
+ */
+
 // Persistent storage for electron app: used for window position
 const Store = require('electron-store');
 const uuid = require('uuid');
