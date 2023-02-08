696495472994cf13b2f367edc1652d40f1ade6c8
renambot <renambot@gmail.com>
2023-02-08 15:42:28 -0600

    new: change url parsing in wsRouter to using node URL libs and getting the parameters.

--- webstack/libs/backend/src/lib/generics/SAGEWSRouter.ts
+++ webstack/libs/backend/src/lib/generics/SAGEWSRouter.ts
@@ -15,6 +15,8 @@ import { SubscriptionCache } from '../utils/subscription-cache';
 import { SAGE3Collection } from './SAGECollection';
 import { checkPermissionsWS, AuthSubject } from './permissions';
 
+import { URL } from 'node:url';
+
 export async function sageWSRouter<T extends SBJSON>(
   collection: SAGE3Collection<T>,
   socket: WebSocket,
@@ -22,6 +24,7 @@ export async function sageWSRouter<T extends SBJSON>(
   user: SBAuthSchema,
   cache: SubscriptionCache
 ): Promise<void> {
+  // path to the current collection
   const path = '/api/' + collection.name.toLowerCase();
 
   //  Check permissions on collections
@@ -29,6 +32,14 @@ export async function sageWSRouter<T extends SBJSON>(
     socket.send(JSON.stringify({ id: message.id, success: false, message: 'Not Allowed' }));
     return;
   }
+
+  // Create a URL object to parse the route:
+  // second argument required because the route is a relative URL but otherwise meaningless
+  const socket_url = new URL(message.route, 'ws://localhost');
+  const route = message.route;
+  // get all the parameters and their values into an array, instead of a Map
+  const params = Array.from(socket_url.searchParams);
+  const numParams = params.length;
 
   switch (message.method) {
     case 'POST': {
@@ -46,14 +57,18 @@ export async function sageWSRouter<T extends SBJSON>(
     }
     case 'GET': {
       // GET: Get all the docs.
-      if (message.route === path) {
+      if (route === path) {
         const docs = await collection.getAll();
         if (docs) socket.send(JSON.stringify({ id: message.id, success: true, data: docs }));
-        else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to get docs.' }));
+        else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to get docs' }));
       }
       // GET: Get one doc.
-      else if (message.route.startsWith(path + '/')) {
-        const id = message.route.split('/').at(-1) as string;
+      else if (route.startsWith(path + '/')) {
+        const id = getIdFromRoute(route);
+        if (!id) {
+          socket.send(JSON.stringify({ id: message.id, success: false, message: 'No id provided' }));
+          return;
+        }
         const doc = await collection.get(id);
         if (doc) socket.send(JSON.stringify({ id: message.id, success: true, data: doc }));
         else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to get doc.' }));
@@ -62,37 +77,44 @@ export async function sageWSRouter<T extends SBJSON>(
     }
     // PUT: Update one doc.
     case 'PUT': {
-      const id = message.route.split('/').at(-1) as string;
+      const id = getIdFromRoute(route);
+      if (!id) {
+        socket.send(JSON.stringify({ id: message.id, success: false, message: 'No id provided' }));
+        return;
+      }
       const body = message.body as SBDocumentUpdate<T>;
       const update = await collection.update(id, user.id, body);
       if (update) socket.send(JSON.stringify({ id: message.id, success: true }));
-      else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to update doc.' }));
+      else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to update doc' }));
       break;
     }
     // DELETE: Delete one doc.
     case 'DELETE': {
-      const id = message.route.split('/').at(-1) as string;
+      const id = getIdFromRoute(route);
+      if (!id) {
+        socket.send(JSON.stringify({ id: message.id, success: false, message: 'No id provided' }));
+        return;
+      }
       const del = await collection.delete(id);
       if (del) socket.send(JSON.stringify({ id: message.id, success: true }));
-      else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to delete doc.' }));
+      else socket.send(JSON.stringify({ id: message.id, success: false, message: 'Failed to delete doc' }));
       break;
     }
     case 'SUB': {
       // Subscribe to all docs
-      if (message.route === path) {
+      if (route === path) {
         const sub = await collection.subscribeAll((doc) => {
           const msg = { id: message.id, event: doc };
           socket.send(JSON.stringify(msg));
         });
         if (sub) cache.add(message.id, [sub]);
-      } else if (message.route.includes('?') && message.route.includes('=')) {
-        const parsedQuery = message.route.split('?')[1].split('=');
-        if (parsedQuery.length != 2) {
-          socket.send(JSON.stringify({ id: message.id, success: false, message: 'Improper query format.' }));
+      } else if (numParams > 0) {
+        if (numParams != 1) {
+          socket.send(JSON.stringify({ id: message.id, success: false, message: 'Improper query format' }));
           return;
         } else {
-          const prop = parsedQuery[0];
-          const query = parsedQuery[1];
+          const prop = params[0][0]; // first key
+          const query = params[0][1]; // first value
           const sub = await collection.subscribeByQuery(prop, query, (doc) => {
             const msg = { id: message.id, event: doc };
             socket.send(JSON.stringify(msg));
@@ -101,8 +123,8 @@ export async function sageWSRouter<T extends SBJSON>(
         }
       }
       // Subscribe to one doc
-      else if (message.route.startsWith(path + '/')) {
-        const id = message.route.split('/').at(-1);
+      else if (route.startsWith(path + '/')) {
+        const id = getIdFromRoute(route);
         if (!id) {
           socket.send(JSON.stringify({ id: message.id, success: false, message: 'No id provided' }));
           return;
@@ -121,7 +143,17 @@ export async function sageWSRouter<T extends SBJSON>(
       break;
     }
     default: {
-      socket.send(JSON.stringify({ id: message.id, success: false, message: 'Invalid method.' }));
+      socket.send(JSON.stringify({ id: message.id, success: false, message: 'Invalid method' }));
     }
   }
+}
+
+/*
+ * Get the id from the end of the route
+ *
+ * @param {string} route
+ * @returns {(string | undefined)}
+ * */
+function getIdFromRoute(route: string): string | undefined {
+  return route.split('/').at(-1);
 }
