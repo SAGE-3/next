8926ea70334e7f1f1c077180ee8ba427951eaeca
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2023-03-02 17:48:55 -1000

    Redo pinboard to use more local states

--- webstack/libs/applications/src/lib/apps/PinBoard/PinBoard.tsx
+++ webstack/libs/applications/src/lib/apps/PinBoard/PinBoard.tsx
@@ -1,22 +1,32 @@
 /**
- * Copyright (c) SAGE3 Development Team 2022. All Rights Reserved
- * University of Hawaii, University of Illinois Chicago, Virginia Tech
+ * Copyright (c) SAGE3 Development Team
  *
  * Distributed under the terms of the SAGE3 License.  The full license is in
  * the file LICENSE, distributed as part of this software.
+ *
  */
 
 import {
   Box,
+  Button, ButtonGroup,
+  CloseButton,
+  Icon,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Popover,
+  PopoverBody,
+  PopoverContent,
   PopoverTrigger,
-  useColorModeValue,
+  Portal,
+  Tooltip,
+  VisuallyHidden,
 } from '@chakra-ui/react';
 
-import {HiMail} from 'react-icons/hi';
 
-import {useAppStore, useUIStore, useHexColor} from '@sage3/frontend';
+import {useAppStore, useUIStore} from '@sage3/frontend';
 
 import {App} from '../../schema';
 import {state as AppState} from './index';
@@ -27,44 +37,33 @@ import './styles.css';
 import {useEffect, useState, useRef} from 'react';
 
 import {v4 as getUUID} from 'uuid';
-
-type UpdateFunc = (id: string, state: Partial<AppState>) => Promise<void>;
+import {Position, Size} from "@sage3/shared/types";
 
 function AppComponent(props: App): JSX.Element {
   const s = props.data.state as AppState;
   const updateState = useAppStore((state) => state.updateState);
 
   const selectedAppId = useUIStore((state) => state.selectedAppId);
+
   const boardApps = useAppStore((state) => state.apps);
   const selApp = boardApps.find((el) => el._id === selectedAppId);
 
   const update = useAppStore((state) => state.update);
 
-  // Keeps track of Pin Board previous position, necessary to move hosted apps with board
+  /**
+   * Keeps track of AI Pane previous position, necessary to move hosted apps with pane
+   */
   const prevX = useRef(props.data.position.x);
   const prevY = useRef(props.data.position.y);
 
-  // Generate a random color
-  // const randColor = () => {
-  //   const colors =
-  //     [
-  //       "red.400",
-  //       "green.400",
-  //       "blue.400",
-  //       "yellow.400",
-  //       "purple.400",
-  //       "pink.400",
-  //       "teal.400",
-  //     ]
-  //   const randomIndex = Math.floor(Math.random() * colors.length);
-  //   return colors[randomIndex];
-  // }
+  const prevHosted = useRef(s.hostedApps);
 
-  // Colors
-  const bg = useColorModeValue('yellow.100', 'yellow.400');
-  const backgroundColor = useHexColor(bg);
+  const [localHostedApps, setLocalHostedApps] = useState<Record<string, string>>({});
 
-  // Checks for apps on or off the pane
+  /**
+   * Checks for apps on or off the pane
+   * Should work if pane or any hosted apps are moved, resized, or closed out
+   */
   useEffect(() => {
     // Check all apps on board
     for (const app of boardApps) {
@@ -79,42 +78,33 @@ function AppComponent(props: App): JSX.Element {
         app.data.size.height + app.data.position.y > props.data.position.y &&
         app.data.type !== 'PinBoard'
       ) {
-        if (!Object.keys(s.hostedApps).includes(app._id)) {
-          const hosted = {
-            ...s.hostedApps,
-            ...client,
-          };
-          updateState(props._id, {hostedApps: hosted});
-          // TODO Make messages more informative rather than simply types of apps being hosted
-          // console.log('app ' + app._id + ' added');
-          newAppAdded(app.data.type);
-        } else {
-          // console.log('app ' + app._id + ' already in hostedApps');
+        if (!Object.keys(localHostedApps).includes(app._id)) {
+          setLocalHostedApps(localHostedApps => ({
+            ...localHostedApps,
+            ...client
+          }));
+
         }
       } else {
-        if (Object.keys(s.hostedApps).includes(app._id)) {
-          const hostedCopy = {...s.hostedApps};
+        // This code is necessary to remove hosted apps and messages once apps are no longer being hosted
+        if (Object.keys(localHostedApps).includes(app._id)) {
+          const hostedCopy = {...localHostedApps};
           delete hostedCopy[app._id];
-          updateState(props._id, {messages: hostedCopy, hostedApps: hostedCopy});
+          setLocalHostedApps(hostedCopy)
         }
       }
     }
-  }, [selApp?.data.position.x, selApp?.data.position.y, selApp?.data.size.height, selApp?.data.size.width, JSON.stringify(boardApps)]);
+  }, [selApp?.data.position, selApp?.data.size, boardApps, JSON.stringify(localHostedApps)]);
 
-  // TODO Be mindful of client updates
-  // Currently, every client updates once one does. Eventually add a way to monitor userID's and let only one person send update to server
-  // Refer to videoViewer play function
-  // Currently needed to keep track of hosted apps that are added when created or removed when deleted from board, rather than only those moved on and off
   useEffect(() => {
-    const copyofhostapps = {} as { [key: string]: string };
-    Object.keys(s.hostedApps).forEach((key: string) => {
-      const app = boardApps.find((el) => el._id === key);
-      if (app) copyofhostapps[key] = app.data.type;
-    });
-    updateState(props._id, {hostedApps: copyofhostapps});
-  }, [boardApps.length]);
+    updateState(props._id, {hostedApps: localHostedApps});
+    console.log('Updated hostedApps: ' + Object.keys(s.hostedApps))
+  }, [localHostedApps]);
 
-  // Move all apps together with the Pin Board
+  /**
+   * Move all apps together with the AIPane
+   * TODO: Track movement in a local state
+   */
   useEffect(() => {
     const hostedCopy = {...s.hostedApps};
     const xDiff = props.data.position.x - prevX.current;
@@ -134,18 +124,15 @@ function AppComponent(props: App): JSX.Element {
     }
     prevX.current = props.data.position.x;
     prevY.current = props.data.position.y;
-  }, [props.data.position.x, props.data.position.y, JSON.stringify(s.hostedApps)]);
+  }, [props.data.position.x, props.data.position.y, JSON.stringify(localHostedApps)]);
 
-  // Notifies backend of a new app being added and it's app type
-  function newAppAdded(appType: string) {
-    updateState(props._id, {
-      executeInfo: {executeFunc: 'new_app_added', params: {app_type: appType}},
-    });
-  }
 
   return (
     <AppWindow app={props} lockToBackground={true}>
-      <Box bgColor={bg} w={'100%'} h={'100%'} opacity={0.3}></Box>
+      <>
+        <Box>
+        </Box>
+      </>
     </AppWindow>
   );
 }
@@ -154,10 +141,14 @@ function ToolbarComponent(props: App): JSX.Element {
   const s = props.data.state as AppState;
   const updateState = useAppStore((state) => state.updateState);
 
+
   return (
-    <>
-    </>
+    <Box>
+    </Box>
   );
 }
 
-export default {AppComponent, ToolbarComponent};
+export default {
+  AppComponent, ToolbarComponent
+}
+;
