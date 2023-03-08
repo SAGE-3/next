c4f738d5112eaa0ce2680277d60669779455453c
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2023-03-07 16:37:04 -1000

    Button to add new lists prototype

--- webstack/libs/applications/src/lib/apps/PinBoard/PinBoard.tsx
+++ webstack/libs/applications/src/lib/apps/PinBoard/PinBoard.tsx
@@ -6,7 +6,7 @@
  *
  */
 
-import {Box} from '@chakra-ui/react';
+import {Box, Button} from '@chakra-ui/react';
 import {useEffect, useState, useRef} from 'react';
 
 import {useAppStore, useUIStore} from '@sage3/frontend';
@@ -14,6 +14,7 @@ import {useAppStore, useUIStore} from '@sage3/frontend';
 import {App} from '../../schema';
 import {state as AppState} from './index';
 import {AppWindow} from '../../components';
+import {v4 as uuidv4} from 'uuid';
 
 import './styles.css';
 
@@ -88,26 +89,53 @@ function AppComponent(props: (App & DraggableListProps)): JSX.Element {
 
   useEffect(() => {
     updateState(props._id, {lists: localLists});
+    console.log("Local list update")
+    console.log(JSON.stringify(localLists))
   }, [localLists])
 
-  const dragStart = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
+  const updateListCount = () => {
+    const newuuid = uuidv4();
+
+    // const newList = localLists.find((list) => list.listID === listID)?.list;
+    const blankListItem: ListItem = {item: "NEW ITEM", isDragging: false}
+    const blankList: List = {listID: newuuid, list: [blankListItem]}
+    const newList = [...localLists, blankList]
+
+    setLocalLists(newList)
+    // updateList(newuuid, newList)
+  }
+
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
     dragItem.current = {listID, itemId};
+
   };
 
-  const dragEnter = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
+  // Visually splice dragged items out of array
+  // const handleDragStart = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
+  //   dragItem.current = {listID, itemId};
+  //
+  //   const tempList = localLists.map((list) => ({
+  //     ...list,
+  //     list: list.list.filter((item, index) => index !== itemId),
+  //   }));
+  //
+  //   setLocalLists(tempList);
+  // };
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
     dragOverItem.current = {listID, itemId};
 
     const newList = localLists.find((list) => list.listID === listID)?.list.map((item, index) => ({
       ...item,
       isDragging: index === itemId,
     }));
-
     if (newList) {
       updateList(listID, newList);
     }
   };
 
-  const drop = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
+
+  const handleDragEnd = (e: React.DragEvent<HTMLDivElement>, listID: string, itemId: number) => {
     const dragItemId = dragItem.current?.itemId;
     const draglistID = dragItem.current?.listID;
     const dropItemId = dragOverItem.current?.itemId;
@@ -145,34 +173,46 @@ function AppComponent(props: (App & DraggableListProps)): JSX.Element {
   return (
     <AppWindow app={props} lockToBackground={true}>
       <div>
-        {s.lists.map((list) => (
-          <Box
-            key={list.listID}
-            border="1px"
-            borderColor="teal"
-            borderWidth="5px"
-            borderRadius="25px"
-            backgroundColor="green.300"
-            p="5px">
-            {list.list.map((item) => (
-              <>
-                <div
-                  style={{backgroundColor: 'orange', borderRadius: '25px', margin: '10px 5%', textAlign: 'center', fontSize: '40px'}}
-                  key={item.item}
-                  draggable
-                  onDragStart={(e) => dragStart(e, list.listID, list.list.indexOf(item))}
-                  onDragEnter={(e) => dragEnter(e, list.listID, list.list.indexOf(item))}
-                  // onDragEnd={() => dragEnd()}
-                  onDragEnd={(e) => drop(e, list.listID, list.list.indexOf(item))}
-                >
-                  {item.item}
-                </div>
-                {item.isDragging ? <div className="drag-line"/> : null}
-              </>
-            ))}
-          </Box>
-
-        ))}
+        {s.lists &&
+          s.lists.map((list) => (
+            <Box
+              key={list.listID}
+              border="1px"
+              borderColor="teal"
+              borderWidth="5px"
+              borderRadius="25px"
+              backgroundColor="green.300"
+              p="5px">
+              {list.list.map((item, index) => (
+                <>
+                  <div
+                    style={{
+                      backgroundColor: 'orange',
+                      borderRadius: '25px',
+                      margin: '10px 5%',
+                      textAlign: 'center',
+                      fontSize: '40px'
+                    }}
+                    key={item.item}
+                    draggable
+                    onDragStart={(e) => handleDragStart(e, list.listID, list.list.indexOf(item))}
+                    onDragEnter={(e) => handleDragEnter(e, list.listID, list.list.indexOf(item))}
+                    // onDragEnd={() => dragEnd()}
+                    onDragEnd={(e) => handleDragEnd(e, list.listID, list.list.indexOf(item))}
+                  >
+                    {item.item}
+                  </div>
+                  {item.isDragging ? <div className="drag-line"/> : null}
+                </>
+              ))}
+            </Box>
+          ))}
+        <Button
+          colorScheme="blue"
+          onClick={() => updateListCount()}
+        >
+          Add Column
+        </Button>
       </div>
     </AppWindow>
   );
@@ -181,6 +221,12 @@ function AppComponent(props: (App & DraggableListProps)): JSX.Element {
 function ToolbarComponent(props: App): JSX.Element {
   const s = props.data.state as AppState;
   const updateState = useAppStore((state) => state.updateState);
+
+  const [addColumns, setAddColumns] = useState<List[]>();
+
+
+
+
 
 
   return (
