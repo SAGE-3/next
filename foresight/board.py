6c6a6f433bc843f3d245ff4e8a326642c3fc1398
Mahdi  B <b.mahdi@gmail.com>
2023-05-03 12:20:53 -1000

    send update after the work is done

--- foresight/board.py
+++ foresight/board.py
@@ -33,13 +33,13 @@ class Board:
             self.executeInfo = {"executeFunc": "", "params": {}}
 
     def reorganize_layout(
-        self,
-        viewport_position,
-        viewport_size,
-        buffer_size=100,
-        by="combined",
-        mode="graphviz",
-        selected_apps = None,
+            self,
+            viewport_position,
+            viewport_size,
+            buffer_size=100,
+            by="combined",
+            mode="graphviz",
+            selected_apps=None,
     ):
         if by not in ["app_type", "semantic"]:
             print(f"{by} not a valid by option to organize layout. Not executing")
@@ -95,25 +95,29 @@ class Board:
             sb.send_updates()
 
     def group_by_topic(self,
-        viewport_position: dict,
-        viewport_size: dict,
-        selected_apps: list = None):
+                       viewport_position: dict,
+                       viewport_size: dict,
+                       selected_apps: list = None):
+
         self.executeInfo = {"executeFunc": "", "params": {}}
-        colors = ['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
-        # double check the selected apps are all Stickies (for now)
+        # colors = ['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
+        colors = ['red', 'yellow', 'green']
+
+        # double-check the selected apps are all Stickies (for now)
         if selected_apps is not None:
+
             # group all the smartbits as a list of tuples (app_id, text)
             sticky_list = [(app_id, self.smartbits[app_id].state.text) for app_id in selected_apps]
+
             print(f"sticky_list is {sticky_list}")
+
             # TODO: call the clustering algorithm
-            # cluster the stickies by topic
+
             for app_id in selected_apps:
                 sb = self.smartbits[app_id]
                 if sb.data.type != "Stickie":
                     print(f"App {app_id} is not a Sticky. Not executing")
                 else:
                     random_color = np.random.choice(colors)
-                    # print(f"App {app_id} is a Sticky. Changing color to {random_color}")
                     sb.state.color = random_color
-                    sb.send_updates()
-
+            sb.send_updates()
