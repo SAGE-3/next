b6555f8a9b4b2c98a904f18c2941a15fe1d795b3
Ryan Theriot <rtheriot@hawaii.edu>
2023-05-04 07:18:31 -1000

    Removed current user from list. Sort by name alphabetically included. Added some css padding.

--- webstack/libs/frontend/src/lib/ui/components/modals/UserSearchModal.tsx
+++ webstack/libs/frontend/src/lib/ui/components/modals/UserSearchModal.tsx
@@ -12,12 +12,9 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
-  Button,
   ModalCloseButton,
   Input,
-  Avatar,
   Box,
   Text,
   Divider,
@@ -25,12 +22,15 @@ import {
   useDisclosure,
   useColorModeValue,
   useToast,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react';
 
-import { SAGEColors } from '@sage3/shared';
-import { Board, Presence, User } from '@sage3/shared/types';
-import { APIHttp, EnterBoardModal, useHexColor, usePresenceStore, useUsersStore } from '@sage3/frontend';
+import { Board, User } from '@sage3/shared/types';
+import { EnterBoardModal, useHexColor, usePresenceStore, useUser, useUsersStore } from '@sage3/frontend';
+import { MdSearch } from 'react-icons/md';
 
+// User Search Modal Props
 interface UserSearchProps {
   isOpen: boolean;
   onOpen: () => void;
@@ -38,109 +38,146 @@ interface UserSearchProps {
 }
 
 /**
- * Confirmation Modal
+ * User Search Modal to allow users to find other users within the SAGE3 server.
  * @param props
  * @returns
  */
 export function UserSearchModal(props: UserSearchProps): JSX.Element {
   // Users & Presences
+  const { user } = useUser();
   const users = useUsersStore((state) => state.users);
   const presences = usePresenceStore((state) => state.presences);
+
   // Local search results
   const [userResults, setUserResults] = useState<User[]>([]);
-  const [searchPrompt, setSearchPrompt] = useState<string>('');
-  // Element to set the focus to when opening the dialog
-  const initialRef = useRef<HTMLInputElement>(null);
 
+  // Search prompt and change handler
+  const [searchPrompt, setSearchPrompt] = useState<string>('');
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchPrompt(event.target.value);
   };
 
+  // Element to set the focus to when opening the dialog
+  const initialRef = useRef<HTMLInputElement>(null);
+
+  // Sort the users by presence and name. Remove current user.
+  const sortAndFilterUsers = () => {
+    // sort by name
+    users.sort((a, b) => a.data.name.localeCompare(b.data.name));
+    // sort by presence and name
+    users.sort((a, b) => {
+      const pa = presences.find((p) => p._id === a._id);
+      const pb = presences.find((p) => p._id === b._id);
+      if (pa && pb) return a.data.name.localeCompare(b.data.name);
+      if (pa) return -1;
+      return a.data.name.localeCompare(b.data.name);
+    });
+    // remove current user
+    return users.filter((u) => u._id !== user?._id);
+  };
+
+  // Perform the user search and set the results
   const performUserSearch = async (searchPrompt: string) => {
+    const sortedUsers = sortAndFilterUsers();
     // Search for users
     if (searchPrompt.length > 0) {
-      const results = users.filter((user) =>
-        user.data.name.toLowerCase().includes(searchPrompt.toLowerCase()) ||
-        user.data.email.toLowerCase().includes(searchPrompt.toLowerCase())
+      const results = sortedUsers.filter(
+        (user) =>
+          user.data.name.toLowerCase().includes(searchPrompt.toLowerCase()) ||
+          user.data.email.toLowerCase().includes(searchPrompt.toLowerCase())
       );
       setUserResults(results);
     } else {
-      setUserResults(users);
+      setUserResults(sortedUsers);
     }
   };
 
   // Perform search when users change and search prompt changes
+
   useEffect(() => {
-    // sort by name
-    users.sort((a, b) => a.data.name.localeCompare(b.data.name));
-    // sort by presence
-    users.sort((a, b) => {
-      const pa = presences.find((p) => p._id === a._id);
-      const pb = presences.find((p) => p._id === b._id);
-      if (pa && pb) return 0;
-      if (pa) return -1;
-      return 1;
-    });
     performUserSearch(searchPrompt);
   }, [searchPrompt, users, presences]);
 
+  // Close the modal
   const closeModal = () => {
     props.onClose();
   };
 
   return (
-    <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered size={"xl"} initialFocusRef={initialRef}>
+    <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered size={'xl'} initialFocusRef={initialRef}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent pb={3}>
         <ModalHeader>User Search</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          {/* User serarch input */}
           <Box px="2">
-            <Input ref={initialRef} placeholder="Search for a user" _placeholder={{ color: 'gray.400', opacity: 1.0 }} onChange={handleSearchChange} />
+            <InputGroup>
+              <InputLeftElement pointerEvents="none" children={<MdSearch color="gray.300" />} />
+              <Input
+                placeholder="Search for a user"
+                _placeholder={{ color: 'gray.400', opacity: 1.0 }}
+                onChange={handleSearchChange}
+                value={searchPrompt}
+                ref={initialRef}
+              />
+            </InputGroup>
           </Box>
-
-          <Box px="3" mt="3">
+          {/* Divider */}
+          <Box px="3" my="3">
             <Divider></Divider>
           </Box>
+          {/* User Results */}
           <Box display="flex" flexDir="column" height="50vh" width="100%" overflowY="auto" overflowX="hidden" p="3">
             {userResults.map((user) => (
               <UserCard key={user._id} user={user} onClose={closeModal} />
             ))}
           </Box>
         </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme={'gray'} mr={3} onClick={props.onClose}>
-            Close{' '}
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
 }
 
+/**
+ * User Card Props
+ */
 type UserCardProps = {
   user: User;
   onClose: () => void;
 };
+
+/**
+ * Displays the user's name, email, and current board if they are in one.
+ */
 function UserCard(props: UserCardProps) {
-  const green = useHexColor('green');
-  const red = useHexColor('red');
+  // Status colors
+  const onlineColor = useHexColor('green');
+  const awayColor = useHexColor('yellow');
+  const offlineColor = useHexColor('red');
+
+  // Card Background colors
   const background = useColorModeValue('#f5f5f5', 'gray.600');
   const hoverBackground = useColorModeValue('#fffefe', 'gray.500');
+
   // Current Presences
   const presences = usePresenceStore((state) => state.presences);
   const presence = presences.find((presence) => presence._id === props.user._id);
 
+  // Display information
   const name = props.user.data.name;
   const email = props.user.data.email ? props.user.data.email : 'Guest';
 
+  // Modal control
   const { isOpen: enterBoardIsOpen, onOpen: enterBoardOnOpen, onClose: enterBoardOnClose } = useDisclosure();
 
+  // Board information for the user
   const [board, setBoard] = useState<Board | undefined>(undefined);
 
+  // Toast for information feedback
   const toast = useToast();
 
+  // Get the board information for the user
   async function getBoardInfo(boardId: string) {
     const res = await fetch(`/api/boards/${boardId}`);
     const data = await res.json();
@@ -150,17 +187,21 @@ function UserCard(props: UserCardProps) {
     return;
   }
 
+  // User effect to obtain the board information at start
+  // Then update information if presence changes
   useEffect(() => {
     if (presence && presence.data.boardId) {
       getBoardInfo(presence.data.boardId);
     }
   }, [JSON.stringify(presence)]);
 
+  // Close the modal
   const closeModal = () => {
     props.onClose();
     enterBoardOnClose();
   };
 
+  // Attempt to enter the board
   const enterBoard = () => {
     if (board) {
       enterBoardOnOpen();
@@ -187,13 +228,25 @@ function UserCard(props: UserCardProps) {
       transition="all 0.1s ease-in-out"
       _hover={{ backgroundColor: hoverBackground, transform: 'scale(1.05)' }}
     >
+      {/* Board Modal */}
       {board && <EnterBoardModal board={board} isOpen={enterBoardIsOpen} onClose={closeModal} />}
+
+      {/* Left side of display card */}
       <Box display="flex" justifyContent="left" alignContent="center" width="50%">
+        {/* Status Icon */}
         <Box>
           <Tooltip label={presence ? 'User is online' : 'User is offline'} aria-label="online-status" hasArrow placement="top">
-            <Box borderRadius="100%" backgroundColor={presence ? green : red} width="28px" height="28px" transform="translateY(8px)"></Box>
+            <Box
+              borderRadius="100%"
+              backgroundColor={presence ? onlineColor : offlineColor}
+              width="28px"
+              height="28px"
+              transform="translateY(8px)"
+            ></Box>
           </Tooltip>
         </Box>
+
+        {/* Name and Email information */}
         <Box display="flex" flexDir="column" ml="2" whiteSpace="nowrap" overflow="hidden">
           <Text fontSize="md" fontWeight="bold" textOverflow="ellipsis">
             {name}
@@ -201,7 +254,10 @@ function UserCard(props: UserCardProps) {
           <Text fontSize="sm">{email}</Text>
         </Box>
       </Box>
+
+      {/* Right side of display card */}
       <Box display="flex" justifyContent="right">
+        {/* Board information */}
         <Box display="flex" flexDir="column" ml="2" whiteSpace="nowrap" overflow="hidden" textAlign="right">
           <Text fontSize="md" textOverflow="ellipsis">
             Board
