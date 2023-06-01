3f513dc62313e73a4024ab0a34059549ac7d3af0
renambot <renambot@gmail.com>
2023-05-31 20:30:42 -0500

    new: set redis url for celery

--- foresight/smartbits/celery.py
+++ foresight/smartbits/celery.py
@@ -1,11 +1,26 @@
 from celery import Celery
-import requests
-import json
+import os
+
+# import requests, json
+
+prod_type = os.getenv("ENVIRONMENT")
+if prod_type is None:
+    prod_type = "production"
 
 # Broker is the message queue, backend is the result store
-app = Celery(
-    "smartbits", broker="redis://localhost:6379/0", backend="redis://localhost:6379/1"
-)
+
+if prod_type == "development":
+    app = Celery(
+        "smartbits",
+        broker="redis://localhost:6379/0",
+        backend="redis://localhost:6379/1",
+    )
+else:
+    app = Celery(
+        "smartbits",
+        broker="redis://redis-server:6379/0",
+        backend="redis://redis-server:6379/1",
+    )
 
 
 @app.task
