e9fc65d7b12ce58784172d9ec692762de033a04e
Mahdi  B <b.mahdi@gmail.com>
2023-06-20 11:09:48 -1000

    Update setup.py

--- foresight/SAGE3Sugar/setup.py
+++ foresight/SAGE3Sugar/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
 setup(
     name='Sage3Sugar',
     version='1.0',
