92a36d0a4ec0d934bb5d37784ea1334382512e51
Mahdi  B <b.mahdi@gmail.com>
2023-06-20 11:00:07 -1000

    updated requirements and added setup file

---
+++ foresight/SAGE3Sugar/setup.py
@@ -0,0 +1,11 @@
+rom setuptools import setup, find_packages
+
+setup(
+    name='Sage3Sugar',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    author='Mahdi',
+    description='Sage3 Python API',
+    url='https://github.com/SAGE-3/next',
+)
