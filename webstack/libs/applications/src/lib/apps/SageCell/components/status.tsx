f89ffd836ad93200236c5e24c7df4ee90efc0c95
Michael Rogers <mlr2010@hawaii.edu>
2023-07-18 00:05:21 -1000

    Removed "isTyping" indicator

--- webstack/libs/applications/src/lib/apps/SageCell/components/status.tsx
+++ webstack/libs/applications/src/lib/apps/SageCell/components/status.tsx
@@ -4,18 +4,13 @@ import { truncateWithEllipsis } from '@sage3/frontend';
 interface StatusBarProps {
   kernel: string;
   access: boolean;
-  isTyping: boolean;
-  bgColor: string;
 }
 
 export const StatusBar = (props: StatusBarProps) => {
   return (
-    <Stack direction="row" bgColor={props.bgColor} p={1}>
+    <Stack direction="row" p={1}>
       <Badge variant="outline" colorScheme="blue">
         {props.kernel ? `Kernel: ${truncateWithEllipsis(props.kernel, 8)}` : 'No Kernel Selected'}
-      </Badge>
-      <Badge variant="ghost" colorScheme="red">
-        {props.isTyping ? `typing...` : ''}
       </Badge>
       <Spacer />
       {!props.kernel && !props.access ? ( // no kernel selected and no access
