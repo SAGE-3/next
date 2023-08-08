2f49bb264dd7c295357085372796ad605a38dd9f
renambot <renambot@gmail.com>
2023-08-08 13:30:37 -0500

    fix: add plotly to jupyter

--- deployment/configurations/jupyter/conf/start.sh
+++ deployment/configurations/jupyter/conf/start.sh
@@ -18,7 +18,8 @@ python3 /conf/redis-store.py $token
 
 # dependencies (requirements.txt)
 # pip3 install websockets matplotlib rejson pytest requests namesgenerator httpx pydantic
-# pip3 install ipython jupyter_client jupyter-console jupyterlab-link-share plotly
+# pip3 install ipython jupyter_client jupyter-console jupyterlab-link-share
+pip3 install matplotlib plotly
 pip3 install jupyterlab-lsp pyright python-language-server python-lsp-server[all] jupyter_collaboration
 # Getting All the NodeJS-based Language Servers
 jlpm add --dev bash-language-server vscode-css-languageserver-bin dockerfile-language-server-nodejs vscode-html-languageserver-bin javascript-typescript-langserver vscode-json-languageserver-bin yaml-language-server
