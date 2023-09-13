34cdc355844526d99bbe1aa7ed2005962ca4203a
renambot <renambot@gmail.com>
2023-09-13 14:56:24 -0500

    fix: reduce sleep to connect to websocket from 2sec to 0.05sec

--- foresight/foresight/utils/sage_websocket.py
+++ foresight/foresight/utils/sage_websocket.py
@@ -16,23 +16,21 @@ import time
 import sys
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 class SageWebsocket:
-
     def __init__(self, on_message_fn: Callable = None):
         self.connected = False
-        self.ws = websocket.WebSocketApp(conf[prod_type]["ws_server"]+"/api",
-                                         header={
-                                             "Authorization": "Bearer " + os.getenv('TOKEN')},
-                                         on_message=lambda ws, msg: self.on_message(
-                                             ws, msg),
-                                         on_error=lambda ws, msg: self.on_error(
-                                             ws, msg),
-                                         # on_close=lambda ws: self.on_close(ws),
-                                         on_open=lambda ws: self.on_open(ws)
-                                         )
+        self.ws = websocket.WebSocketApp(
+            conf[prod_type]["ws_server"] + "/api",
+            header={"Authorization": "Bearer " + os.getenv("TOKEN")},
+            on_message=lambda ws, msg: self.on_message(ws, msg),
+            on_error=lambda ws, msg: self.on_error(ws, msg),
+            # on_close=lambda ws: self.on_close(ws),
+            on_open=lambda ws: self.on_open(ws),
+        )
 
         self.wst = None
 
@@ -51,7 +49,8 @@ class SageWebsocket:
 
     def on_message(self, ws, message):
         logger.warning(
-            f"received message in default func on_message {message}, WRANIGN---not doing anything")
+            f"received message in default func on_message {message}, WRANIGN---not doing anything"
+        )
         # msg = json.loads(message)
         # sub_id = msg['id']
         # if self.queue_list[sub_id]:
@@ -71,13 +70,14 @@ class SageWebsocket:
             return True
         count = 0
         while self.connected == False:
-            logger.info('Websocket still not connected')
+            logger.info("Websocket still not connected")
             count = count + 1
             if count > attempts:
                 logger.error(
-                    'Could not establish a connection to the server after {attempts} attempts')
+                    "Could not establish a connection to the server after {attempts} attempts"
+                )
                 return False
-            time.sleep(2)
+            time.sleep(0.05)
         return True
 
     # Subscribe to a route
@@ -90,10 +90,7 @@ class SageWebsocket:
         for route in routes:
             # # Generate id for subscription
             subscription_id = str(uuid.uuid4())
-            msg_sub = {
-                'route': route,
-                'id': subscription_id, 'method': 'SUB'
-            }
+            msg_sub = {"route": route, "id": subscription_id, "method": "SUB"}
             self.ws.send(json.dumps(msg_sub))
 
     def run(self):
