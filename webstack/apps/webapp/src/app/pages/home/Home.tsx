6f7da1362590abd754fd61977b1f20ec77ac212e
Ryan Theriot <rtheriot@hawaii.edu>
2023-10-10 16:46:29 -1000

    Experimentation with home page

--- webstack/apps/webapp/src/app/pages/home/Home.tsx
+++ webstack/apps/webapp/src/app/pages/home/Home.tsx
@@ -9,7 +9,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Box, useColorModeValue, Text, Image, Progress } from '@chakra-ui/react';
+import { Box, useColorModeValue, Text, Image, Progress, Button, IconButton, Input } from '@chakra-ui/react';
 
 import {
   JoinBoardCheck,
@@ -24,8 +24,10 @@ import {
   usePluginStore,
   useConfigStore,
   useRouteNav,
+  useHexColor,
 } from '@sage3/frontend';
-import { Board, Room } from '@sage3/shared/types';
+import { Board, Room, User } from '@sage3/shared/types';
+import { MdAdd, MdExitToApp, MdLock, MdPerson } from 'react-icons/md';
 
 export function HomePage() {
   // URL Params
@@ -43,7 +45,7 @@ export function HomePage() {
   const roomsFetched = useRoomStore((state) => state.fetched);
 
   // Board Store
-  const boards = useBoardStore((state) => state.boards);
+  const { boards, subscribeByRoomId } = useBoardStore((state) => state);
   const [selectedBoard, setSelectedBoard] = useState<Board | undefined>(undefined);
 
   // Plugins
@@ -51,12 +53,22 @@ export function HomePage() {
 
   // Users and presence
   const { user } = useUser();
+  const users = useUsersStore((state) => state.users);
   const updatePresence = usePresenceStore((state) => state.update);
   const subscribeToPresence = usePresenceStore((state) => state.subscribe);
   const subscribeToUsers = useUsersStore((state) => state.subscribeToUsers);
 
   // SAGE3 Image
   const imageUrl = useColorModeValue('/assets/SAGE3LightMode.png', '/assets/SAGE3DarkMode.png');
+
+  // UI Colors
+  const roomListBackgroud = useColorModeValue('gray.300', 'gray.700');
+  const roomListBG = useHexColor(roomListBackgroud);
+  const boardListBackground = useColorModeValue('gray.200', 'gray.800');
+  const boardListBG = useHexColor(boardListBackground);
+  const infoBackground = useColorModeValue('gray.100', 'gray.900');
+  const infoBG = useHexColor(infoBackground);
+  const borderColor = useHexColor(selectedRoom ? selectedRoom.data.color : 'gray.300');
 
   // Subscribe to user updates
   useEffect(() => {
@@ -118,13 +130,19 @@ export function HomePage() {
     }
   }, [roomsFetched]);
 
+  useEffect(() => {
+    if (selectedRoom) {
+      subscribeByRoomId(selectedRoom?._id);
+    }
+  }, [selectedRoom]);
+
   return (
     // Main Container
     <Box display="flex" flexDir={'column'} width="100%" height="100%" alignItems="center" justifyContent="space-between">
       {/* Check if the user wanted to join a board through a URL */}
       <JoinBoardCheck />
       {/* Top Bar */}
-      <Box display="flex" flexDirection="row" justifyContent="space-between" minHeight={45} width="100vw" px="2">
+      <Box display="flex" flexDirection="row" justifyContent="space-between" height={'32px'} width="100vw" px="2">
         <Box flex="1 1 0px">
           <Text
             fontSize="xl"
@@ -137,7 +155,7 @@ export function HomePage() {
             userSelect="none"
             whiteSpace={'nowrap'}
           >
-            {config?.serverName}
+            Server: {config?.serverName}
           </Text>
         </Box>
         <Box></Box>
@@ -156,11 +174,100 @@ export function HomePage() {
         justifyContent={'center'}
         minHeight={0}
         width="100%"
-        maxWidth="1200px"
-        minWidth="400px"
-        px="4"
+        height="100%"
+        overflow="hidden"
       >
         {roomsFetched ? (
+          <>
+            <Box display="column" height="100%" pt="8px" width="400px" background={boardListBG} borderRight={`solid 1px white`}>
+              <Box px="2" mb="2" display="flex" justifyContent={'space-between'}>
+                <h3>Rooms</h3>
+                <IconButton
+                  size="sm"
+                  colorScheme="teal"
+                  variant={'outline'}
+                  aria-label="enter-board"
+                  fontSize="xl"
+                  icon={<MdAdd />}
+                ></IconButton>
+              </Box>
+
+              {rooms
+                .sort((a, b) => a.data.name.localeCompare(b.data.name))
+                .map((room) => {
+                  return <RoomCard room={room} selected={selectedRoom?._id === room._id} onClick={handleRoomClick} />;
+                })}
+            </Box>
+            <Box
+              display="column"
+              height="100%"
+              width="400px"
+              p="8px"
+              background={roomListBG}
+              borderTop={`solid 1px white`}
+              borderBottom={`solid 1px white`}
+            >
+              <Box display="flex" justifyContent={'space-between'}>
+                <h3>{selectedRoom?.data.name}</h3>
+                <IconButton size="sm" variant={'outline'} aria-label="enter-board" fontSize="xl" icon={<MdLock />}></IconButton>{' '}
+              </Box>
+              <Box height="300px" display="flex" my="4" flexDir="column" justifyContent={'space-between'}>
+                <Box>
+                  <Text fontSize="sm">This is the board's description.</Text>
+                  <Text fontSize="sm">Owner: Ryan Theriot</Text>
+                  <Text fontSize="sm">Created: Jan 5th, 2023</Text>
+                  <Text fontSize="sm">Updated: Oct 5th, 2023</Text>
+                </Box>
+                <Box>
+                  <Button size="sm" width="100%" my="1" colorScheme="teal" variant="outline">
+                    Edit
+                  </Button>
+                  <Button size="sm" width="100%" my="1" colorScheme="teal" variant="outline">
+                    Manage Members
+                  </Button>
+                </Box>
+              </Box>
+              <h3>Room Members</h3>
+              {users.map((user) => {
+                return <UserCard user={user} onClick={() => {}} />;
+              })}
+            </Box>
+            <Box
+              display="column"
+              height="100%"
+              width="600px"
+              p="8px"
+              background={boardListBG}
+              borderTop={`solid 1px white`}
+              borderBottom={`solid 1px white`}
+            >
+              <Box px="2" display="flex" justifyContent={'space-between'}>
+                <h3>Boards</h3>
+                <IconButton size="sm" variant={'outline'} aria-label="enter-board" fontSize="xl" icon={<MdAdd />}></IconButton>{' '}
+              </Box>
+              {boards
+                .sort((a, b) => a.data.name.localeCompare(b.data.name))
+                .map((board) => {
+                  return <BoardCard board={board} onClick={() => {}} />;
+                })}
+            </Box>
+
+            <Box
+              display="column"
+              height="100%"
+              width="100%"
+              background={infoBG}
+              borderTop={`solid 1px white`}
+              borderBottom={`solid 1px white`}
+            ></Box>
+          </>
+        ) : (
+          <Box display="flex" flexDirection="column" justifyContent={'center'} alignItems="center" height="100%" width="100%">
+            <Progress isIndeterminate width="100%" borderRadius="md" />
+          </Box>
+        )}
+      </Box>
+      {/* {roomsFetched ? (
           <Box
             display="flex"
             flexDirection="column"
@@ -184,7 +291,7 @@ export function HomePage() {
             <Progress isIndeterminate width="100%" borderRadius="md" />
           </Box>
         )}
-      </Box>
+      </Box> */}
 
       {/* Bottom Bar */}
       <Box
@@ -197,9 +304,109 @@ export function HomePage() {
         py="2"
         px="2"
       >
-        <MainButton buttonStyle="solid" config={config} />
+        <Box display={'flex'}>
+          <MainButton buttonStyle="solid" config={config} />
+          <Box width="400px" px="8px" py="8px" display="flex">
+            <Text fontSize="sm" mr="2">
+              {' '}
+              Search
+            </Text>
+            <Input size="xs"></Input>
+          </Box>
+        </Box>
         <Image src={imageUrl} height="30px" style={{ opacity: 0.7 }} alt="sag3" userSelect={'auto'} draggable={false} />
       </Box>
+    </Box>
+  );
+}
+
+function RoomCard(props: { room: Room; selected: boolean; onClick: (room: Room) => void }) {
+  const borderColorValue = useColorModeValue(props.room.data.color, props.room.data.color);
+  const borderColor = useHexColor(borderColorValue);
+  const borderColorGray = useColorModeValue('gray.300', 'gray.700');
+  const borderColorG = useHexColor(borderColorGray);
+
+  return (
+    <Box
+      // my="4"
+      p="2"
+      pl="4"
+      display="flex"
+      onClick={() => props.onClick(props.room)}
+      border={`solid 1px ${props.selected ? 'white' : 'none'}`}
+      // borderRadius="md"
+      borderRight="none"
+      borderLeft="none"
+      boxSizing="border-box"
+      backgroundColor={props.selected ? borderColorG : 'transparent'}
+      _hover={{ cursor: 'pointer', backgroundColor: borderColorG }}
+      width={props.selected ? 'calc(100% + 2px)' : '100%'}
+    >
+      <Text fontSize="md" fontWeight="bold" textAlign="center">
+        {props.room.data.name}
+      </Text>
+    </Box>
+  );
+}
+
+function BoardCard(props: { board: Board; onClick: (board: Board) => void }) {
+  const borderColorValue = useColorModeValue(props.board.data.color, props.board.data.color);
+  const borderColor = useHexColor(borderColorValue);
+  const borderColorGray = useColorModeValue('gray.300', 'gray.700');
+  const borderColorG = useHexColor(borderColorGray);
+  return (
+    <Box
+      my="1"
+      p="1"
+      px="2"
+      display="flex"
+      borderRadius="md"
+      justifyContent={'space-between'}
+      alignContent={'center'}
+      onClick={() => props.onClick(props.board)}
+      background="gray.800"
+      _hover={{ cursor: 'pointer', backgroundColor: borderColorG }}
+    >
+      <Text fontSize="md" textAlign="center" lineHeight="32px" fontWeight="bold">
+        {props.board.data.name}
+      </Text>
+      <IconButton size="sm" variant={'ghost'} aria-label="enter-board" fontSize="xl" icon={<MdExitToApp />}></IconButton>
+    </Box>
+  );
+}
+
+function UserCard(props: { user: User; onClick: (user: User) => void }) {
+  const borderColorValue = useColorModeValue(props.user.data.color, props.user.data.color);
+  const borderColor = useHexColor(borderColorValue);
+  const borderColorGray = useColorModeValue('gray.300', 'gray.600');
+  const borderColorG = useHexColor(borderColorGray);
+
+  const offline = useHexColor('red');
+  const online = useHexColor('green');
+  return (
+    <Box
+      my="2"
+      p="2"
+      display="flex"
+      justifyContent={'left'}
+      alignContent={'center'}
+      onClick={() => props.onClick(props.user)}
+      borderRadius="md"
+      _hover={{ cursor: 'pointer', background: borderColorG }}
+    >
+      <IconButton
+        size="xs"
+        variant={'ghost'}
+        aria-label="enter-board"
+        fontSize="2xl"
+        mr="2"
+        color={Math.random() > 0.5 ? offline : online}
+        icon={<MdPerson />}
+      ></IconButton>
+
+      <Text fontSize="sm" fontWeight="bold" textAlign="center" lineHeight="24px">
+        {props.user.data.name}
+      </Text>
     </Box>
   );
 }
