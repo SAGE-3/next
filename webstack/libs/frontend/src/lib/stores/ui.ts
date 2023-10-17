1883d884017176ea726e54c694d91da6d4266b1b
Ryan Theriot <rtheriot@hawaii.edu>
2023-10-17 13:03:56 -1000

    Added indicator that you have saveselectedapps. Esc logic consolidated to deselect everything.

--- webstack/libs/frontend/src/lib/stores/ui.ts
+++ webstack/libs/frontend/src/lib/stores/ui.ts
@@ -231,7 +231,7 @@ export const useUIStore = create<UIState>((set, get) => ({
 
   deltaPos: { p: { x: 0, y: 0, z: 0 }, id: '' },
   setDeltaPostion: (position: Position, id: string) => set((state) => ({ ...state, deltaPos: { id, p: position } })),
-  setSelectedAppsIds: (appIds: string[]) => set((state) => ({ ...state, selectedAppsIds: appIds })),
+  setSelectedAppsIds: (appIds: string[]) => set((state) => ({ ...state, selectedAppsIds: appIds, savedSelectedAppsIds: appIds })),
   setSelectedAppSnapshot: (snapshot: { [id: string]: Position }) => {
     snapshot = structuredClone(snapshot);
     set((state) => ({ ...state, selectedAppsSnapshot: snapshot }));
@@ -248,7 +248,7 @@ export const useUIStore = create<UIState>((set, get) => ({
 
   savedSelectedAppsIds: [],
   setSavedSelectedAppsIds: () => set((state) => ({ ...state, savedSelectedAppsIds: get().selectedAppsIds })),
-  clearSavedSelectedAppsIds: () => set((state) => ({ ...state, savedSelectedAppsIds: [], selectedAppsIds: [] })),
+  clearSavedSelectedAppsIds: () => set((state) => ({ ...state, savedSelectedAppsIds: [] })),
 
   setWhiteboardMode: (mode: DrawingMode) => set((state) => ({ ...state, whiteboardMode: mode })),
   setClearMarkers: (clear: boolean) => set((state) => ({ ...state, clearMarkers: clear })),
