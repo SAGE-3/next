66957d2caab0dc107f0afeabb216048bff4eda0f
renambot <renambot@gmail.com>
2023-10-24 20:14:47 -0500

    new: add network indicator next to clock

--- webstack/libs/frontend/src/lib/ui/components/general/Clock.tsx
+++ webstack/libs/frontend/src/lib/ui/components/general/Clock.tsx
@@ -6,7 +6,10 @@
  * the file LICENSE, distributed as part of this software.
  */
 import { CSSProperties, useEffect, useState } from 'react';
-import { Box, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Text, useColorModeValue, Tooltip, IconButton } from '@chakra-ui/react';
+import { MdNetworkCheck } from 'react-icons/md';
+
+import { useNetworkState } from '@sage3/frontend';
 
 type ClockProps = {
   style?: CSSProperties;
@@ -25,6 +28,11 @@ export function Clock(props: ClockProps) {
   const textColor = useColorModeValue('gray.800', 'gray.50');
   const backgroundColor = useColorModeValue('#ffffff69', '#22222269');
 
+  // Network Status
+  const networkStatus = useNetworkState();
+  const [netcolor, setNetcolor] = useState('green');
+  const [netlabel, setNetlabel] = useState('online');
+
   // Update the time on an interval every 30secs
   useEffect(() => {
     const interval = window.setInterval(() => {
@@ -32,6 +40,28 @@ export function Clock(props: ClockProps) {
     }, 30 * 1000);
     return () => clearInterval(interval);
   }, []);
+
+
+  useEffect(() => {
+    if (networkStatus) {
+      if (networkStatus.online) {
+        setNetcolor('#38A169');
+        setNetlabel('online');
+        console.log('green');
+        if (networkStatus.effectiveType === '3g') {
+          setNetcolor('#F6AD55');
+          setNetlabel('mid-tier mobile');
+        }
+        if (networkStatus.effectiveType === '2g') {
+          setNetcolor('#FC8181');
+          setNetlabel('low-tier mobile');
+        }
+      } else {
+        setNetcolor('red');
+        setNetlabel('offline');
+      }
+    }
+  }, [networkStatus]);
 
   if (isBoard) {
     return (
@@ -44,9 +74,28 @@ export function Clock(props: ClockProps) {
         px={2}
         justifyContent="left"
         alignItems={'center'}>
+
         <Text fontSize={'lg'} opacity={props.opacity ? props.opacity : 1.0} color={textColor} userSelect="none" whiteSpace="nowrap">
           {time}
         </Text>
+
+        <Tooltip label={"Network status: " + netlabel} placement="top-start" shouldWrapChildren={true} openDelay={200} hasArrow={true}>
+          <IconButton borderRadius="md" h="auto" p={0} m={0}
+            fontSize="lg"
+            justifyContent="center"
+            aria-label={"Network status"}
+            icon={<MdNetworkCheck size="24px" color={netcolor} />}
+            background={'transparent'}
+            color={netcolor}
+            transition={'all 0.2s'}
+            opacity={0.75}
+            variant="ghost"
+            // onClick={props.onClick}
+            isDisabled={false}
+            _hover={{ color: netcolor, opacity: 1, transform: 'scale(1.15)' }}
+          />
+        </Tooltip>
+
       </Box>
     );
   } else {
