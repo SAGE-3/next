9239e2cdc71595d5096c23b91fb6e30c4174c7ca
Giorgio <tran.giorgio@gmail.com>
2024-02-19 23:32:57 -1000

    Refactor Overview component

--- webstack/libs/applications/src/lib/apps/RAPID/components/Overview.tsx
+++ webstack/libs/applications/src/lib/apps/RAPID/components/Overview.tsx
@@ -19,20 +19,28 @@ function Overview({ s }: RAPIDState): JSX.Element {
 
   const getMin = (arr: ResultDataPoint[], org: string) => {
     if (org === 'Sage Node') {
-      return Math.min(...arr.filter((d: ResultDataPoint) => d['Sage Node'] !== null).map((d: ResultDataPoint) => Number(d['Sage Node']))).toFixed(2);
+      return Math.min(
+        ...arr.filter((d: ResultDataPoint) => d['Sage Node'] !== null).map((d: ResultDataPoint) => Number(d['Sage Node']))
+      ).toFixed(2);
     }
     if (org === 'Mesonet') {
-      return Math.min(...arr.filter((d: ResultDataPoint) => d['Mesonet'] !== null).map((d: ResultDataPoint) => Number(d['Mesonet']))).toFixed(2);
+      return Math.min(
+        ...arr.filter((d: ResultDataPoint) => d['Mesonet'] !== null).map((d: ResultDataPoint) => Number(d['Mesonet']))
+      ).toFixed(2);
     }
     return;
   };
 
   const getMax = (arr: ResultDataPoint[], org: string) => {
     if (org === 'Sage Node') {
-      return Math.max(...arr.filter((d: ResultDataPoint) => d['Sage Node'] !== null).map((d: ResultDataPoint) => Number(d['Sage Node']))).toFixed(2);
+      return Math.max(
+        ...arr.filter((d: ResultDataPoint) => d['Sage Node'] !== null).map((d: ResultDataPoint) => Number(d['Sage Node']))
+      ).toFixed(2);
     }
     if (org === 'Mesonet') {
-      return Math.max(...arr.filter((d: ResultDataPoint) => d['Mesonet'] !== null).map((d: ResultDataPoint) => Number(d['Mesonet']))).toFixed(2);
+      return Math.max(
+        ...arr.filter((d: ResultDataPoint) => d['Mesonet'] !== null).map((d: ResultDataPoint) => Number(d['Mesonet']))
+      ).toFixed(2);
     }
     return;
   };
@@ -40,39 +48,47 @@ function Overview({ s }: RAPIDState): JSX.Element {
   return (
     <>
       {s.metricData ? (
-        <Box padding="5">
-          <h1>Overview</h1>
-          <div> Average Measurements Over 24 Hours</div>
-          <h1>Sage Node</h1>
-          <Box display="flex">
-            <Stat>
-              <StatLabel>Average {s.metric.NAME}</StatLabel>
-              <StatNumber>{getAverage(s.metricData.data, 'Sage Node')}</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Min {s.metric.NAME}</StatLabel>
-              <StatNumber>{getMin(s.metricData.data, 'Sage Node')}</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Max {s.metric.NAME}</StatLabel>
-              <StatNumber>{getMax(s.metricData.data, 'Sage Node')}</StatNumber>
-            </Stat>
+        <Box padding="5" display="flex" flexDir="column" height="100%">
+          <Box mb="10">
+            <h1>Average Measurements Over 24 Hours</h1>
           </Box>
 
-          <Box textAlign="center">Mesonet</Box>
-          <Box display="flex" justifyContent="center">
-            <Stat>
-              <StatLabel>Average {s.metric.NAME}</StatLabel>
-              <StatNumber>{getAverage(s.metricData.data, 'Mesonet')}</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Min {s.metric.NAME}</StatLabel>
-              <StatNumber>{getMin(s.metricData.data, 'Mesonet')}</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Max {s.metric.NAME}</StatLabel>
-              <StatNumber>{getMax(s.metricData.data, 'Mesonet')}</StatNumber>
-            </Stat>
+          <Box>
+            <Box textAlign="center">
+              <h1>Sage Node</h1>
+            </Box>
+            <Box display="flex">
+              <Stat>
+                <StatLabel>Average {s.metric.NAME}</StatLabel>
+                <StatNumber>{getAverage(s.metricData.data, 'Sage Node')}</StatNumber>
+              </Stat>
+              <Stat>
+                <StatLabel>Min {s.metric.NAME}</StatLabel>
+                <StatNumber>{getMin(s.metricData.data, 'Sage Node')}</StatNumber>
+              </Stat>
+              <Stat>
+                <StatLabel>Max {s.metric.NAME}</StatLabel>
+                <StatNumber>{getMax(s.metricData.data, 'Sage Node')}</StatNumber>
+              </Stat>
+            </Box>
+
+            <Box textAlign="center">
+              <h1>Mesonet</h1>
+            </Box>
+            <Box display="flex" justifyContent="center">
+              <Stat>
+                <StatLabel>Average {s.metric.NAME}</StatLabel>
+                <StatNumber>{getAverage(s.metricData.data, 'Mesonet')}</StatNumber>
+              </Stat>
+              <Stat>
+                <StatLabel>Min {s.metric.NAME}</StatLabel>
+                <StatNumber>{getMin(s.metricData.data, 'Mesonet')}</StatNumber>
+              </Stat>
+              <Stat>
+                <StatLabel>Max {s.metric.NAME}</StatLabel>
+                <StatNumber>{getMax(s.metricData.data, 'Mesonet')}</StatNumber>
+              </Stat>
+            </Box>
           </Box>
         </Box>
       ) : (
