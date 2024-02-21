eb6b03f352e297fa8dbd164328694162e89773ef
Ryan Theriot <rtheriot@hawaii.edu>
2024-02-20 14:42:24 -1000

    Presence Settings panel/modal and clock area button added. Added 'settings' to userSchema

--- webstack/libs/shared/src/lib/types/schemas/user.ts
+++ webstack/libs/shared/src/lib/types/schemas/user.ts
@@ -36,6 +36,17 @@ const schema = z.object({
   savedBoards: z.array(z.string()),
   // Recent Boards
   recentBoards: z.array(z.string()),
+  // Settings
+  settings: z.object({
+    // Show the cursor of the user
+    showCursor: z.boolean(),
+    // Show other users cursors
+    showOthersCursors: z.boolean(),
+    // Show the viewport of the user
+    showViewport: z.boolean(),
+    // Show other users viewports
+    showOthersViewports: z.boolean(),
+  }),
 });
 
 export type UserSchema = z.infer<typeof schema>;
