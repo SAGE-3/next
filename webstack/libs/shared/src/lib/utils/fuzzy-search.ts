61be8f6785e0bcb4d45f55b808a19dcd6b160d3b
Christopher Lee <clee48@hawaii.edu>
2024-05-29 15:23:01 -1000

    cleaned code

--- webstack/libs/shared/src/lib/utils/fuzzy-search.ts
+++ webstack/libs/shared/src/lib/utils/fuzzy-search.ts
@@ -37,17 +37,4 @@ export const fuzzySearch = (text: string, query: string, fuse?: Fuse<{ text: str
   fuse.setCollection([{ text: normalizedText }]);
   const result = fuse.search(normalizedQuery);
   return result.length > 0;
-  
-  ///// Fallback, delete after 6/29/24
-  // A simple fuzzy search implementation that checks if all words in the query are present in the text.
-  // This is not the official fuzzy search algorithm that uses levenshtein distance, just a cheap imitation to achieve fuzzy effects.
-  // "Hello World" -> regex("h.*e.*l.*l.*o") && regex("w.*o.*r.*l.*d")
-  // return normalizedQuery.split(' ').every((word) => {
-  //   // For cleaned words: specifically we do not want the user to input .*,(abc),etc. that could affect the regex.
-  //   const cleanedWord = word.replace(/[^a-z0-9]/g, '');
-  //   const pattern = cleanedWord.split('').join('.*');
-  //   const regex = new RegExp(pattern);
-  //   return regex.test(normalizedText);
-  // });
-  ////
 };
