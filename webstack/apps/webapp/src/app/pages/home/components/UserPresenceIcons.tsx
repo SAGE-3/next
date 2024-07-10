96e577e70fe66766031b45fa462e2e2944352ea1
renambot <renambot@gmail.com>
2024-07-09 19:50:44 -0500

    fix: display avatar initials properly

--- webstack/apps/webapp/src/app/pages/home/components/UserPresenceIcons.tsx
+++ webstack/apps/webapp/src/app/pages/home/components/UserPresenceIcons.tsx
@@ -6,32 +6,36 @@
  * the file LICENSE, distributed as part of this software.
  */
 
-import { useColorModeValue, Box, Avatar, AvatarBadge, AvatarGroup, BoxProps} from '@chakra-ui/react';
+import { useColorModeValue, Box, Avatar, AvatarGroup, BoxProps, MenuButton } from '@chakra-ui/react';
 
-import { useHexColor, useUsersStore } from '@sage3/frontend';
+import { useHexColor, useUsersStore, initials } from '@sage3/frontend';
 import { User, Presence } from '@sage3/shared/types';
-
-// Required to Fix React Hook Order Changes
-function UserIcons(props: { users: User[], currentUser: Presence, anonymousNames?: boolean }){
-  const user = props.users.find((user) => user._id === props.currentUser.data.userId)
-  const backgroundColorValue = useColorModeValue(`${user?.data.color}.600`, `${user?.data.color}.400`);
-  const backgroundColor = useHexColor(backgroundColorValue);
-  return <Avatar name={props.anonymousNames ? undefined : user?.data.name} color={"var(--chakra-colors-body-text)"} backgroundColor={backgroundColor} borderColor={"grey.400"}/>
-}
 
 export function UserPresenceIcons(props: { usersPresent: Presence[], maxUsersDisplayed: number, anonymousNames?: boolean } & Omit<BoxProps, 'usersPresent' | 'maxUsersDisplayed' | 'anonymousNames'>) {
   const { usersPresent, maxUsersDisplayed, anonymousNames, ...boxProps } = props;
   const { users } = useUsersStore((state) => state);
   return (
-        <Box {...boxProps}>
-          <AvatarGroup size='md' max={props.maxUsersDisplayed}>
-            {props.usersPresent.map((pUser, index) => 
-              <Box key={index}>
-                <UserIcons users={users} currentUser={pUser} anonymousNames={props.anonymousNames}/>
-              </Box>
-            )}
-          </AvatarGroup>
-        </Box>
+    <Box {...boxProps}>
+      <AvatarGroup size='md' max={props.maxUsersDisplayed}>
+        {props.usersPresent.map((pUser, index) =>
+          <Box key={index}>
+            <UserIcons users={users} currentUser={pUser} anonymousNames={props.anonymousNames} />
+          </Box>
+        )}
+      </AvatarGroup>
+    </Box>
 
   );
+}
+
+// Required to Fix React Hook Order Changes
+function UserIcons(props: { users: User[], currentUser: Presence, anonymousNames?: boolean }) {
+  const user = props.users.find((user) => user._id === props.currentUser.data.userId)
+  const backgroundColorValue = useColorModeValue(`${user?.data.color}.600`, `${user?.data.color}.400`);
+  const backgroundColor = useHexColor(backgroundColorValue);
+  const name = props.anonymousNames ? undefined : user ? initials(user.data.name) : undefined;
+  return <Avatar name={' '} color={"var(--chakra-colors-body-text)"} backgroundColor={backgroundColor} borderColor={"grey.400"}
+    textAlign="center" whiteSpace="nowrap">
+    {name}
+  </Avatar>
 }
