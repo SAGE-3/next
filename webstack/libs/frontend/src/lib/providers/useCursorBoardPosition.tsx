f1c784f5bf15863d77e885cc24fed79210f22f2b
Ryan Theriot <rtheriot@hawaii.edu>
2024-08-27 09:32:53 -1000

    Board cursor logic change

--- webstack/libs/frontend/src/lib/providers/useCursorBoardPosition.tsx
+++ webstack/libs/frontend/src/lib/providers/useCursorBoardPosition.tsx
@@ -8,15 +8,7 @@
 
 import { useCallback, useEffect, useState, createContext, useContext } from 'react';
 import { useUIStore } from '../stores';
-
-const GLOBAL_CURSOR = { x: 0, y: 0 };
-
-function handleCursorUpdate(event: MouseEvent) {
-  GLOBAL_CURSOR.x = event.clientX;
-  GLOBAL_CURSOR.y = event.clientY;
-}
-
-window.addEventListener('mousemove', handleCursorUpdate);
+import { throttle } from 'throttle-debounce';
 
 type CursorBoardPositionContextType = {
   boardCursor: { x: number; y: number };
@@ -41,49 +33,32 @@ export function useCursorBoardPosition() {
   return useContext(CursorBoardPositionContext);
 }
 
-// Interval to update the cursor position within react provider
-let updateCursorInterval = null as null | number;
-let updateBoardCursorInterval = null as null | number;
-const updateFrequency = 100;
-
 export function CursorBoardPositionProvider(props: React.PropsWithChildren<Record<string, unknown>>) {
   const [boardCursor, setBoardCursor] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const [cursor, setCursor] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const boardPosition = useUIStore((state) => state.boardPosition);
   const scale = useUIStore((state) => state.scale);
 
-  // Update the cursor position useEffect
-  useEffect(() => {
-    updateCursorInterval = window.setInterval(() => {
-      // Check if the cursor is value changed
-      if (GLOBAL_CURSOR.x === cursor.x && GLOBAL_CURSOR.y === cursor.y) {
-        return;
-      }
-      setCursor({
-        x: GLOBAL_CURSOR.x,
-        y: GLOBAL_CURSOR.y,
-      });
-    }, updateFrequency);
-    return () => {
-      if (updateCursorInterval) {
-        window.clearInterval(updateCursorInterval);
-      }
-    };
-  }, []);
+  // Throttle Functions for the cursor mousemove
+  const throttleMove = throttle(100, (e: any) => {
+    setCursor({ x: e.clientX, y: e.clientY });
+    // Calculate Board Position
+    const boardX = Math.floor(e.clientX / scale - boardPosition.x);
+    const boardY = Math.floor(e.clientY / scale - boardPosition.y);
+    setBoardCursor({ x: boardX, y: boardY });
+  });
+  const throttleMoveRef = useCallback(throttleMove, [boardPosition.x, boardPosition.y, scale]);
 
-  // Update the board cursor position
+  // UseEffect to update the cursor position
   useEffect(() => {
-    updateBoardCursorInterval = window.setInterval(() => {
-      const boardX = Math.floor(GLOBAL_CURSOR.x / scale - boardPosition.x);
-      const boardY = Math.floor(GLOBAL_CURSOR.y / scale - boardPosition.y);
-      setBoardCursor({ x: boardX, y: boardY });
-    }, updateFrequency);
-    return () => {
-      if (updateBoardCursorInterval) {
-        clearInterval(updateBoardCursorInterval);
-      }
+    const updateCursor = (e: MouseEvent) => {
+      throttleMoveRef(e);
     };
-  }, [boardPosition.x, boardPosition.y, scale]);
+    window.addEventListener('mousemove', updateCursor);
+    return () => {
+      window.removeEventListener('mousemove', updateCursor);
+    };
+  }, [throttleMoveRef]);
 
   const uiToBoard = useCallback(
     (x: number, y: number) => {
