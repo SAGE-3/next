0e23f6f114d4ca843cae693066f5d4614d2a7f2c
Christopher Lee <clee48@hawaii.edu>
2024-09-19 15:29:57 -1000

    updated copyright

--- webstack/apps/webapp/src/app/pages/board/layers/ui/components/Interactionbar.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/ui/components/Interactionbar.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) SAGE3 Development Team 2022. All Rights Reserved
+ * Copyright (c) SAGE3 Development Team 2024. All Rights Reserved
  * University of Hawaii, University of Illinois Chicago, Virginia Tech
  *
  * Distributed under the terms of the SAGE3 License.  The full license is in
