eb5d9e0979042d9a4fff78ae8409f466849a1c6a
Giorgio <tran.giorgio@gmail.com>
2024-09-24 11:36:36 -1000

    Add useCallback to the OnDrop event in DragAndDropBoard

--- webstack/apps/webapp/src/app/pages/board/layers/background/components/DragAndDropBoard.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/background/components/DragAndDropBoard.tsx
@@ -6,7 +6,7 @@
  * the file LICENSE, distributed as part of this software.
  */
 
-import { ReactNode, useState } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 import {
   Button,
   useToast,
@@ -79,157 +79,160 @@ export const useDragAndDropBoard = (props: useDragAndDropBoardProps) => {
   };
 
   // Drop event
-  async function OnDrop(event: React.DragEvent<HTMLOrSVGElement>) {
-    if (!user) return;
+  const OnDrop = useCallback(
+    async (event: React.DragEvent<HTMLOrSVGElement>) => {
+      if (!user) return;
 
-    if (!canDrop) {
-      toast({
-        title: 'Guests and Spectators cannot upload assets',
-        status: 'warning',
-        duration: 4000,
-        isClosable: true,
-      });
-      return;
-    }
-    // Get the position of the drop
-    const xdrop = event.nativeEvent.offsetX;
-    const ydrop = event.nativeEvent.offsetY;
-    setDropCursor({ x: event.clientX, y: event.clientY });
-    setDropPosition({ x: xdrop, y: ydrop });
+      if (!canDrop) {
+        toast({
+          title: 'Guests and Spectators cannot upload assets',
+          status: 'warning',
+          duration: 4000,
+          isClosable: true,
+        });
+        return;
+      }
+      // Get the position of the drop
+      const xdrop = event.nativeEvent.offsetX;
+      const ydrop = event.nativeEvent.offsetY;
+      setDropCursor({ x: event.clientX, y: event.clientY });
+      setDropPosition({ x: xdrop, y: ydrop });
 
-    if (event.dataTransfer.types.includes('Files') && event.dataTransfer.files.length > 0) {
-      event.preventDefault();
-      event.stopPropagation();
-      // Collect all the files dropped into an array
-      collectFiles(event.dataTransfer)
-        .then(async (files) => {
-          if (!uploadInProgress) {
-            toast.closeAll();
-            // do the actual upload
-            uploadFiles(Array.from(files), xdrop, ydrop, props.roomId, props.boardId);
-          } else {
+      if (event.dataTransfer.types.includes('Files') && event.dataTransfer.files.length > 0) {
+        event.preventDefault();
+        event.stopPropagation();
+        // Collect all the files dropped into an array
+        collectFiles(event.dataTransfer)
+          .then(async (files) => {
+            if (!uploadInProgress) {
+              toast.closeAll();
+              // do the actual upload
+              uploadFiles(Array.from(files), xdrop, ydrop, props.roomId, props.boardId);
+            } else {
+              toast({
+                title: 'Upload in progress - Please wait',
+                status: 'warning',
+                duration: 4000,
+                isClosable: true,
+              });
+            }
+          })
+          .catch((err) => {
+            console.log('Error> uploading files', err);
+            lotsOnOpen();
+          });
+      } else {
+        // Drag/Drop a URL
+        if (event.dataTransfer.types.includes('text/uri-list')) {
+          event.preventDefault();
+          event.stopPropagation();
+
+          // Block guests from uploading assets
+          if (auth?.provider === 'guest') {
             toast({
-              title: 'Upload in progress - Please wait',
+              title: 'Guests cannot upload assets',
               status: 'warning',
               duration: 4000,
               isClosable: true,
             });
+            return;
           }
-        })
-        .catch((err) => {
-          console.log('Error> uploading files', err);
-          lotsOnOpen();
-        });
-    } else {
-      // Drag/Drop a URL
-      if (event.dataTransfer.types.includes('text/uri-list')) {
-        event.preventDefault();
-        event.stopPropagation();
 
-        // Block guests from uploading assets
-        if (auth?.provider === 'guest') {
-          toast({
-            title: 'Guests cannot upload assets',
-            status: 'warning',
-            duration: 4000,
-            isClosable: true,
-          });
-          return;
-        }
-
-        const pastedText = event.dataTransfer.getData('Url');
-        if (pastedText) {
-          if (pastedText.startsWith('data:image/png;base64')) {
-            const title = event.dataTransfer.getData('title') || 'Image';
-            // it's a base64 image
-            getImageDimensionsFromBase64(pastedText).then((res) => {
-              const ar = res.w / res.h;
-              let w = res.w;
-              let h = w / ar;
-              if (ar < 1) {
-                w = res.h * ar;
-                h = res.h;
+          const pastedText = event.dataTransfer.getData('Url');
+          if (pastedText) {
+            if (pastedText.startsWith('data:image/png;base64')) {
+              const title = event.dataTransfer.getData('title') || 'Image';
+              // it's a base64 image
+              getImageDimensionsFromBase64(pastedText).then((res) => {
+                const ar = res.w / res.h;
+                let w = res.w;
+                let h = w / ar;
+                if (ar < 1) {
+                  w = res.h * ar;
+                  h = res.h;
+                }
+                createApp(setupApp(title, 'ImageViewer', xdrop, ydrop, props.roomId, props.boardId, { w, h }, { assetid: pastedText }));
+              });
+            } else {
+              // Is it a valid URL
+              const valid = isValidURL(pastedText);
+              if (valid) {
+                // Create a link or view app
+                popOnOpen();
+                setValidURL(valid);
               }
-              createApp(setupApp(title, 'ImageViewer', xdrop, ydrop, props.roomId, props.boardId, { w, h }, { assetid: pastedText }));
-            });
-          } else {
-            // Is it a valid URL
-            const valid = isValidURL(pastedText);
-            if (valid) {
-              // Create a link or view app
-              popOnOpen();
-              setValidURL(valid);
             }
           }
-        }
-      } else {
-        // if no files were dropped, create an application
-        const appName = event.dataTransfer.getData('app') as AppName;
-        if (appName) {
-          // Setup initial size
-          let w = 600;
-          let h = 400;
-          if (appName === 'SageCell') {
-            w = 650;
-            h = 400;
-          } else if (appName === 'Calculator') {
-            w = 260;
-            h = 369;
-          } else if (appName === 'Chat') {
-            w = 800;
-            h = 420;
-          }
-          // if a specific app was setup, create it
-          const appstatestr = event.dataTransfer.getData('app_state');
-          if (appstatestr) {
-            const appstate = JSON.parse(appstatestr);
-            const newState = {
-              title: user.data.name,
-              roomId: props.roomId,
-              boardId: props.boardId,
-              position: { x: xdrop, y: ydrop, z: 0 },
-              size: { width: w, height: h, depth: 0 },
-              rotation: { x: 0, y: 0, z: 0 },
-              type: appName,
-              state: { ...(initialValues[appName] as AppState), ...appstate },
-              raised: true,
-              dragging: false,
-              pinned: false,
-            };
-            createApp(newState);
-          } else {
-            newApp(appName, w, h, xdrop, ydrop);
-          }
         } else {
-          // Get information from the drop
-          const ids = event.dataTransfer.getData('file');
-          const types = event.dataTransfer.getData('type');
-          if (ids && types) {
-            // if it's files from the asset manager
-            const fileIDs = JSON.parse(ids);
-            const fileTypes = JSON.parse(types);
-            // Open the file at the drop location
-            const num = fileIDs.length;
-            if (num < 20) {
-              const batch: AppSchema[] = [];
-              let xpos = xdrop;
-              for (let i = 0; i < num; i++) {
-                const res = await openAppForFile(fileIDs[i], fileTypes[i], xpos, ydrop, props.roomId, props.boardId);
-                if (res) {
-                  batch.push(res);
-                  xpos += res.size.width + 10;
-                }
-              }
-              createBatch(batch);
+          // if no files were dropped, create an application
+          const appName = event.dataTransfer.getData('app') as AppName;
+          if (appName) {
+            // Setup initial size
+            let w = 600;
+            let h = 400;
+            if (appName === 'SageCell') {
+              w = 650;
+              h = 400;
+            } else if (appName === 'Calculator') {
+              w = 260;
+              h = 369;
+            } else if (appName === 'Chat') {
+              w = 800;
+              h = 420;
+            }
+            // if a specific app was setup, create it
+            const appstatestr = event.dataTransfer.getData('app_state');
+            if (appstatestr) {
+              const appstate = JSON.parse(appstatestr);
+              const newState = {
+                title: user.data.name,
+                roomId: props.roomId,
+                boardId: props.boardId,
+                position: { x: xdrop, y: ydrop, z: 0 },
+                size: { width: w, height: h, depth: 0 },
+                rotation: { x: 0, y: 0, z: 0 },
+                type: appName,
+                state: { ...(initialValues[appName] as AppState), ...appstate },
+                raised: true,
+                dragging: false,
+                pinned: false,
+              };
+              createApp(newState);
             } else {
-              // Too many assets selected, not doing it.
-              lotsOnOpen();
+              newApp(appName, w, h, xdrop, ydrop);
+            }
+          } else {
+            // Get information from the drop
+            const ids = event.dataTransfer.getData('file');
+            const types = event.dataTransfer.getData('type');
+            if (ids && types) {
+              // if it's files from the asset manager
+              const fileIDs = JSON.parse(ids);
+              const fileTypes = JSON.parse(types);
+              // Open the file at the drop location
+              const num = fileIDs.length;
+              if (num < 20) {
+                const batch: AppSchema[] = [];
+                let xpos = xdrop;
+                for (let i = 0; i < num; i++) {
+                  const res = await openAppForFile(fileIDs[i], fileTypes[i], xpos, ydrop, props.roomId, props.boardId);
+                  if (res) {
+                    batch.push(res);
+                    xpos += res.size.width + 10;
+                  }
+                }
+                createBatch(batch);
+              } else {
+                // Too many assets selected, not doing it.
+                lotsOnOpen();
+              }
             }
           }
         }
       }
-    }
-  }
+    },
+    [user]
+  );
 
   const createWeblink = () => {
     createApp(
