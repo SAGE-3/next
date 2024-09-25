939d911e1077c72e150c3785f166ee54186fb193
Christopher Lee <clee48@hawaii.edu>
2024-09-24 15:44:49 -1000

    added code (commented out) to allow for hotkey selection of grab, pen, lasso. eraser

--- webstack/apps/webapp/src/app/pages/board/layers/ui/components/Interactionbar.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/ui/components/Interactionbar.tsx
@@ -12,7 +12,7 @@ import { BiPencil } from 'react-icons/bi';
 import { BsEraserFill } from 'react-icons/bs';
 import { LiaMousePointerSolid, LiaHandPaperSolid } from 'react-icons/lia';
 
-import { useUserSettings, useUser } from '@sage3/frontend';
+import { useUserSettings, useUser, useHotkeys } from '@sage3/frontend';
 
 export function Interactionbar() {
   // Settings
@@ -26,10 +26,82 @@ export function Interactionbar() {
   // const annotations = usePanelStore((state) => state.panels['annotations']);
   // const updatePanel = usePanelStore((state) => state.updatePanel);
 
+  // useHotkeys(
+  //   'h',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('grab');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   's',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('lasso');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   'p',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('pen');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   'e',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('eraser');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   '1',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('grab');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   '2',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('lasso');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   '3',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('pen');
+  //   },
+  //   { dependencies: [] }
+  // );
+
+  // useHotkeys(
+  //   '4',
+  //   (event: KeyboardEvent): void | boolean => {
+  //     event.stopPropagation();
+  //     setPrimaryActionMode('eraser');
+  //   },
+  //   { dependencies: [] }
+  // );
+
   return (
     <>
       <ButtonGroup isAttached size="xs">
-        <Tooltip label={'Grab (Panning Tool)'}>
+        <Tooltip label={'Grab (Panning Tool) — [1]'}>
           <IconButton
             size="sm"
             colorScheme={primaryActionMode === 'grab' ? user?.data.color || 'teal' : 'gray'}
@@ -41,7 +113,7 @@ export function Interactionbar() {
             }}
           ></IconButton>
         </Tooltip>
-        <Tooltip label={'Selection'}>
+        <Tooltip label={'Selection — [2]'}>
           <IconButton
             size="sm"
             colorScheme={primaryActionMode === 'lasso' ? user?.data.color || 'teal' : 'gray'}
@@ -54,7 +126,7 @@ export function Interactionbar() {
           ></IconButton>
         </Tooltip>
 
-        <Tooltip label={'Marker'}>
+        <Tooltip label={'Marker — [3]'}>
           <IconButton
             size="sm"
             colorScheme={primaryActionMode === 'pen' ? user?.data.color || 'teal' : 'gray'}
@@ -66,7 +138,7 @@ export function Interactionbar() {
             }}
           ></IconButton>
         </Tooltip>
-        <Tooltip label={'Eraser'}>
+        <Tooltip label={'Eraser — [4]'}>
           <IconButton
             size="sm"
             colorScheme={primaryActionMode === 'eraser' ? user?.data.color || 'teal' : 'gray'}
