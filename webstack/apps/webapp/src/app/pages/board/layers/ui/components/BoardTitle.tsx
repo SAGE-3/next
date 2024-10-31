03225d1361a86e194ac7cd72cd4930567cf1fa74
renambot <renambot@gmail.com>
2024-10-31 15:15:36 -0500

    fix: lint and pop

--- webstack/apps/webapp/src/app/pages/board/layers/ui/components/BoardTitle.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/ui/components/BoardTitle.tsx
@@ -5,28 +5,11 @@
  * Distributed under the terms of the SAGE3 License.  The full license is in
  * the file LICENSE, distributed as part of this software.
  */
-import {
-  Box,
-  Button,
-  Icon,
-  IconButton,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Text,
-  useColorModeValue,
-  useDisclosure,
-  VStack,
-} from '@chakra-ui/react';
-import { MdArrowBack, MdCloudQueue } from 'react-icons/md';
+import { Box, IconButton, Text, useColorModeValue, } from '@chakra-ui/react';
+import { MdArrowCircleLeft } from 'react-icons/md';
 
-import { StuckTypes, useHexColor, useRouteNav, useUser } from '@sage3/frontend';
+import { useHexColor, useRouteNav, useUser } from '@sage3/frontend';
 import { Board, OpenConfiguration, Room } from '@sage3/shared/types';
-import { IconButtonPanel } from './Panels/Panel';
 
 type BoardTitleProps = {
   config: OpenConfiguration;
@@ -34,18 +17,25 @@ type BoardTitleProps = {
   board?: Board;
 };
 
-// The title of the board which includes the servername, room name, and board name
+
+/**
+ * Component that displays the servername, room name, and board name along with a back button.
+ * The back button allows the user to navigate back to the previous room or the homepage.
+ *
+ * @param {BoardTitleProps} props - The properties for the BoardTitle component.
+ * @param {Object} props.config - Configuration object containing server details.
+ * @param {Object} props.room - Room object containing room details.
+ * @param {Object} props.board - Board object containing board details.
+ *
+ * @returns {JSX.Element} The rendered BoardTitle component.
+ */
 export function BoardTitle(props: BoardTitleProps) {
-  const title = `${props.config?.serverName} / ${
-    (props.room?.data.name ? props.room.data.name : '') + ' / ' + (props.board?.data.name ? props.board.data.name : '')
-  }`;
-
-  const textColor = useColorModeValue('gray.800', 'gray.50');
-
   const { user } = useUser();
   const usersColor = user ? user.data.color : 'teal';
   const backButtonColorMode = useColorModeValue(`${usersColor}.500`, `${usersColor}.300`);
+  const textColor = useColorModeValue('gray.800', 'gray.50');
   const backButtonColor = useHexColor(user ? backButtonColorMode : textColor);
+  const title = `${props.config?.serverName} / ${(props.room?.data.name ? props.room.data.name : '') + ' / ' + (props.board?.data.name ? props.board.data.name : '')}`;
 
   // Redirect the user back to the homepage when clicking the arrow button
   const { toHome, back } = useRouteNav();
@@ -66,7 +56,6 @@ export function BoardTitle(props: BoardTitleProps) {
   return (
     <Box
       borderRadius="md"
-      // backgroundColor={backgroundColor}
       whiteSpace={'nowrap'}
       width="100%"
       display="flex"
@@ -75,7 +64,7 @@ export function BoardTitle(props: BoardTitleProps) {
       ml="1"
     >
       <IconButton
-        icon={<MdArrowBack />}
+        icon={<MdArrowCircleLeft fontSize='24px' />}
         isActive={false}
         onClick={handleHomeClick}
         aria-label={''}
@@ -85,7 +74,6 @@ export function BoardTitle(props: BoardTitleProps) {
         fontSize="2xl"
         _hover={{ transform: 'scale(1.2)' }}
       />
-
       <Text fontSize={'lg'} color={textColor} userSelect="none" ml="1" whiteSpace="nowrap">
         {title}
       </Text>
