54e2050f5e0354ae7e5ca037c33f52db488c2093
Ryan Theriot <rtheriot@hawaii.edu>
2024-10-30 15:36:49 -1000

    Moved backbutton from control panel to Board Title component

--- webstack/apps/webapp/src/app/pages/board/layers/ui/components/BoardTitle.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/ui/components/BoardTitle.tsx
@@ -5,11 +5,28 @@
  * Distributed under the terms of the SAGE3 License.  The full license is in
  * the file LICENSE, distributed as part of this software.
  */
-import { Box, Text, useColorModeValue } from '@chakra-ui/react';
-import { MdCloudQueue } from 'react-icons/md';
+import {
+  Box,
+  Button,
+  Icon,
+  IconButton,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { MdArrowBack, MdCloudQueue } from 'react-icons/md';
 
-import { useHexColor } from '@sage3/frontend';
+import { StuckTypes, useHexColor, useRouteNav, useUser } from '@sage3/frontend';
 import { Board, OpenConfiguration, Room } from '@sage3/shared/types';
+import { IconButtonPanel } from './Panels/Panel';
 
 type BoardTitleProps = {
   config: OpenConfiguration;
@@ -19,12 +36,32 @@ type BoardTitleProps = {
 
 // The title of the board which includes the servername, room name, and board name
 export function BoardTitle(props: BoardTitleProps) {
-  const title = `${props.config?.serverName} / ${(props.room?.data.name ? props.room.data.name : '') + ' / ' + (props.board?.data.name ? props.board.data.name : '')
-    }`;
+  const title = `${props.config?.serverName} / ${
+    (props.room?.data.name ? props.room.data.name : '') + ' / ' + (props.board?.data.name ? props.board.data.name : '')
+  }`;
 
   const textColor = useColorModeValue('gray.800', 'gray.50');
-  const textColorHex = useHexColor(textColor);
-  // const backgroundColor = useColorModeValue('#ffffff69', '#22222269');
+
+  const { user } = useUser();
+  const usersColor = user ? user.data.color : 'teal';
+  const backButtonColorMode = useColorModeValue(`${usersColor}.500`, `${usersColor}.300`);
+  const backButtonColor = useHexColor(user ? backButtonColorMode : textColor);
+
+  // Redirect the user back to the homepage when clicking the arrow button
+  const { toHome, back } = useRouteNav();
+  function handleHomeClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (event.shiftKey) {
+      // Just go back to the previous room
+      back();
+    } else {
+      if (props.room) {
+        // Back to the homepage with the room id
+        toHome(props.room._id);
+      } else {
+        back();
+      }
+    }
+  }
 
   return (
     <Box
@@ -33,12 +70,23 @@ export function BoardTitle(props: BoardTitleProps) {
       whiteSpace={'nowrap'}
       width="100%"
       display="flex"
-      px={2}
       justifyContent="left"
       alignItems={'center'}
+      ml="1"
     >
-      <MdCloudQueue fontSize={'22px'} color={textColorHex} />
-      <Text fontSize={'lg'} color={textColor} ml="2" userSelect="none" whiteSpace="nowrap">
+      <IconButton
+        icon={<MdArrowBack />}
+        isActive={false}
+        onClick={handleHomeClick}
+        aria-label={''}
+        size="xs"
+        variant={'unstyled'}
+        color={backButtonColor}
+        fontSize="2xl"
+        _hover={{ transform: 'scale(1.2)' }}
+      />
+
+      <Text fontSize={'lg'} color={textColor} userSelect="none" ml="1" whiteSpace="nowrap">
         {title}
       </Text>
     </Box>
