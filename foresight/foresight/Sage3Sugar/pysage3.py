6356c0b69bcab29031c52928c40150843786d471
renambot <renambot@gmail.com>
2024-12-04 12:31:41 -0600

    new: sage3 python lib tracks assets

--- foresight/foresight/Sage3Sugar/pysage3.py
+++ foresight/foresight/Sage3Sugar/pysage3.py
@@ -45,10 +45,12 @@ class PySage3:
         }
 
         self.rooms = {}
+        self.assets = {}
         self.s3_comm = SageCommunication(self.conf, self.prod_type)
         self.socket = SageWebsocket(on_message_fn=self.__process_messages)
 
-        self.socket.subscribe(["/api/apps", "/api/rooms", "/api/boards"])
+        self.socket.subscribe(["/api/apps", "/api/rooms", "/api/boards", "/api/assets"])
+
         # Grab and load info already on the board
         self.__populate_existing()
         self.room = None
@@ -68,6 +70,10 @@ class PySage3:
         apps_info = self.s3_comm.get_apps()
         for app_info in apps_info:
             self.__handle_create("APPS", app_info)
+        # Populate existing apps
+        assets_info = self.s3_comm.get_assets()
+        for asset_info in assets_info:
+            self.__handle_create("ASSETS", asset_info)
 
     def create_app(self, room_id, board_id, app_type, state, app=None):
         try:
@@ -190,6 +196,9 @@ class PySage3:
                     self.rooms[room_id].boards[board_id].smartbits[
                         smartbit.app_id
                     ] = smartbit
+        elif collection == "ASSETS":
+            self.assets[doc["_id"]] = doc
+            print("Asset created", doc["data"]["originalfilename"])
 
     # Handle Update Messages
     def __handle_update(self, collection, doc, updates):
