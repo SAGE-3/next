60e9a5af24e48a0eaed621bff5c86840e008e893
renambot <renambot@gmail.com>
2024-12-20 15:45:26 -0600

    lint

--- webstack/libs/applications/src/lib/apps/ImageViewer/ImageViewer.tsx
+++ webstack/libs/applications/src/lib/apps/ImageViewer/ImageViewer.tsx
@@ -7,7 +7,7 @@
  */
 
 import { useEffect, useState } from 'react';
-import { Image, Button, ButtonGroup, Tooltip, Box, Text } from '@chakra-ui/react';
+import { Image, Button, ButtonGroup, Tooltip, Box } from '@chakra-ui/react';
 // Icons
 import { MdBrokenImage, MdFileDownload, MdImage } from 'react-icons/md';
 import { HiPencilAlt } from 'react-icons/hi';
@@ -134,24 +134,24 @@ function AppComponent(props: App): JSX.Element {
 
             {s.boxes && Array.isArray(s.boxes)
               ? s.boxes.map((box, idx) => {
-                  // TODO Need to handle text overflow for labels
-                  return (
-                    <Box
-                      key={'label' + idx}
-                      position="absolute"
-                      left={box.xmin * (displaySize.width / origSizes.width) + 'px'}
-                      top={box.ymin * (displaySize.height / origSizes.height) + 'px'}
-                      width={(box.xmax - box.xmin) * (displaySize.width / origSizes.width) + 'px'}
-                      height={(box.ymax - box.ymin) * (displaySize.height / origSizes.height) + 'px'}
-                      border="2px solid red"
-                      style={{ display: s.annotations === true ? 'block' : 'none' }}
-                    >
-                      <Box position="relative" top={'-1.5rem'} fontWeight={'bold'} textColor={'black'}>
-                        {box.label || 'Unknown'}
-                      </Box>
+                // TODO Need to handle text overflow for labels
+                return (
+                  <Box
+                    key={'label' + idx}
+                    position="absolute"
+                    left={box.xmin * (displaySize.width / origSizes.width) + 'px'}
+                    top={box.ymin * (displaySize.height / origSizes.height) + 'px'}
+                    width={(box.xmax - box.xmin) * (displaySize.width / origSizes.width) + 'px'}
+                    height={(box.ymax - box.ymin) * (displaySize.height / origSizes.height) + 'px'}
+                    border="2px solid red"
+                    style={{ display: s.annotations === true ? 'block' : 'none' }}
+                  >
+                    <Box position="relative" top={'-1.5rem'} fontWeight={'bold'} textColor={'black'}>
+                      {box.label || 'Unknown'}
                     </Box>
-                  );
-                })
+                  </Box>
+                );
+              })
               : null}
           </>
         ) : (
