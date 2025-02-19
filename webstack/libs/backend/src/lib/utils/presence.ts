b0ef92d84f9ecb4c08d13f3df032cb31278df4ca
Ryan Theriot <rtheriot@hawaii.edu>
2025-02-18 14:59:58 -1000

    Using REDIS to sync presence across all node_server replicas. Uses TTL and giving sockets a unique ID

--- webstack/libs/backend/src/lib/utils/presence.ts
+++ webstack/libs/backend/src/lib/utils/presence.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) SAGE3 Development Team 2022. All Rights Reserved
+ * Copyright (c) SAGE3 Development Team 2025. All Rights Reserved
  * University of Hawaii, University of Illinois Chicago, Virginia Tech
  *
  * Distributed under the terms of the SAGE3 License.  The full license is in
@@ -9,91 +9,124 @@
 import { SAGE3Collection } from '../generics';
 import { WebSocket } from 'ws';
 import { PresenceSchema } from '@sage3/shared/types';
+import { createClient, RedisClientType } from 'redis';
+import { genId } from '@sage3/shared';
 
-// Dictionary to keep track of users if they sign in multiple times
-const userPresenceDict = {} as { [key: string]: SAGEPresence[] };
+// REDIS class to sync presence across multiple node-server replicas
+class RedisPresence {
+  private _redisClient!: RedisClientType;
+  private _path!: string;
+  private _collection!: SAGE3Collection<PresenceSchema>;
+
+  async init(redisUrl: string, prefix: string, presenceCollection: SAGE3Collection<PresenceSchema>) {
+    this._redisClient = createClient({ url: redisUrl });
+    this._path = `${prefix}:SOCKET:PRESENCE`;
+    this._collection = presenceCollection;
+    await this._redisClient.connect();
+
+    // Set interval to check all users
+    setInterval(() => this.AllUserCheck(), 30);
+  }
+
+  // Go over all presence collection and find everyone that is online.
+  // Check they are online by going over the redis socket connections and ensure that user is online
+  private async AllUserCheck() {
+    const usersOnline = await this._collection.query('status', 'online');
+    if (!usersOnline) return;
+    // Get all userIds
+    const uids = usersOnline.map((el) => el.data.userId);
+    uids.forEach((uid) => this.checkAndUpdate(uid));
+  }
+
+  // Add a socket connection by a user to the database
+  public async addSocket(socketId: string, uid: string) {
+    // Add a socket to the redis path `THIS.PATH:SOCKETID:UID with a TTL of 30 secs
+    this._redisClient.set(`${this._path}:${socketId}:${uid}`, 1, { EX: 30 });
+    // Update the presence collection with the new socket
+    // Check if the user is already in the presence collection
+    const check = await this._collection.get(uid);
+    if (!check) {
+      // If not, add the user to the presence collection
+      const presence = {
+        userId: uid,
+        status: 'online',
+        roomId: '',
+        boardId: '',
+        cursor: { x: 0, y: 0, z: 0 },
+        viewport: {
+          position: { x: 0, y: 0, z: 0 },
+          size: { width: 0, height: 0, depth: 0 },
+        },
+      } as PresenceSchema;
+      await this._collection.add(presence, uid, uid);
+    } else {
+      // If the user is already in the presence collection, update the status to online
+      await this._collection.update(uid, uid, { status: 'online' });
+    }
+  }
+
+  // Refresh the socket TTL
+  public async refreshSocket(socketId: string, uid: string) {
+    // Refresh the TTL of the socket
+    this._redisClient.expire(`${this._path}:${socketId}:${uid}`, 30);
+  }
+
+  public async removeSocket(socketId: string, uid: string) {
+    // Remove the socket from the redis path
+    this._redisClient.del(`${this._path}:${socketId}:${uid}`);
+    // Check if the user is still connected
+    this.checkAndUpdate(uid);
+  }
+
+  // Check if the user is online. If not, set them offline
+  private async checkAndUpdate(uid: string) {
+    const online = await this.checkUser(uid);
+    if (!online) {
+      await this._collection.update(uid, uid, { status: 'offline' });
+    }
+  }
+
+  // Is this user connected on any of the server replicas?
+  private async checkUser(uid: string) {
+    // Check if the user is still connected. the path is SAGE3:SOCKET:PRESENCE:SOCKETID:UID
+    const sockets = await this._redisClient.keys(`${this._path}:*:${uid}`);
+    return sockets.length > 0;
+  }
+}
+
+export const redisPresence = new RedisPresence();
 
 /**
  * Class to help with the management of presence of users connected to the server.
  */
 export class SAGEPresence {
   private _userId: string;
+  private _socketId: string;
   private _socket: WebSocket;
-  private _collection: SAGE3Collection<PresenceSchema>;
 
   // Constructor.
   // Sets listeners on socket for disconnects and erros so server can update presenceCollection.
-  constructor(userId: string, socket: WebSocket, presenceCollection: SAGE3Collection<PresenceSchema>) {
+  constructor(userId: string, socket: WebSocket) {
     this._userId = userId;
     this._socket = socket;
-    this._collection = presenceCollection;
+
+    this._socketId = genId();
+
+    redisPresence.addSocket(this._socketId, this._userId);
+
+    // Refresh Key every 15 seconds
+    setInterval(() => {
+      redisPresence.refreshSocket(this._socketId, this._userId);
+    }, 15 * 1000);
 
     this._socket.on('close', () => {
       console.log(`Presence> ${this._userId} disconnected.`);
-      this.onDisconnect();
+      redisPresence.removeSocket(this._socketId, this._userId);
     });
 
     this._socket.on('error', () => {
       console.log(`Presence> ${this._userId} disconnected.`);
-      this.onDisconnect();
+      redisPresence.removeSocket(this._socketId, this._userId);
     });
-  }
-
-  // Due eo the CheckPresnece being an async call,
-  // We need async init call since a constructor can not be async.
-  public async init() {
-    const check = await this.checkPresence();
-    if (!check) {
-      this.addPresence();
-    }
-    this.addToDict();
-  }
-
-  // On Close handler
-  private async onDisconnect() {
-    this.removeFromDict();
-    // If the user's array in the dict is empty, remove the user from the presence collection
-    if (userPresenceDict[this._userId].length === 0) {
-      this.removePresence();
-    }
-  }
-
-  // Add to the dict
-  public async addToDict() {
-    userPresenceDict[this._userId] = userPresenceDict[this._userId] ? [...userPresenceDict[this._userId], this] : [this];
-  }
-
-  // Remove from the dict
-  private async removeFromDict() {
-    userPresenceDict[this._userId] = userPresenceDict[this._userId].filter((presence) => presence !== this);
-  }
-
-  // Check if the user's presence already exists
-  private async checkPresence(): Promise<boolean> {
-    const check = await this._collection.get(this._userId);
-    return check !== null;
-  }
-
-  // Helper function to add the user's presence to the collection
-  private async addPresence(): Promise<boolean> {
-    const presence = {
-      userId: this._userId,
-      status: 'online',
-      roomId: '',
-      boardId: '',
-      cursor: { x: 0, y: 0, z: 0 },
-      viewport: {
-        position: { x: 0, y: 0, z: 0 },
-        size: { width: 0, height: 0, depth: 0 },
-      },
-    } as PresenceSchema;
-    const res = await this._collection.add(presence, this._userId, this._userId);
-    return res !== undefined;
-  }
-
-  // Remove the doc from the presence collection when the user goes offline
-  private async removePresence(): Promise<boolean> {
-    const res = await this._collection.delete(this._userId);
-    return res !== undefined;
   }
 }
