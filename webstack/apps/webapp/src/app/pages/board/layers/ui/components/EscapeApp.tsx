b505de1bb30bc00dc7eab81fa3dd20c2c5883992
Chris <clee48@hawaii.edu>
2025-04-09 10:59:19 -1000

    Changed icon to MdDeselect

--- webstack/apps/webapp/src/app/pages/board/layers/ui/components/EscapeApp.tsx
+++ webstack/apps/webapp/src/app/pages/board/layers/ui/components/EscapeApp.tsx
@@ -9,14 +9,11 @@
 import { useEffect, useState } from 'react';
 import { Tooltip, IconButton, Divider } from '@chakra-ui/react';
 
-import {
-  useUIStore,
-  useThrottleApps,
-} from '@sage3/frontend';
-import { IoMdExit } from 'react-icons/io';
+import { useUIStore, useThrottleApps } from '@sage3/frontend';
+import { MdDeselect } from 'react-icons/md';
 
 type EscapeAppProps = {};
-const APP_SPACE_BUFFER = 150;            
+const APP_SPACE_BUFFER = 150;
 
 export function EscapeApp(props: EscapeAppProps) {
   const apps = useThrottleApps(1000);
@@ -27,11 +24,11 @@ export function EscapeApp(props: EscapeAppProps) {
   const selectedAppId = useUIStore((state) => state.selectedAppId);
   const setSelectedApp = useUIStore((state) => state.setSelectedApp);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!selectedAppId) {
       setViewportInApp(false);
-      return
-    };
+      return;
+    }
 
     let vpInApp = false;
     for (let i = 0; i < apps.length; i++) {
@@ -44,11 +41,12 @@ export function EscapeApp(props: EscapeAppProps) {
       const vy = viewport.position.y;
       const vw = viewport.size.width;
       const vh = viewport.size.height;
-      const isInside = vx >= x - APP_SPACE_BUFFER &&
-                      vx + vw <= x + w + APP_SPACE_BUFFER &&
-                      vy >= y - APP_SPACE_BUFFER &&
-                      vy + vh <= y + h + APP_SPACE_BUFFER;
-      
+      const isInside =
+        vx >= x - APP_SPACE_BUFFER &&
+        vx + vw <= x + w + APP_SPACE_BUFFER &&
+        vy >= y - APP_SPACE_BUFFER &&
+        vy + vh <= y + h + APP_SPACE_BUFFER;
+
       if (isInside) {
         vpInApp = true;
         break;
@@ -58,25 +56,25 @@ export function EscapeApp(props: EscapeAppProps) {
   }, [apps, viewport, selectedAppId]);
 
   return (
-  <>
-    {viewportInApp && 
     <>
-      <Divider orientation="vertical" mx="1" />
-      <Tooltip label={'Deselect App'} placement={'top'} hasArrow={true} openDelay={400}>
-        <IconButton
-          borderRadius={'0.5rem 0.5rem 0.5rem 0.5rem'}
-          size="sm"
-          colorScheme={'red'}
-          icon={<IoMdExit/>}
-          fontSize="xl"
-          aria-label={'input-type'}
-          onClick={() => {
-            setSelectedApp('');
-          }}
-        ></IconButton>
-        </Tooltip>
+      {viewportInApp && (
+        <>
+          <Divider orientation="vertical" mx="1" />
+          <Tooltip label={'Deselect App'} placement={'top'} hasArrow={true} openDelay={400}>
+            <IconButton
+              borderRadius={'0.5rem 0.5rem 0.5rem 0.5rem'}
+              size="sm"
+              colorScheme={'red'}
+              icon={<MdDeselect />}
+              fontSize="xl"
+              aria-label={'input-type'}
+              onClick={() => {
+                setSelectedApp('');
+              }}
+            ></IconButton>
+          </Tooltip>
+        </>
+      )}
     </>
-    }
-  </>
   );
 }
