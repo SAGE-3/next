ab80f2db9bfa6d66e606fbf0cc687538aa78d481
Ryan Theriot <rtheriot@hawaii.edu>
2025-04-11 11:48:11 -1000

    Join a party with a sharable id

--- webstack/libs/frontend/src/lib/ui/components/party/components/PartyHub.tsx
+++ webstack/libs/frontend/src/lib/ui/components/party/components/PartyHub.tsx
@@ -6,14 +6,15 @@
  * the file LICENSE, distributed as part of this software.
  */
 
-import { Button, VStack, HStack, Text, Flex, Divider, useColorModeValue, IconButton, Tooltip, Box } from '@chakra-ui/react';
+import { Button, VStack, HStack, Text, Flex, Divider, useColorModeValue, IconButton, Tooltip, Box, Input } from '@chakra-ui/react';
 
 // SAGE3 Imports
-import { useUsersStore } from '@sage3/frontend';
+import { useConfigStore, useUsersStore } from '@sage3/frontend';
 
 // Party Imports
 import { usePartyStore } from '././PartyStore';
-import { MdAdd } from 'react-icons/md';
+import { MdAdd, MdArrowForward } from 'react-icons/md';
+import { useState } from 'react';
 
 export function PartyHub(): JSX.Element {
   // Store imports
@@ -24,6 +25,11 @@ export function PartyHub(): JSX.Element {
 
   // Theme
   const scrollBarGripColor = useColorModeValue('#c1c1c1', '#2b2b2b');
+
+  // Party Id Enter
+  const [partyId, setPartyId] = useState('');
+
+  const { config } = useConfigStore();
 
   // List of all current parties
   // This is a list of all the parties that are currently active. Has a user connected to it.
@@ -36,8 +42,26 @@ export function PartyHub(): JSX.Element {
   const uniqueParties = Array.from(new Set(partyList));
   // Does the party really exist?
   const availableParties = parties.filter((party) => {
-    return uniqueParties.includes(party.ownerId);
+    console.log(party);
+    return uniqueParties.includes(party.ownerId) && !party.private;
   });
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevents new line in the input field
+      handleJoinPartyById();
+    }
+  };
+
+  async function handleJoinPartyById() {
+    // Party Join
+    const id = decodeUID(partyId, config.namespace);
+    // find party with matching id
+    const party = parties.find((p) => p.ownerId.includes(id));
+    if (party) {
+      setCurrentParty(party);
+    }
+  }
 
   return (
     <Flex direction="column" height="100%">
@@ -90,12 +114,51 @@ export function PartyHub(): JSX.Element {
       <Flex flexDir="column" mt="auto" width="100%">
         <Divider my="2" />
         <HStack justify="space-between" align="center">
-          <Box></Box>
+          <Box display="flex" gap="1">
+            <Input
+              placeholder="Enter Party ID..."
+              value={partyId}
+              onChange={(e) => setPartyId(e.target.value)}
+              onKeyDown={handleKeyDown}
+              size="sm"
+              flex="1"
+              borderRadius="md"
+              _placeholder={{ opacity: 0.5, color: 'gray.400' }}
+            />
+
+            <Tooltip label="Join Party" placement="top" hasArrow>
+              <IconButton
+                size="sm"
+                icon={<MdArrowForward />}
+                aria-label="Send Message "
+                onClick={handleJoinPartyById}
+                colorScheme="teal"
+                variant={'ghost'}
+                fontSize="lg"
+                isDisabled={partyId.length <= 0}
+              />
+            </Tooltip>
+          </Box>
           <Tooltip label="Create a new party" placement="top" hasArrow>
-            <IconButton size="sm" icon={<MdAdd />} aria-label="Create Party" onClick={createParty} colorScheme="teal" />
+            <IconButton size="sm" icon={<MdAdd />} aria-label="Create Party" onClick={createParty} colorScheme="teal" variant={'ghost'} />
           </Tooltip>
         </HStack>
       </Flex>
     </Flex>
   );
+}
+
+function decodeUID(encoded: string, s: string): string {
+  const salt = s.substring(0, 5);
+  const binary = atob(encoded);
+  const encoder = new TextEncoder();
+  const saltBytes = encoder.encode(salt.substring(0, 5));
+  const result = new Uint8Array(binary.length);
+
+  for (let i = 0; i < binary.length; i++) {
+    result[i] = binary.charCodeAt(i) ^ saltBytes[i % saltBytes.length];
+  }
+
+  const decoder = new TextDecoder();
+  return decoder.decode(result);
 }
