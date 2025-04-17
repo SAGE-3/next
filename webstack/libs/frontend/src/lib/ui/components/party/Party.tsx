8fae7f028a0293bfe4746b059ffa2387e49297c4
Ryan Theriot <rtheriot@hawaii.edu>
2025-04-17 06:45:41 -1000

    Typo

--- webstack/libs/frontend/src/lib/ui/components/party/Party.tsx
+++ webstack/libs/frontend/src/lib/ui/components/party/Party.tsx
@@ -1,9 +1,9 @@
 /**
- * Copyright (c) SAGE3 Development Team 2025.
+ * Copyright (c) SAGE3 Development Team 2025. All Rights Reserved
  * University of Hawaii, University of Illinois Chicago, Virginia Tech
  *
- * Distributed under the terms of the SAGE3 License.
- * See LICENSE for full details.
+ * Distributed under the terms of the SAGE3 License.  The full license is in
+ * the file LICENSE, distributed as part of this software.
  */
 
 import { useEffect } from 'react';
