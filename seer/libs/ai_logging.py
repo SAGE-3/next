f1ea6268045871ffbce5d963127f35d889f30b6a
renambot <renambot@gmail.com>
2025-05-15 16:08:09 -0500

    new: add logging into langchain

--- seer/libs/ai_logging.py
+++ seer/libs/ai_logging.py
@@ -12,6 +12,12 @@ from libs.utils import DotDict
 # logging AI prompts
 from fluent import sender
 
+# Langchain API
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.schema import BaseMessage
+from typing import Any, Dict, List, Union, Optional
+import uuid
+
 ai_logger = None
 
 
@@ -42,3 +48,104 @@ def initFluent(ps3):
     global ai_logger
     flinfo = getFluentInfo(ps3)
     ai_logger = sender.FluentSender("seer", host=flinfo.server, port=flinfo.port)
+
+
+class LoggingChainHandler(AsyncCallbackHandler):
+    def __init__(self, name: str):
+        super().__init__()
+        # Save the human prompt
+        self.human = ""
+        self.ai = ""
+        self.name = name
+
+    def setAI(self, ai):
+        """
+        Set the AI name.
+
+        Args:
+          ai (str): The AI name to set.
+        """
+        self.ai = ai
+
+    async def on_chain_start(
+        self,
+        serialized: Dict[str, Any],
+        inputs: Dict[str, Any],
+        *,
+        run_id: uuid.UUID,
+        parent_run_id: Union[uuid.UUID, None] = None,
+        tags: Union[List[str], None] = None,
+        metadata: Union[Dict[str, Any], None] = None,
+        **kwargs: Any,
+    ) -> Any:
+        if type(inputs) is dict:
+            self.human = inputs["question"].strip()
+        else:
+            if inputs.usage_metadata:
+                in_tokens = inputs.usage_metadata.get("input_tokens")
+                out_tokens = inputs.usage_metadata.get("output_tokens")
+                total_tokens = inputs.usage_metadata.get("total_tokens")
+                model = inputs.response_metadata.get("model_name")
+                # print(
+                #     f"[{self.name}] Prompt: {self.human} - Model: {model} - Input tokens: {in_tokens} - Output tokens: {out_tokens} - Total tokens: {total_tokens}"
+                # )
+                ai_logger.emit(
+                    self.ai + "." + self.name,
+                    {
+                        "prompt": self.human,
+                        "model": model,
+                        "input_tokens": in_tokens,
+                        "output_tokens": out_tokens,
+                        "total_tokens": total_tokens,
+                    },
+                )
+
+
+class LoggingLLMHandler(AsyncCallbackHandler):
+    def __init__(self, name: str):
+        super().__init__()
+        # Save the human prompt
+        self.human = ""
+        self.ai = ""
+        self.name = name
+
+    def setAI(self, ai):
+        """
+        Set the AI name.
+
+        Args:
+          ai (str): The AI name to set.
+        """
+        self.ai = ai
+
+    def setPrompt(self, prompt):
+        """
+        Set the human prompt.
+
+        Args:
+          prompt (str): The human prompt to set.
+        """
+        self.human = prompt.strip()
+
+    async def on_llm_end(self, response, run_id, parent_run_id=None, **kwargs):
+        try:
+            usage = response.llm_output.get("token_usage", {})
+            in_tokens = usage.get("prompt_tokens")
+            out_tokens = usage.get("completion_tokens")
+            total_tokens = usage.get("total_tokens")
+            model = response.llm_output.get("model_name", {})
+            # print(
+            #     f"[{self.name}] Prompt: {self.human} - Model: {model} - Input tokens: {in_tokens} - Output tokens: {out_tokens} - Total tokens: {total_tokens}"
+            # )
+            ai_logger.emit(
+                self.ai + "." + self.name,
+                {
+                    "prompt": self.human,
+                    "model": model,
+                    "input_tokens": in_tokens,
+                    "output_tokens": out_tokens,
+                    "total_tokens": total_tokens,
+                },
+            )
+        except Exception as e:
+            print("No token usage found:", e)
