3bdd2c8546ce4361787bbbefd106c43a60fab060
LAVA <lava@RiotBook-4.local>
2025-07-31 09:20:32 -1000

    Revert index

--- webstack/libs/applications/src/lib/apps/PDFViewer/index.ts
+++ webstack/libs/applications/src/lib/apps/PDFViewer/index.ts
@@ -13,20 +13,11 @@
 
 import { z } from 'zod';
 
-// Layout mode enum for different page arrangements
-export const LayoutMode = {
-  HORIZONTAL: 'horizontal',
-  VERTICAL: 'vertical',
-} as const;
-
-export type LayoutModeType = typeof LayoutMode[keyof typeof LayoutMode];
-
 export const schema = z.object({
   assetid: z.string(),
   currentPage: z.number(),
   numPages: z.number(),
   displayPages: z.number(),
-  layoutMode: z.enum([LayoutMode.HORIZONTAL, LayoutMode.VERTICAL]).default(LayoutMode.HORIZONTAL),
   // Functions to execute
   executeInfo: z.object({
     executeFunc: z.string(),
@@ -42,7 +33,6 @@ export const init: Partial<state> = {
   currentPage: 0,
   numPages: 1,
   displayPages: 1,
-  layoutMode: LayoutMode.HORIZONTAL,
   executeInfo: { executeFunc: '', params: {} },
   analyzed: '',
   client: '',
