5d214616df9c2ae3a793caf264485dd304435680
renambot <renambot@gmail.com>
2025-10-30 15:56:21 -0500

    fix: pass apikey when calling 'llama' models with the ChatNVIDIA class from langchain

--- seer/app/chat.py
+++ seer/app/chat.py
@@ -70,6 +70,7 @@ class ChatAgent:
             llm_llama = ChatNVIDIA(
                 base_url=llama["url"] + "/v1",
                 model=llama["model"],
+                api_key=llama["apiKey"],
                 stream=False,
                 max_tokens=2000,
             )
